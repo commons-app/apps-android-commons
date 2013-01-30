64e16ad1cca96afed40f0b8ff27b7cad59e645c1
YuviPanda <yuvipanda@gmail.com>
2013-01-30 22:30:42 +0530

    Add missing DbOpenHelper class (required by prev. commit)

---
+++ commons/src/main/java/org/wikimedia/commons/data/DBOpenHelper.java
@@ -0,0 +1,26 @@
+package org.wikimedia.commons.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import org.wikimedia.commons.contributions.Contribution;
+
+public class DBOpenHelper  extends SQLiteOpenHelper{
+
+    private static final String DATABASE_NAME = "commons.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DBOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        Contribution.Table.onCreate(sqLiteDatabase);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
+        Contribution.Table.onUpdate(sqLiteDatabase);
+    }
+}
