7f1ab39683a684897aaf66a83b9fc011a7a1e920
YuviPanda <yuvipanda@gmail.com>
2013-02-23 16:47:13 +0530

    Show loading progress for images in detail view

--- commons/src/main/java/org/wikimedia/commons/media/MediaDetailFragment.java
+++ commons/src/main/java/org/wikimedia/commons/media/MediaDetailFragment.java
@@ -1,16 +1,21 @@
 package org.wikimedia.commons.media;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import com.actionbarsherlock.app.SherlockFragment;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.assist.ImageLoadingListener;
 import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.assist.SimpleImageLoadingListener;
 import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
 import org.wikimedia.commons.Media;
 import org.wikimedia.commons.R;
@@ -29,6 +34,8 @@ public class MediaDetailFragment extends SherlockFragment {
 
     private ImageView image;
     private TextView title;
+    private ProgressBar loadingProgress;
+    private ImageView loadingFailed;
 
 
     @Override
@@ -45,8 +52,34 @@ public class MediaDetailFragment extends SherlockFragment {
         View view = inflater.inflate(R.layout.fragment_media_detail, container, false);
         image = (ImageView) view.findViewById(R.id.mediaDetailImage);
         title = (TextView) view.findViewById(R.id.mediaDetailTitle);
+        loadingProgress = (ProgressBar) view.findViewById(R.id.mediaDetailImageLoading);
+        loadingFailed = (ImageView) view.findViewById(R.id.mediaDetailImageFailed);
+
         String actualUrl = TextUtils.isEmpty(media.getImageUrl()) ? media.getLocalUri().toString() : media.getThumbnailUrl(640);
-        ImageLoader.getInstance().displayImage(actualUrl, image, displayOptions);
+        ImageLoader.getInstance().displayImage(actualUrl, image, displayOptions, new ImageLoadingListener() {
+            public void onLoadingStarted() {
+                loadingProgress.setVisibility(View.VISIBLE);
+            }
+
+            public void onLoadingFailed(FailReason failReason) {
+                loadingProgress.setVisibility(View.GONE);
+                loadingFailed.setVisibility(View.VISIBLE);
+
+            }
+
+            public void onLoadingComplete(Bitmap bitmap) {
+                loadingProgress.setVisibility(View.GONE);
+                loadingFailed.setVisibility(View.GONE);
+                image.setVisibility(View.VISIBLE);
+
+            }
+
+            public void onLoadingCancelled() {
+                // wat?
+                throw new RuntimeException("Image loading cancelled. But why?");
+
+            }
+        });
         title.setText(Utils.displayTitleFromTitle(media.getFilename()));
 
         return view;
