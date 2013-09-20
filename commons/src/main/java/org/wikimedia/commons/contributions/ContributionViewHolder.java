2da5453ec13221c663f1898a55f65de6aa4d9d74
YuviPanda <yuvipanda@gmail.com>
2013-09-19 18:18:41 -0700

    Make the ImageView to be of the specific type
    Change-Id: I51ced26ea0b654457919d915e322d91eb211f781

--- commons/src/main/java/org/wikimedia/commons/contributions/ContributionViewHolder.java
+++ commons/src/main/java/org/wikimedia/commons/contributions/ContributionViewHolder.java
@@ -4,10 +4,11 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import org.wikimedia.commons.MediaWikiImageView;
 import org.wikimedia.commons.R;
 
 class ContributionViewHolder {
-    final ImageView imageView;
+    final MediaWikiImageView imageView;
     final TextView titleView;
     final TextView stateView;
     final TextView seqNumView;
@@ -16,7 +17,7 @@ class ContributionViewHolder {
     String url;
 
     ContributionViewHolder(View parent) {
-        imageView = (ImageView)parent.findViewById(R.id.contributionImage);
+        imageView = (MediaWikiImageView) parent.findViewById(R.id.contributionImage);
         titleView = (TextView)parent.findViewById(R.id.contributionTitle);
         stateView = (TextView)parent.findViewById(R.id.contributionState);
         seqNumView = (TextView)parent.findViewById(R.id.contributionSequenceNumber);
