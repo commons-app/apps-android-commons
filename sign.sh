649af6c5168d8afe73541558e93162834145a6f9
Nicolas Raoul <nicolas.raoul@gmail.com>
2015-09-08 10:10:04 +0900

    Prepare for Google Play release

---
+++ sign.sh
@@ -0,0 +1,2 @@
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore /home/nico/d/d/wya/nr-commons.keystore commons/target/commons-1.0-SNAPSHOT.apk nrkeystorealias
+zipalign -f -v 4 commons/target/commons-1.0-SNAPSHOT.apk commons/target/commons-1.0-SNAPSHOT_signed.apk
