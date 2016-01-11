6eea283dc4edca1bd222379d24790491175a6b15
misaochan <josephinelim86@gmail.com>
2016-01-11 19:24:05 +1300

    Close cursor after use

--- commons/src/main/java/fr/free/nrw/commons/upload/ShareActivity.java
+++ commons/src/main/java/fr/free/nrw/commons/upload/ShareActivity.java
@@ -171,7 +171,10 @@ public  class       ShareActivity
         Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
         int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
         cursor.moveToFirst();
-        return cursor.getString(column_index);
+
+        String cursorString = cursor.getString(column_index);
+        cursor.close();
+        return cursorString;
     }
 
     @Override
