8c2959f5b07524b3105744ed077922766bd39ed4
misaochan <josephinelim86@gmail.com>
2016-01-12 16:41:19 +1300

    Added FileUtils class and license

---
+++ commons/src/main/java/fr/free/nrw/commons/upload/FileUtils.java
@@ -0,0 +1,5 @@
+package fr.free.nrw.commons.upload;
+
+//Taken from https://github.com/iPaulPro/aFileChooser/blob/master/aFileChooser/src/com/ipaulpro/afilechooser/utils/FileUtils.java#L257 (Apache License 2.0)
+public class FileUtils {
+}
