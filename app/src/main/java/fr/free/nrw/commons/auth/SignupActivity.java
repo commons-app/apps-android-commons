18e69ca435e45af336709679c608ad6945e2f1c1
misaochan <josephinelim86@gmail.com>
2016-08-03 18:14:21 +1200

    Add activity to manifest

--- app/src/main/java/fr/free/nrw/commons/auth/SignupActivity.java
+++ app/src/main/java/fr/free/nrw/commons/auth/SignupActivity.java
@@ -2,6 +2,7 @@ package fr.free.nrw.commons.auth;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 
 import fr.free.nrw.commons.R;
 
@@ -11,6 +12,7 @@ public class SignupActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        Log.d("SignupActivity", "Signup Activity started");
     }
 
 }
