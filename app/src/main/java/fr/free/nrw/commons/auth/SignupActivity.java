498873adae360c8b470c23a3bdd49d873eb4dae6
misaochan <josephinelim86@gmail.com>
2016-08-05 18:34:20 +1200

    Modify URL and webview

--- app/src/main/java/fr/free/nrw/commons/auth/SignupActivity.java
+++ app/src/main/java/fr/free/nrw/commons/auth/SignupActivity.java
@@ -3,6 +3,7 @@ package fr.free.nrw.commons.auth;
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
+import android.webkit.WebView;
 
 import fr.free.nrw.commons.R;
 
@@ -13,6 +14,11 @@ public class SignupActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         Log.d("SignupActivity", "Signup Activity started");
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        //myWebView.loadUrl("https://commons.wikimedia.org/w/index.php?title=Special:CreateAccount&returnto=Main+Page");
+        myWebView.loadUrl("https://commons.m.wikimedia.org/w/index.php?title=Special:CreateAccount&returnto=Main+Page&returntoquery=welcome%3Dyes");
+        //Problem: display not good on large screen. Use mobile page?
+        //After Create Account button is pressed within WebView, it brings user to https://commons.wikimedia.org/wiki/Main_Page. So can we just override that URL?
     }
 
 }
