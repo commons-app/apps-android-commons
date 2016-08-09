0a0725f0a1e3fa1c3c8f9f2abed7bf274ad1231c
misaochan <josephinelim86@gmail.com>
2016-08-09 15:20:42 +1200

    Modify code according to dev tutorial

--- app/src/main/java/fr/free/nrw/commons/auth/SignupActivity.java
+++ app/src/main/java/fr/free/nrw/commons/auth/SignupActivity.java
@@ -12,13 +12,14 @@ public class SignupActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
         Log.d("SignupActivity", "Signup Activity started");
-        WebView myWebView = (WebView) findViewById(R.id.webview);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
         //myWebView.loadUrl("https://commons.wikimedia.org/w/index.php?title=Special:CreateAccount&returnto=Main+Page");
 
         //Mobile page, looks better than the above
-        myWebView.loadUrl("https://commons.m.wikimedia.org/w/index.php?title=Special:CreateAccount&returnto=Main+Page&returntoquery=welcome%3Dyes");
+        webView.loadUrl("https://commons.m.wikimedia.org/w/index.php?title=Special:CreateAccount&returnto=Main+Page&returntoquery=welcome%3Dyes");
 
         //After Create Account button is pressed within WebView, it brings user to https://commons.wikimedia.org/wiki/Main_Page. So can we just override that URL?
         //Do we NEED to enable JS? Validation seems to work fine here
