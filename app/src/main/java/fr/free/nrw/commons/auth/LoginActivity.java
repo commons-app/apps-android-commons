9242c2144f2a9b3cddc482d1d9b14c9c34ac7183
misaochan <josephinelim86@gmail.com>
2016-08-13 18:29:02 +1200

    Add intent flags

--- app/src/main/java/fr/free/nrw/commons/auth/LoginActivity.java
+++ app/src/main/java/fr/free/nrw/commons/auth/LoginActivity.java
@@ -93,8 +93,10 @@ public class LoginActivity extends AccountAuthenticatorActivity {
                 // FIXME: If the user turns it off, it shouldn't be auto turned back on
                 ContentResolver.setSyncAutomatically(account, ContributionsContentProvider.AUTHORITY, true); // Enable sync by default!
                 ContentResolver.setSyncAutomatically(account, ModificationsContentProvider.AUTHORITY, true); // Enable sync by default!
-                //FIXME: This is taking users back to SignupActivity
+
+                //FIXME: This is supposed to return user to ContributionsActivity (launcher activity), but is instead taking users back to SignupActivity
                 context.finish();
+
             } else {
                 int response;
                 if(result.equals("NetworkFailure")) {
@@ -244,6 +246,7 @@ public class LoginActivity extends AccountAuthenticatorActivity {
     //Called when Sign Up button is clicked
     public void signUp(View view) {
         Intent intent = new Intent(this, SignupActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
         startActivity(intent);
     }
 }
