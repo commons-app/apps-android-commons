43149610afed0cfc970892a514952d7955295405
misaochan <josephinelim86@gmail.com>
2016-08-17 18:02:38 +1200

    Remove extra title

--- app/src/main/java/fr/free/nrw/commons/contributions/ContributionsActivity.java
+++ app/src/main/java/fr/free/nrw/commons/contributions/ContributionsActivity.java
@@ -135,14 +135,12 @@ public  class       ContributionsActivity
             /** Called when a drawer has settled in a completely open state. */
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
-                getSupportActionBar().setTitle("Navigation!");
                 invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
             }
 
             /** Called when a drawer has settled in a completely closed state. */
             public void onDrawerClosed(View view) {
                 super.onDrawerClosed(view);
-                getSupportActionBar().setTitle(mActivityTitle);
                 invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
             }
         };
