05af8a0827c7afc698f9f0bc6693762357c5ed4f
misaochan <josephinelim86@gmail.com>
2016-08-22 17:46:28 +1200

    Add NearbyActivity to manifest

--- app/src/main/java/fr/free/nrw/commons/nearby/NearbyActivity.java
+++ app/src/main/java/fr/free/nrw/commons/nearby/NearbyActivity.java
@@ -1,17 +1,19 @@
 package fr.free.nrw.commons.nearby;
 
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 
 import fr.free.nrw.commons.R;
 
-public class NearbyActivity extends Activity {
+public class NearbyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nearby);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
