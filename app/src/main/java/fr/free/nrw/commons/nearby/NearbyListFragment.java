7d99c93a0058c5773d00d8fe26a016a131e82ac3
misaochan <josephinelim86@gmail.com>
2016-08-24 18:01:16 +1200

    Make NearbyListFragment extend ListFragment

--- app/src/main/java/fr/free/nrw/commons/nearby/NearbyListFragment.java
+++ app/src/main/java/fr/free/nrw/commons/nearby/NearbyListFragment.java
@@ -3,17 +3,21 @@ package fr.free.nrw.commons.nearby;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import fr.free.nrw.commons.R;
 
-public class NearbyListFragment extends Fragment {
+public class NearbyListFragment extends ListFragment {
 
     private LatLng mLatestLocation;
     private int mImageSize;
     private boolean mItemClicked;
+
+    private static final String TAG = "NearbyListFragment";
 
     public NearbyListFragment() {
     }
@@ -22,7 +26,7 @@ public class NearbyListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-
+        Log.d(TAG, "NearbyListFragment created");
 
         View view = inflater.inflate(R.layout.fragment_nearby, container, false);
 
