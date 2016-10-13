9519210ec341fd113ebc1c8d2b9c54b644526ff8
misaochan <josephinelim86@gmail.com>
2016-10-13 13:58:34 +1300

    Add SO url in comment

--- app/src/main/java/fr/free/nrw/commons/category/CategorizationFragment.java
+++ app/src/main/java/fr/free/nrw/commons/category/CategorizationFragment.java
@@ -128,7 +128,8 @@ public class CategorizationFragment extends Fragment {
         String title = titleDesc.getString("Title", "");
         Log.d(TAG, "Title: " + title);
 
-        //TODO: How to get results? Maybe change to using onPostExecute instead?
+        //TODO: Change to using onPostExecute instead
+        //See http://stackoverflow.com/questions/10972114/how-to-get-a-string-back-from-asynctask
         TitleCategories asyncTask = new TitleCategories(title);
         try {
             items = asyncTask.execute().get();
