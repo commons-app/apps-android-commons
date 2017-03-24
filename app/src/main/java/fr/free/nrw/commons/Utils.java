06aeede864242e3652fe10b0c69a739a4c926d1d
veyndan <veyndan89@gmail.com>
2017-03-24 04:30:08 +0000

    Remove unused method implode(…)

--- app/src/main/java/fr/free/nrw/commons/Utils.java
+++ app/src/main/java/fr/free/nrw/commons/Utils.java
@@ -251,20 +251,6 @@ public class Utils {
         throw new RuntimeException("Unrecognized license value");
     }
 
-    public static String implode(String glue, Iterable<String> pieces) {
-        StringBuffer buffer = new StringBuffer();
-        boolean first = true;
-        for (String piece : pieces) {
-            if (first) {
-                first = false;
-            } else {
-                buffer.append(glue);
-            }
-            buffer.append(pieces);
-        }
-        return buffer.toString();
-    }
-
     public static Uri uriForWikiPage(String name) {
         String underscored = name.trim().replace(" ", "_");
         String uriStr = CommonsApplication.HOME_URL + urlEncode(underscored);
