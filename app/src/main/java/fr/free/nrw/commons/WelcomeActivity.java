4c68ede14ca1080929b5a4e7d6acba8463c3f04c
maskara <maskara@zeta.tech>
2017-04-02 04:41:11 +0530

    Fix codacy warnings

--- app/src/main/java/fr/free/nrw/commons/WelcomeActivity.java
+++ app/src/main/java/fr/free/nrw/commons/WelcomeActivity.java
@@ -1,18 +1,15 @@
 package fr.free.nrw.commons;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-
-import com.viewpagerindicator.CirclePageIndicator;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
+import com.viewpagerindicator.CirclePageIndicator;
+
 import fr.free.nrw.commons.theme.BaseActivity;
 
 public class WelcomeActivity extends BaseActivity {
--- app/src/main/java/fr/free/nrw/commons/WelcomePagerAdapter.java
+++ app/src/main/java/fr/free/nrw/commons/WelcomePagerAdapter.java
@@ -3,7 +3,6 @@ package fr.free.nrw.commons;
 import android.app.Activity;
 import android.content.Context;
 import android.support.v4.view.PagerAdapter;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
