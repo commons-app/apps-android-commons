82c0d24f371b52b0549e1c8b6c03369bf6e6ae04
maskara <maskara@zeta.tech>
2017-05-21 11:56:40 +0200

    Show the upload count for contributions by the user

---
+++ app/src/main/java/fr/free/nrw/commons/concurrency/BackgroundPoolExceptionHandler.java
@@ -0,0 +1,21 @@
+package fr.free.nrw.commons.concurrency;
+
+import android.support.annotation.NonNull;
+import fr.free.nrw.commons.BuildConfig;
+
+public class BackgroundPoolExceptionHandler implements ExceptionHandler {
+    @Override
+    public void onException(@NonNull final Throwable t) {
+        //Crash for debug build
+        if (BuildConfig.DEBUG) {
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    throw new RuntimeException(t);
+                }
+            });
+            thread.start();
+        }
+    }
+}
+
