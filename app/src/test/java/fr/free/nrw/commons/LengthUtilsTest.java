a62bfd20013284a525cd375784b96b70bd642a31
Tobias Schönberg <tobias47n9e@gmail.com>
2017-05-28 18:35:03 +0200

    Fix LatLng tests

--- app/src/test/java/fr/free/nrw/commons/LengthUtilsTest.java
+++ app/src/test/java/fr/free/nrw/commons/LengthUtilsTest.java
@@ -10,36 +10,36 @@ import org.junit.Test;
 
 public class LengthUtilsTest {
     @Test public void testZeroDistance() {
-        LatLng pointA = new LatLng(0, 0);
-        LatLng pointB = new LatLng(0, 0);
+        LatLng pointA = new LatLng(0, 0, 0);
+        LatLng pointB = new LatLng(0, 0, 0);
         String distance = LengthUtils.formatDistanceBetween(pointA, pointB);
         Assert.assertThat(distance, is("0m"));
     }
 
     @Test public void testOneDegreeOnEquator() {
-        LatLng pointA = new LatLng(0, 0);
-        LatLng pointB = new LatLng(0, 1);
+        LatLng pointA = new LatLng(0, 0, 0);
+        LatLng pointB = new LatLng(0, 1, 0);
         String distance = LengthUtils.formatDistanceBetween(pointA, pointB);
         Assert.assertThat(distance, is("111.2km"));
     }
 
     @Test public void testOneDegreeFortyFiveDegrees() {
-        LatLng pointA = new LatLng(45, 0);
-        LatLng pointB = new LatLng(45, 1);
+        LatLng pointA = new LatLng(45, 0, 0);
+        LatLng pointB = new LatLng(45, 1, 0);
         String distance = LengthUtils.formatDistanceBetween(pointA, pointB);
         Assert.assertThat(distance, is("78.6km"));
     }
 
     @Test public void testOneDegreeSouthPole() {
-        LatLng pointA = new LatLng(-90, 0);
-        LatLng pointB = new LatLng(-90, 1);
+        LatLng pointA = new LatLng(-90, 0, 0);
+        LatLng pointB = new LatLng(-90, 1, 0);
         String distance = LengthUtils.formatDistanceBetween(pointA, pointB);
         Assert.assertThat(distance, is("0m"));
     }
 
     @Test public void testPoleToPole() {
-        LatLng pointA = new LatLng(90, 0);
-        LatLng pointB = new LatLng(-90, 0);
+        LatLng pointA = new LatLng(90, 0, 0);
+        LatLng pointB = new LatLng(-90, 0, 0);
         String distance = LengthUtils.formatDistanceBetween(pointA, pointB);
         Assert.assertThat(distance, is("20,015.1km"));
     }
