1618640046a7749a9e8075f8e0f3e73430bf7343
Mikel <mikel@akaita.com>
2017-08-02 22:02:36 +0100

    Make Codacy happy

--- app/src/main/java/fr/free/nrw/commons/utils/UiUtils.java
+++ app/src/main/java/fr/free/nrw/commons/utils/UiUtils.java
@@ -30,25 +30,23 @@ public class UiUtils {
 
     /**
      * Converts dp unit to equivalent pixels.
-     *
      * @param dp density independent pixels
-     * @param context
+     * @param context Context to access display metrics
      * @return px equivalent to dp value
      */
-    public static float convertDpToPixel(float dp, Context context){
+    public static float convertDpToPixel(float dp, Context context) {
         DisplayMetrics metrics = context.getResources().getDisplayMetrics();
-        return dp * ((float)metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+        return dp * ((float) metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
     }
 
     /**
      * Converts device specific pixels to dp.
-     *
      * @param px pixels
-     * @param context
+     * @param context Context to access display metrics
      * @return dp equivalent to px value
      */
-    public static float convertPixelsToDp(float px, Context context){
+    public static float convertPixelsToDp(float px, Context context) {
         DisplayMetrics metrics = context.getResources().getDisplayMetrics();
-        return px / ((float)metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+        return px / ((float) metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
     }
 }
