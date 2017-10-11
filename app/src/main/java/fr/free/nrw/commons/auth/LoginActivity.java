c6de66e32a59c208278622b0df682765a2f1a07e
Andrew Marshall <amarshall@boomtownroi.com>
2017-10-11 10:57:22 -0400

    Optimized Imports.

--- app/src/main/java/fr/free/nrw/commons/auth/LoginActivity.java
+++ app/src/main/java/fr/free/nrw/commons/auth/LoginActivity.java
@@ -17,10 +17,9 @@ import android.widget.Toast;
 
 import fr.free.nrw.commons.BuildConfig;
 import fr.free.nrw.commons.CommonsApplication;
+import fr.free.nrw.commons.PageTitle;
 import fr.free.nrw.commons.R;
 import fr.free.nrw.commons.WelcomeActivity;
-
-import fr.free.nrw.commons.PageTitle;
 import fr.free.nrw.commons.contributions.ContributionsActivity;
 import timber.log.Timber;
 
