836a1e36645662be5a51a2288ddf0c3e2d2d318a
Vishan Seru <vishanseru9@gmail.com>
2017-10-21 13:44:06 +0530

    Added option in SettingsFragment to allow users to send the logs as a file via email

--- app/src/main/java/fr/free/nrw/commons/settings/SettingsFragment.java
+++ app/src/main/java/fr/free/nrw/commons/settings/SettingsFragment.java
@@ -1,19 +1,37 @@
 package fr.free.nrw.commons.settings;
 
+import android.Manifest;
 import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.EditTextPreference;
 import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
+import java.io.File;
+
+import fr.free.nrw.commons.BuildConfig;
 import fr.free.nrw.commons.CommonsApplication;
 import fr.free.nrw.commons.R;
 import fr.free.nrw.commons.Utils;
+import fr.free.nrw.commons.utils.FileUtils;
 
 public class SettingsFragment extends PreferenceFragment {
+
+    private static final int REQUEST_CODE_WRITE_EXTERNAL_STORAGE = 100;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,6 +84,57 @@ public class SettingsFragment extends PreferenceFragment {
             return true;
         });
 
+        Preference sendLogsPreference = findPreference("sendLogs");
+        sendLogsPreference.setOnPreferenceClickListener(preference -> {
+            //first we need to check if we have the necessary permissions
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (ContextCompat.checkSelfPermission(
+                        getActivity(),
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        ==
+                        PackageManager.PERMISSION_GRANTED) {
+                    sendAppLogsViaEmail();
+                } else {
+                    //first get the necessary permission
+                    requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            REQUEST_CODE_WRITE_EXTERNAL_STORAGE);
+                }
+            } else {
+                sendAppLogsViaEmail();
+            }
 
+            return true;
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_WRITE_EXTERNAL_STORAGE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                sendAppLogsViaEmail();
+            }
+        }
+    }
+
+    private void sendAppLogsViaEmail() {
+        String appLogs = Utils.getAppLogs();
+        File appLogsFile = FileUtils.createAndGetAppLogsFile(appLogs);
+        Uri appLogsFilePath = Uri.fromFile(appLogsFile);
+
+        Intent feedbackIntent = new Intent(Intent.ACTION_SEND);
+        feedbackIntent.setType("message/rfc822");
+        feedbackIntent.putExtra(Intent.EXTRA_EMAIL,
+                new String[]{CommonsApplication.FEEDBACK_EMAIL});
+        feedbackIntent.putExtra(Intent.EXTRA_SUBJECT,
+                String.format(CommonsApplication.FEEDBACK_EMAIL_SUBJECT,
+                        BuildConfig.VERSION_NAME));
+        feedbackIntent.putExtra(Intent.EXTRA_STREAM,appLogsFilePath);
+
+        try {
+            startActivity(feedbackIntent);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(getActivity(), R.string.no_email_client, Toast.LENGTH_SHORT).show();
+        }
     }
 }
