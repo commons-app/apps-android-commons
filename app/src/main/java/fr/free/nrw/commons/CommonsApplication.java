0964b05eedaa06986d9a4db6e514dcade760f0f3
Yusuke Matsubara <whym@whym.org>
2017-11-03 11:26:56 +0900

    Disable LeakCanary for Robolectric tests
    See 'For Robolectric users:' https://github.com/square/leakcanary

--- app/src/main/java/fr/free/nrw/commons/CommonsApplication.java
+++ app/src/main/java/fr/free/nrw/commons/CommonsApplication.java
@@ -17,6 +17,7 @@ import android.support.v4.util.LruCache;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.stetho.Stetho;
 import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 
 import org.acra.ACRA;
 import org.acra.ReportingInteractionMode;
@@ -117,12 +118,8 @@ public class CommonsApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(this);
+
+        setupLeakCanary();
 
         Timber.plant(new Timber.DebugTree());
 
@@ -141,6 +138,13 @@ public class CommonsApplication extends Application {
 
         //For caching area -> categories
         cacheData  = new CacheController();
+    }
+
+    protected RefWatcher setupLeakCanary() {
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return RefWatcher.DISABLED;
+        }
+        return LeakCanary.install(this);
     }
 
     /**
