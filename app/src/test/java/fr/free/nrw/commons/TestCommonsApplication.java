e317c369c1f0d0efc7be342ad423482c68ae6386
Yusuke Matsubara <whym@whym.org>
2017-11-03 11:26:56 +0900

    Disable LeakCanary for Robolectric tests
    See 'For Robolectric users:' https://github.com/square/leakcanary

---
+++ app/src/test/java/fr/free/nrw/commons/TestCommonsApplication.java
@@ -0,0 +1,11 @@
+package fr.free.nrw.commons;
+
+import com.squareup.leakcanary.RefWatcher;
+
+// This class is automatically discovered by Robolectric
+public class TestCommonsApplication extends CommonsApplication {
+    @Override protected RefWatcher setupLeakCanary() {
+        // No leakcanary in unit tests.
+        return RefWatcher.DISABLED;
+    }
+}
