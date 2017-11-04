07db06008dee1ee5a23a98bce2a126ab562942d9
maskara <maskara@zeta.tech>
2017-11-04 10:43:16 +0530

    WIP: OAuth for App login

---
+++ app/src/main/java/fr/free/nrw/commons/auth/OAuthConstants.java
@@ -0,0 +1,16 @@
+package fr.free.nrw.commons.auth;
+
+public class OAuthConstants {
+
+    public static final String CONSUMER_KEY = "efc15ccf765eb39aad7c6a3c25f73826";
+
+    public static final String CONSUMER_SECRET = "5d3adf518c5fced4fd28d92d73704d3a9e8bdd04";
+
+    public static final String TEMPORARY_TOKEN_REQUEST_URL = "https://en.wikipedia.org/w/index.php?title=Special:OAuth/initiate";
+
+    public static final String AUTHORIZATION_VERIFIER_SERVER_URL = "https://en.wikipedia.org/wiki/Special:OAuth/authorize";
+
+    public static final String TOKEN_SERVER_URL = "https://en.wikipedia.org/w/index.php?title=Special:OAuth/initiate";
+
+    public static final String REDIRECT_URL = "https://commons-app.github.io/";
+}
