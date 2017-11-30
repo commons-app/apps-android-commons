a841fdaafd54f217bcf4ab87a4903de30eafed68
Paul Hawke <paul.hawke@gmail.com>
2017-11-29 21:22:27 -0600

    Made test configuration more obvious and added mocks for Dagger injection so it can be controlled in tests.

--- app/src/test/java/fr/free/nrw/commons/MediaTest.java
+++ app/src/test/java/fr/free/nrw/commons/MediaTest.java
@@ -9,7 +9,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 @RunWith(RobolectricTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = 21)
+@Config(constants = BuildConfig.class, sdk = 21, application = TestCommonsApplication.class)
 public class MediaTest {
     @Test
     public void displayTitleShouldStripExtension() {
