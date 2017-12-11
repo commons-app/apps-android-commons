503a7a01a4c87f8dbd93d0f289ceac9de894146e
neslihanturan <tur.neslihan@gmail.com>
2017-12-11 12:42:12 +0300

    Set display list button visibility according to bottom sheet visibility

--- app/src/main/java/fr/free/nrw/commons/nearby/NearbyActivity.java
+++ app/src/main/java/fr/free/nrw/commons/nearby/NearbyActivity.java
@@ -105,7 +105,7 @@ public class NearbyActivity extends NavigationBaseActivity implements LocationUp
 
             @Override
             public void onStateChanged(View bottomSheet, int newState) {
-                // TODO prepareViewsForSheetPosition(newState);
+                prepareViewsForSheetPosition(newState);
             }
 
             @Override
@@ -403,5 +403,16 @@ public class NearbyActivity extends NavigationBaseActivity implements LocationUp
     @Override
     public void onLocationChanged(LatLng latLng) {
         refreshView(false);
+    }
+
+    public void prepareViewsForSheetPosition(int bottomSheetState) {
+        if (bottomSheetState >= BottomSheetBehavior.STATE_COLLAPSED) {
+            fabList.show();
+        }
+        else if (bottomSheetState == BottomSheetBehavior.STATE_EXPANDED) {
+            fabList.hide();
+            //NearbyActivity.bottomSheetStatus = BottomSheetStatus.DISPLAY_LIST_SHEET;
+        }
+
     }
 }
