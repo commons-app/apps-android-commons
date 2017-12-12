f2edf158f6b87a1b5178ba27fd1115adaa9c1da1
neslihanturan <tur.neslihan@gmail.com>
2017-12-12 21:44:31 +0300

    Add .xml of nearby lit items

--- app/src/main/java/fr/free/nrw/commons/nearby/PlaceRenderer.java
+++ app/src/main/java/fr/free/nrw/commons/nearby/PlaceRenderer.java
@@ -2,6 +2,8 @@ package fr.free.nrw.commons.nearby;
 
 import android.os.CountDownTimer;
 import android.support.annotation.NonNull;
+import android.support.v4.view.ViewCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,26 +22,34 @@ import butterknife.ButterKnife;
 import fr.free.nrw.commons.R;
 
 class PlaceRenderer extends Renderer<Place> {
+    //private static boolean isAnyItemOpen = false;
 
     @BindView(R.id.tvName) TextView tvName;
     @BindView(R.id.tvDesc) TextView tvDesc;
     @BindView(R.id.distance) TextView distance;
     @BindView(R.id.icon) ImageView icon;
-    @BindView(R.id.buttonLayout)
-    LinearLayout buttonLayout;
+    @BindView(R.id.buttonLayout) LinearLayout buttonLayout;
     private Animation animationUp;
     private Animation animationDown;
-    private final int COUNTDOWN_RUNNING_TIME = 300;
+
+    private View view;
+    private final int COUNTDOWN_RUNNING_TIME = 500;
     private static ArrayList<LinearLayout> openedItems;
 
 
+    //private final PlaceClickedListener listener;
+
     PlaceRenderer(){
+        //@NonNull PlaceClickedListener listener) {
+        Log.d("nesli","renderer created");
         openedItems = new ArrayList<>();
+        //this.listener = listener;
     }
 
     @Override
     protected View inflate(LayoutInflater layoutInflater, ViewGroup viewGroup) {
-        return layoutInflater.inflate(R.layout.item_place, viewGroup, false);
+        view = layoutInflater.inflate(R.layout.item_place, viewGroup, false);
+        return view;
     }
 
     @Override
@@ -51,9 +61,11 @@ class PlaceRenderer extends Renderer<Place> {
 
     @Override
     protected void hookListeners(View view) {
+
         final View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.d("Renderer", "clicked");
                 if(buttonLayout.isShown()){
                     closeLayout(buttonLayout);
                 }else {
@@ -76,11 +88,10 @@ class PlaceRenderer extends Renderer<Place> {
         });
     }
 
-    private void closeLayout(LinearLayout buttonLayout) {
-
+    private void closeLayout(LinearLayout buttonLayout){
+        //openedItems.remove(buttonLayout);
         buttonLayout.startAnimation(animationUp);
-        CountDownTimer countDownTimerStatic = new CountDownTimer(COUNTDOWN_RUNNING_TIME
-                , 16) {
+        CountDownTimer countDownTimerStatic = new CountDownTimer(COUNTDOWN_RUNNING_TIME, 16) {
             @Override
             public void onTick(long millisUntilFinished) {
             }
@@ -88,6 +99,7 @@ class PlaceRenderer extends Renderer<Place> {
             @Override
             public void onFinish() {
                 buttonLayout.setVisibility(View.GONE);
+                //buttonLayout.setVisibility(View.GONE);
             }
         };
         countDownTimerStatic.start();
@@ -95,7 +107,9 @@ class PlaceRenderer extends Renderer<Place> {
 
     private void openLayout(LinearLayout buttonLayout){
         buttonLayout.setVisibility(View.VISIBLE);
+        ViewCompat.setElevation( view, 10);
         buttonLayout.startAnimation(animationDown);
+        //openedItems.add(buttonLayout);
     }
 
     @Override
@@ -110,4 +124,8 @@ class PlaceRenderer extends Renderer<Place> {
         distance.setText(place.distance);
         icon.setImageResource(place.getLabel().getIcon());
     }
+
+    /*interface PlaceClickedListener {
+        void onPlaceOpened(Place place);
+    }*/
 }
