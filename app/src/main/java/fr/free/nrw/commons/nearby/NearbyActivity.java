00ad5a7a71a0adab5fb0d54d6e6c7a807590ccaa
neslihanturan <tur.neslihan@gmail.com>
2017-12-13 20:20:38 +0300

    Add icon to bottom sheet details

--- app/src/main/java/fr/free/nrw/commons/nearby/NearbyActivity.java
+++ app/src/main/java/fr/free/nrw/commons/nearby/NearbyActivity.java
@@ -316,6 +316,8 @@ public class NearbyActivity extends NavigationBaseActivity implements LocationUp
         checkGps();
     }
 
+
+
     /**
      * This method should be the single point to load/refresh nearby places
      *
