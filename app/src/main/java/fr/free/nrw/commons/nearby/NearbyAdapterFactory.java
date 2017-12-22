ada00c0047763dc6a33485443e6df8a589e553e1
neslihanturan <tur.neslihan@gmail.com>
2017-12-22 14:05:14 +0300

    Cleanup commented out old codes

--- app/src/main/java/fr/free/nrw/commons/nearby/NearbyAdapterFactory.java
+++ app/src/main/java/fr/free/nrw/commons/nearby/NearbyAdapterFactory.java
@@ -10,17 +10,13 @@ import java.util.Collections;
 import java.util.List;
 
 class NearbyAdapterFactory {
-    //private PlaceRenderer.PlaceClickedListener listener;
-    public static boolean isItemOpen = false;
 
     NearbyAdapterFactory(){
-        //@NonNull PlaceRenderer.PlaceClickedListener listener) {
-        //this.listener = listener;
+
     }
 
     public RVRendererAdapter<Place> create(List<Place> placeList) {
         RendererBuilder<Place> builder = new RendererBuilder<Place>()
-                //.bind(Place.class, new PlaceRenderer(listener));
                 .bind(Place.class, new PlaceRenderer());
         ListAdapteeCollection<Place> collection = new ListAdapteeCollection<>(
                 placeList != null ? placeList : Collections.<Place>emptyList());
