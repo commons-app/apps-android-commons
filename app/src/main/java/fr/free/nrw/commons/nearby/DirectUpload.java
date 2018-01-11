050d878edd5f9cfc485d6aa70a4d455de3457cbc
misaochan <josephinelim86@gmail.com>
2018-01-11 18:39:22 +1000

    Copy camera capture code to DirectUpload.java

--- app/src/main/java/fr/free/nrw/commons/nearby/DirectUpload.java
+++ app/src/main/java/fr/free/nrw/commons/nearby/DirectUpload.java
@@ -11,11 +11,15 @@ import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+
 import fr.free.nrw.commons.R;
 import fr.free.nrw.commons.contributions.ContributionController;
 import timber.log.Timber;
 
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
 class DirectUpload {
@@ -24,6 +28,9 @@ class DirectUpload {
     private String desc;
     private ContributionController controller;
     private Fragment fragment;
+
+    @Inject @Named("prefs") SharedPreferences prefs;
+    @Inject @Named("default_preferences") SharedPreferences defaultPrefs;
 
     DirectUpload(String title, String desc, Fragment fragment, ContributionController controller) {
         this.title = title;
@@ -39,6 +46,31 @@ class DirectUpload {
         editor.putString("Title", title);
         editor.putString("Desc", desc);
         editor.apply();
+    }
+
+    void initiateCameraUpload() {
+        boolean useExtStorage = defaultPrefs.getBoolean("useExternalStorage", true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && useExtStorage) {
+            if (ContextCompat.checkSelfPermission(fragment.getActivity(), WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (fragment.getActivity().shouldShowRequestPermissionRationale(WRITE_EXTERNAL_STORAGE)) {
+                    new AlertDialog.Builder(fragment.getActivity())
+                            .setMessage(fragment.getActivity().getString(R.string.write_storage_permission_rationale))
+                            .setPositiveButton("OK", (dialog, which) -> {
+                                fragment.getActivity().requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE}, 3);
+                                dialog.dismiss();
+                            })
+                            .setNegativeButton("Cancel", null)
+                            .create()
+                            .show();
+                } else {
+                    fragment.getActivity().requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE}, 3);
+                }
+            } else {
+                controller.startCameraCapture();
+            }
+        } else {
+            controller.startCameraCapture();
+        }
     }
 
     void initiateGalleryUpload() {
