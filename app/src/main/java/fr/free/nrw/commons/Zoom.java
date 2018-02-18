db364621857fad09399344eec937459ac4ce219a
tanvidadu <tanvidadu1998@gmail.com>
2018-02-18 22:59:12 +0530

    Refactored the code according to guidelines

--- app/src/main/java/fr/free/nrw/commons/Zoom.java
+++ app/src/main/java/fr/free/nrw/commons/Zoom.java
@@ -15,26 +15,19 @@ import android.widget.ImageView;
 
 public class Zoom  implements OnTouchListener {
 
-
-
-
     Matrix matrix = new Matrix();
     Matrix savedMatrix = new Matrix();
-
 
     static final int NONE = 0;
     static final int DRAG = 1;
     static final int ZOOM = 2;
     int mode = NONE;
 
-
     PointF start = new PointF();
     PointF mid = new PointF();
     float oldDist = 1f;
 
-
     public void ZoomInZoomOut(ImageView imageview) {
-
         ImageView view = imageview;
         view.setOnTouchListener(this);
     }
@@ -44,47 +37,30 @@ public class Zoom  implements OnTouchListener {
         ImageView view = (ImageView) v;
         view.setScaleType(ImageView.ScaleType.MATRIX);
         float scale;
-
-
-
-
         switch (event.getAction() & MotionEvent.ACTION_MASK) {
             case MotionEvent.ACTION_DOWN:
                 savedMatrix.set(matrix);
                 start.set(event.getX(), event.getY());
-
                 mode = DRAG;
                 break;
-
             case MotionEvent.ACTION_UP:
-
             case MotionEvent.ACTION_POINTER_UP:
-
                 mode = NONE;
-
                 break;
-
             case MotionEvent.ACTION_POINTER_DOWN:
-
                 oldDist = spacing(event);
-
                 if (oldDist > 5f) {
                     savedMatrix.set(matrix);
                     midPoint(mid, event);
                     mode = ZOOM;
-
                 }
                 break;
-
             case MotionEvent.ACTION_MOVE:
-
                 if (mode == DRAG) {
                     matrix.set(savedMatrix);
                     matrix.postTranslate(event.getX() - start.x, event.getY() - start.y);
                 } else if (mode == ZOOM) {
-
                     float newDist = spacing(event);
-
                     if (newDist > 5f) {
                         matrix.set(savedMatrix);
                         scale = newDist / oldDist;
@@ -93,12 +69,9 @@ public class Zoom  implements OnTouchListener {
                 }
                 break;
         }
-
         view.setImageMatrix(matrix);
-
         return true;
     }
-
 
     private float spacing(MotionEvent event) {
         float x = event.getX(0) - event.getX(1);
@@ -106,12 +79,9 @@ public class Zoom  implements OnTouchListener {
         return (float) Math.sqrt(x * x + y * y);
     }
 
-
     private void midPoint(PointF point, MotionEvent event) {
         float x = event.getX(0) + event.getX(1);
         float y = event.getY(0) + event.getY(1);
         point.set(x / 2, y / 2);
     }
-
-
 }
