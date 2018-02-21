c3faf25d12d26045fd980e9bb94d10e5bf319c3e
Suchit Kar <suchitkar1997@gmail.com>
2018-02-21 15:34:08 +0530

    Remove unused import in SingleUploadFragment

--- app/src/main/java/fr/free/nrw/commons/upload/SingleUploadFragment.java
+++ app/src/main/java/fr/free/nrw/commons/upload/SingleUploadFragment.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
