9f621539c7eff87a4ddfaf3387aec41842494ff2
Ujjwal Agrawal <ujjwal.iitism@gmail.com>
2018-03-14 14:47:00 +0530

    Added showSnackbar in ViewUtil and shown in NearbyActivity

--- app/src/main/java/fr/free/nrw/commons/utils/ViewUtil.java
+++ app/src/main/java/fr/free/nrw/commons/utils/ViewUtil.java
@@ -2,6 +2,8 @@ package fr.free.nrw.commons.utils;
 
 import android.content.Context;
 import android.support.annotation.StringRes;
+import android.support.design.widget.Snackbar;
+import android.view.View;
 import android.widget.Toast;
 
 public class ViewUtil {
@@ -9,4 +11,13 @@ public class ViewUtil {
     public static void showLongToast(final Context context, @StringRes final int stringResId) {
         ExecutorUtils.uiExecutor().execute(() -> Toast.makeText(context, context.getString(stringResId), Toast.LENGTH_LONG).show());
     }
+
+    public static void showLongSnackbar(View view, String message) {
+        Snackbar.make(view, message, Snackbar.LENGTH_LONG).show();
+    }
+
+    public static void showSnackbar(View view, int messageResourceId) {
+        Snackbar.make(view, messageResourceId, Snackbar.LENGTH_SHORT).show();
+    }
+
 }
