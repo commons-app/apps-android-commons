5e828d45412eef9eb172b3ca86e774a04bbb4920
misaochan <josephinelim86@gmail.com>
2018-03-16 20:01:52 +1000

    Save categories in PlaceRenderer and SingleUploadFragment

--- app/src/main/java/fr/free/nrw/commons/upload/SingleUploadFragment.java
+++ app/src/main/java/fr/free/nrw/commons/upload/SingleUploadFragment.java
@@ -114,6 +114,9 @@ public class SingleUploadFragment extends CommonsDaggerSupportFragment {
         if (isNearbyUpload) {
             String imageTitle = directPrefs.getString("Title", "");
             String imageDesc = directPrefs.getString("Desc", "");
+            //TODO: Remove this later, this fragment does not need to know categories
+            String imageCats = directPrefs.getString("Category", "");
+            Timber.d("Image title: " + imageTitle + ", image desc: " + imageDesc + ", image categories: " + imageCats);
             titleEdit.setText(imageTitle);
             descEdit.setText(imageDesc);
         }
