6a7ced5215e5072e444654bc6608255b9bf2beaf
97balakrishnan <97balakrishnan@gmail.com>
2018-03-22 13:04:52 +0530

    Added NetworkUtils class

--- app/src/main/java/fr/free/nrw/commons/nearby/NearbyActivity.java
+++ app/src/main/java/fr/free/nrw/commons/nearby/NearbyActivity.java
@@ -1,10 +1,7 @@
 package fr.free.nrw.commons.nearby;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -19,7 +16,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
-import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -31,6 +27,7 @@ import javax.inject.Inject;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import fr.free.nrw.commons.utils.NetworkUtils;
 import fr.free.nrw.commons.R;
 import fr.free.nrw.commons.location.LatLng;
 import fr.free.nrw.commons.location.LocationServiceManager;
@@ -298,9 +295,9 @@ public class NearbyActivity extends NavigationBaseActivity implements LocationUp
      */
     private void refreshView(LocationServiceManager.LocationChangeType locationChangeType) {
 
-        if(!haveNetworkConnection())
+        if(!NetworkUtils.haveNetworkConnection(getApplicationContext()))
         {
-            Toast.makeText(getApplicationContext(),"No internet connnection ",Toast.LENGTH_LONG).show();
+            ViewUtil.showLongToast(getApplicationContext(),getResources().getString(R.string.no_internet));
             hideProgressBar();
             return;
         }
@@ -505,25 +502,7 @@ public class NearbyActivity extends NavigationBaseActivity implements LocationUp
     public void prepareViewsForSheetPosition(int bottomSheetState) {
         // TODO
     }
-    private boolean haveNetworkConnection() {
 
-        boolean haveConnectedWifi = false;
-        boolean haveConnectedMobile = false;
-
-        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        if(cm==null)
-            return false;
-        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
-        for (NetworkInfo ni : netInfo) {
-            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
-                if (ni.isConnected())
-                    haveConnectedWifi = true;
-            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
-                if (ni.isConnected())
-                    haveConnectedMobile = true;
-        }
-        return haveConnectedWifi || haveConnectedMobile;
-    }
 
 
 }
