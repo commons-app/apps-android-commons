8aab16229d27cb97d26e5c2c84176d3520d88776
wanderlustyc <yeerane0503@gmail.com>
2018-04-01 20:43:07 -0400

    assigned top and right margin values to the compass button.

--- app/src/main/java/fr/free/nrw/commons/nearby/NearbyMapFragment.java
+++ app/src/main/java/fr/free/nrw/commons/nearby/NearbyMapFragment.java
@@ -452,6 +452,7 @@ public class NearbyMapFragment extends DaggerFragment {
 
     private void setupMapView(Bundle savedInstanceState) {
         MapboxMapOptions options = new MapboxMapOptions()
+                .compassMargins(new int[]{0, 200, 50, 0})
                 .styleUrl(Style.OUTDOORS)
                 .logoEnabled(false)
                 .attributionEnabled(false)
