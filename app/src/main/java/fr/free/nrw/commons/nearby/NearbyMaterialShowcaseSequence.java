557baa19809253d258d0f58f9a84288a1be1f3da
neslihanturan <tur.neslihan@gmail.com>
2018-04-21 16:22:28 +0300

    Create a NearbyMaterialShowcaseSequence class

--- app/src/main/java/fr/free/nrw/commons/nearby/NearbyMaterialShowcaseSequence.java
+++ app/src/main/java/fr/free/nrw/commons/nearby/NearbyMaterialShowcaseSequence.java
@@ -1,5 +1,19 @@
 package fr.free.nrw.commons.nearby;
 
+import android.app.Activity;
 
-public class NearbyMaterialShowcaseTutorial extends MaterialS{
+import uk.co.deanwild.materialshowcaseview.MaterialShowcaseSequence;
+import uk.co.deanwild.materialshowcaseview.ShowcaseConfig;
+
+
+public class NearbyMaterialShowcaseSequence extends MaterialShowcaseSequence {
+
+    public NearbyMaterialShowcaseSequence(Activity activity, String sequenceID) {
+        super(activity, sequenceID);
+        ShowcaseConfig config = new ShowcaseConfig();
+        config.setDelay(500); // half second between each showcase view
+        this.setConfig(config);
+        this.singleUse(sequenceID); // Display tutorial only once
+    }
+
 }
