6744940970b161c4ec41957e27d1a883ed26b80c
tanvidadu <tanvidadu1998@gmail.com>
2018-05-04 01:44:26 +0530

    Implemented Statistics

---
+++ app/src/main/java/fr/free/nrw/commons/Achievements.java
@@ -0,0 +1,20 @@
+package fr.free.nrw.commons;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ImageView;
+
+public class Achievements extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_achievements);
+
+
+    }
+
+}
