fe82581472373095ec0e9236546b3dd44849b5b0
tanvidadu <tanvidadu1998@gmail.com>
2018-05-20 16:31:52 +0530

    Improved the quality of Pr

--- app/src/main/java/fr/free/nrw/commons/achievements/AchievementsActivity.java
+++ app/src/main/java/fr/free/nrw/commons/achievements/AchievementsActivity.java
@@ -1,4 +1,4 @@
-package fr.free.nrw.commons;
+package fr.free.nrw.commons.achievements;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -20,37 +20,42 @@ import android.widget.Toolbar;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Random;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import dagger.Binds;
+import fr.free.nrw.commons.R;
 import fr.free.nrw.commons.theme.NavigationBaseActivity;
 
-public class Achievements extends AppCompatActivity {
+public class AchievementsActivity extends AppCompatActivity {
 
-    private static final double badge_image_ratio_width = 0.4;
-    private static final double badge_image_ratio_height = 0.36;
-    private ImageView imageView;
+    private static final double BADGE_IMAGE_WIDTH_RATIO = 0.4;
+    private static final double BADGE_IMAGE_HEIGHT_RATIO = 0.36;
+
+    @BindView(R.id.achievement_badge) ImageView imageView;
+    @BindView(R.id.toolbar) android.support.v7.widget.Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_achievements);
+        ButterKnife.bind(this);
 
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         int height = displayMetrics.heightPixels;
         int width = displayMetrics.widthPixels;
 
-        imageView = (ImageView)findViewById(R.id.achievement_badge);
-
         RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams)
                 imageView.getLayoutParams();
-        params.height = (int) (height*badge_image_ratio_height);
-        params.width = (int) (width*badge_image_ratio_width);
+        params.height = (int) (height*BADGE_IMAGE_HEIGHT_RATIO);
+        params.width = (int) (width*BADGE_IMAGE_WIDTH_RATIO);
         imageView.setImageResource(R.drawable.sydney_opera_house);
         imageView.requestLayout();
 
-        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
     }
 
     @Override
@@ -73,6 +78,11 @@ public class Achievements extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * To take screenshot of the screen and return it in Bitmap format
+     * @param view
+     * @return
+     */
     public static Bitmap getScreenShot(View view) {
         View screenView = view.getRootView();
         screenView.setDrawingCacheEnabled(true);
@@ -81,6 +91,10 @@ public class Achievements extends AppCompatActivity {
         return bitmap;
     }
 
+    /**
+     * To take bitmap and store it temporary storage and share it
+     * @param bitmap
+     */
     void shareScreen ( Bitmap bitmap){
         try {
             File file = new File(this.getExternalCacheDir(),"screen.png");
@@ -94,11 +108,9 @@ public class Achievements extends AppCompatActivity {
             intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
             intent.setType("image/png");
             startActivity(Intent.createChooser(intent, "Share image via"));
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (IOException e) {
+            //Do Nothing
         }
     }
-
-
 
 }
