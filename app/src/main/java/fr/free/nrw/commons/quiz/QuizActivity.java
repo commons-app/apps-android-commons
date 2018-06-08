7c41f05e4e97b188d35c40d1b74eb76182fca125
tanvidadu <tanvidadu1998@gmail.com>
2018-06-08 21:59:51 +0530

    Layout for mcq added

--- app/src/main/java/fr/free/nrw/commons/quiz/QuizActivity.java
+++ app/src/main/java/fr/free/nrw/commons/quiz/QuizActivity.java
@@ -2,6 +2,10 @@ package fr.free.nrw.commons.quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import fr.free.nrw.commons.R;
 
@@ -11,5 +15,10 @@ public class QuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        LinearLayout linearLayout = (LinearLayout)findViewById(R.id.question);
+        ImageView imageView = (ImageView) linearLayout.findViewById(R.id.question_image);
+        TextView textView = (TextView) linearLayout.findViewById(R.id.question_text);
+        textView.setText("Which License can be used to donate this image to public domain?");
     }
 }
