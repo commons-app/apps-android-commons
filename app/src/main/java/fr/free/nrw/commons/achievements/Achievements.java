c8771e0b58ce7af5f93a2cecbc046a44940a2feb
tanvidadu <tanvidadu1998@gmail.com>
2018-06-15 00:30:47 +0530

    Added Builder model

--- app/src/main/java/fr/free/nrw/commons/achievements/Achievements.java
+++ app/src/main/java/fr/free/nrw/commons/achievements/Achievements.java
@@ -11,107 +11,117 @@ public class Achievements {
     private int featuredImages;
     private int imagesUploaded;
 
-    /**
-     * Sets the unique images used
-     *
-     * @param uniqueUsedImages
-     */
-    public void setUniqueUsedImages(int uniqueUsedImages) {
+    public Achievements(){
+
+    }
+
+    public Achievements(int uniqueUsedImages,
+                        int articlesUsingImages,
+                        int thanksReceived,
+                        int imagesEditedBySomeoneElse,
+                        int featuredImages,
+                        int imagesUploaded) {
         this.uniqueUsedImages = uniqueUsedImages;
-    }
-
-    /**
-     * returns the unique images used
-     *
-     * @return
-     */
-    public int getUniqueUsedImages() {
-        return uniqueUsedImages;
-    }
-
-    /**
-     * sets the number of articles using images
-     *
-     * @param articlesUsingImages
-     */
-    public void setArticlesUsingImages(int articlesUsingImages) {
         this.articlesUsingImages = articlesUsingImages;
-    }
-
-    /**
-     * returns the number of articles using images
-     *
-     * @return
-     */
-    public int getArticlesUsingImages() {
-        return articlesUsingImages;
-    }
-
-    /**
-     * sets the no of thanks received by user
-     *
-     * @param thanksReceived
-     */
-    public void setThanksReceived(int thanksReceived) {
         this.thanksReceived = thanksReceived;
-    }
-
-    /**
-     * return the no of thanks received by user
-     *
-     * @return
-     */
-    public int getThanksReceived() {
-        return thanksReceived;
-    }
-
-    /**
-     * sets the no of images edited by someone else
-     *
-     * @param imagesEditedBySomeoneElse
-     */
-    public void setImagesEditedBySomeoneElse(int imagesEditedBySomeoneElse) {
         this.imagesEditedBySomeoneElse = imagesEditedBySomeoneElse;
-    }
-
-    /**
-     * returns the no of images edited by someone else
-     *
-     * @return
-     */
-    public int getImagesEditedBySomeoneElse() {
-        return imagesEditedBySomeoneElse;
-    }
-
-    /**
-     *  sets the total of images featured
-     * @param featuredImages
-     */
-    public void setFeaturedImages(int featuredImages) {
         this.featuredImages = featuredImages;
+        this.imagesUploaded = imagesUploaded;
+    }
+    public class AchievementsBuilder {
+        private int nestedUniqueUsedImages;
+        private int nestedArticlesUsingImages;
+        private int nestedThanksReceived;
+        private int nestedImagesEditedBySomeoneElse;
+        private int nestedFeaturedImages;
+        private int nestedImagesUploaded;
+
+        public AchievementsBuilder setUniqueUsedImages(int uniqueUsedImages) {
+            this.nestedUniqueUsedImages = uniqueUsedImages;
+            return this;
+        }
+
+        public AchievementsBuilder setArticlesUsingImages(int articlesUsingImages) {
+            this.nestedArticlesUsingImages = articlesUsingImages;
+            return this;
+        }
+
+        public AchievementsBuilder setThanksReceived(int thanksReceived) {
+            this.nestedThanksReceived = thanksReceived;
+            return this;
+        }
+
+        public AchievementsBuilder setImagesEditedBySomeoneElse(int imagesEditedBySomeoneElse) {
+            this.nestedImagesEditedBySomeoneElse = imagesEditedBySomeoneElse;
+            return this;
+        }
+
+        public AchievementsBuilder setFeaturedImages(int featuredImages) {
+            this.nestedFeaturedImages = featuredImages;
+            return this;
+        }
+
+        public AchievementsBuilder setImagesUploaded(int imagesUploaded) {
+            this.nestedImagesUploaded = imagesUploaded;
+            return this;
+        }
+
+        public Achievements createAchievements(){
+            return new Achievements(nestedUniqueUsedImages,
+                    nestedArticlesUsingImages,
+                    nestedThanksReceived,
+                    nestedImagesEditedBySomeoneElse,
+                    nestedFeaturedImages,
+                    nestedImagesUploaded);
+        }
+
     }
 
-    /**
-     * returns the total count of images featured
-     * @return
-     */
+    public int getImagesUploaded() {
+        return imagesUploaded;
+    }
+
     public int getFeaturedImages() {
         return featuredImages;
     }
 
-    /**
-     * to set the total number of images uploaded
-     * @param imagesUploaded
-     */
+    public int getImagesEditedBySomeoneElse() {
+        return imagesEditedBySomeoneElse;
+    }
+
+    public int getThanksReceived() {
+        return thanksReceived;
+    }
+
+    public int getArticlesUsingImages() {
+        return articlesUsingImages;
+    }
+
+    public int getUniqueUsedImages() {
+        return uniqueUsedImages;
+    }
+
     public void setImagesUploaded(int imagesUploaded) {
         this.imagesUploaded = imagesUploaded;
     }
 
-    /**
-     * to get the total number of uploads
-     * @return
-     */
-    public int getImagesUploaded() {
-        return imagesUploaded;
+    public void setFeaturedImages(int featuredImages) {
+        this.featuredImages = featuredImages;
+    }
+
+    public void setImagesEditedBySomeoneElse(int imagesEditedBySomeoneElse) {
+        this.imagesEditedBySomeoneElse = imagesEditedBySomeoneElse;
+    }
+
+    public void setThanksReceived(int thanksReceived) {
+        this.thanksReceived = thanksReceived;
+    }
+
+    public void setArticlesUsingImages(int articlesUsingImages) {
+        this.articlesUsingImages = articlesUsingImages;
+    }
+
+    public void setUniqueUsedImages(int uniqueUsedImages) {
+        this.uniqueUsedImages = uniqueUsedImages;
     }
 }
