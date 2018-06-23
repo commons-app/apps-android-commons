d116a27eeab800bfef3efd7902ccb7a9649f06d4
neslihanturan <tur.neslihan@gmail.com>
2018-06-23 15:15:06 +0300

    Make it work

--- app/src/main/java/fr/free/nrw/commons/upload/ShareActivity.java
+++ app/src/main/java/fr/free/nrw/commons/upload/ShareActivity.java
@@ -170,6 +170,7 @@ public class ShareActivity
         return !FileUtils.isSelfOwned(getApplicationContext(), mediaUri)
                 && (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED);
+        //return false;
     }
 
     /**
