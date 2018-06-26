c8033be08a3bf94f53eb1df093d66df2a390e595
neslihanturan <tur.neslihan@gmail.com>
2018-06-26 23:10:58 +0300

    Add file removal method

--- app/src/main/java/fr/free/nrw/commons/mwapi/MediaWikiApi.java
+++ app/src/main/java/fr/free/nrw/commons/mwapi/MediaWikiApi.java
@@ -1,5 +1,6 @@
 package fr.free.nrw.commons.mwapi;
 
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
@@ -42,7 +43,7 @@ public interface MediaWikiApi {
     List<Media> getCategoryImages(String categoryName);
 
     @NonNull
-    UploadResult uploadFile(String filename, InputStream file, long dataLength, String pageContents, String editSummary, ProgressListener progressListener) throws IOException;
+    UploadResult uploadFile(String filename, InputStream file, long dataLength, String pageContents, String editSummary, ProgressListener progressListener, Uri fileUri) throws IOException;
 
     @Nullable
     String edit(String editToken, String processedPageContent, String filename, String summary) throws IOException;
