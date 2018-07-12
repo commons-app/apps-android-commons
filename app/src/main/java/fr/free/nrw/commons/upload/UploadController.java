96f6113ebdb2941c4e2914dd01d0bba60c6a1280
neslihanturan <tur.neslihan@gmail.com>
2018-07-12 18:23:11 +0300

    Add Nullable anotation to method

--- app/src/main/java/fr/free/nrw/commons/upload/UploadController.java
+++ app/src/main/java/fr/free/nrw/commons/upload/UploadController.java
@@ -206,7 +206,7 @@ public class UploadController {
                         contribution.setDateCreated(new Date());
                     }
                 }
-                return contribution;
+            return contribution;
             }
 
             @Override
