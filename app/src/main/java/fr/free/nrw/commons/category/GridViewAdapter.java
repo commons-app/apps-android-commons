a2c5fc219abd825b40053dff91171ea21fd54336
Ujjwal Agrawal <ujjwal.iitism@gmail.com>
2018-07-14 17:27:54 +0530

    Optimimzed imports

--- app/src/main/java/fr/free/nrw/commons/category/GridViewAdapter.java
+++ app/src/main/java/fr/free/nrw/commons/category/GridViewAdapter.java
@@ -2,13 +2,11 @@ package fr.free.nrw.commons.category;
 
 import android.app.Activity;
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
