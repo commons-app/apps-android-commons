16a9afc7f2d141890fb3a5806dd4c88b089f4a84
Ashish <ashishkumar468@gmail.com>
2018-08-23 00:33:45 +0530

    Added a Constants class to handle request and result codes from one place and other related constants common to the all app elements

---
+++ app/src/main/java/fr/free/nrw/commons/utils/Constants.java
@@ -0,0 +1,9 @@
+package fr.free.nrw.commons.utils;
+
+public class Constants {
+
+    public class REQUEST_CODES {
+
+        public static final int OPEN_APPLICATION_DETAIL_SETTINGS = 1001;
+    }
+}
