d1dd19c50c84f3e871ab482f2b667250ad70b0db
neslihanturan <tur.neslihan@gmail.com>
2018-10-10 13:48:19 +0300

    Remove unused imports, organise logs and add comments for NotificationsActivity

--- app/src/main/java/fr/free/nrw/commons/notification/NotificationActivity.java
+++ app/src/main/java/fr/free/nrw/commons/notification/NotificationActivity.java
@@ -10,14 +10,12 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 
 import com.pedrogomez.renderers.RVRendererAdapter;
 
-import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
@@ -92,7 +90,6 @@ public class NotificationActivity extends NavigationBaseActivity {
 
         // Store when add notification is called last
         long currentDate = new Date(System.currentTimeMillis()).getTime();
-        Log.d("denemeee", "current date"+currentDate);
         getSharedPreferences("prefs", MODE_PRIVATE).edit().putLong("last_read_notification_date", currentDate).apply();
         Timber.d("Set last notification read date to current date:"+ currentDate);
 
