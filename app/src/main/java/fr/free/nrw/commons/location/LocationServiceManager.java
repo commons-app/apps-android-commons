d0f34d93752e00717532dd9aa01773a5ad0e0d25
neslihanturan <tur.neslihan@gmail.com>
2018-10-10 13:47:19 +0300

    Remove unused imports, organise logs and add comments for LocationServiceManager

--- app/src/main/java/fr/free/nrw/commons/location/LocationServiceManager.java
+++ app/src/main/java/fr/free/nrw/commons/location/LocationServiceManager.java
@@ -11,7 +11,6 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.util.Log;
 
 import java.util.HashSet;
 import java.util.List;
@@ -255,7 +254,7 @@ public class LocationServiceManager implements LocationListener {
 
     @Override
     public void onLocationChanged(Location location) {
-        Log.d("deneme","onLocationChanged is called");
+        Timber.d("on location changed");
             if (isBetterLocation(location, lastLocation)
                     .equals(LocationChangeType.LOCATION_SIGNIFICANTLY_CHANGED)) {
                 lastLocation = location;
