582adc8321824c1165af93f9a1508bed9aafa946
Divyanshu Shukla <shukladivyanshu967@gmail.com>
2018-10-24 23:23:54 +0530

    Update FileUtils.java

--- app/src/main/java/fr/free/nrw/commons/utils/FileUtils.java
+++ app/src/main/java/fr/free/nrw/commons/utils/FileUtils.java
@@ -9,7 +9,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.Objects;
 
 /**
  * Created for file operations
