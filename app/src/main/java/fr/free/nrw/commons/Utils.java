872ef7d1dceb7ba7a83bae4d7d909710eb761755
DylanRobson <u1056911@utah.edu>
2018-12-22 21:32:12 -0800

    Fix issue #2164 Links open with handleWebUrl.
    * AboutActivity links now open with the in-app tabbed browser.
    * UploadActivity license and policy links now open with the in-app tabbed browser.

--- app/src/main/java/fr/free/nrw/commons/Utils.java
+++ app/src/main/java/fr/free/nrw/commons/Utils.java
@@ -183,6 +183,9 @@ public class Utils {
         }
     }
 
+    /**
+     * Opens Custom Tab Activity with in-app browser for the specified URL.
+     */
     public static void handleWebUrl(Context context, Uri url) {
         Timber.d("Launching web url %s", url.toString());
         Intent browserIntent = new Intent(Intent.ACTION_VIEW, url);
@@ -197,7 +200,8 @@ public class Utils {
         builder.setSecondaryToolbarColor(ContextCompat.getColor(context, R.color.primaryDarkColor));
         builder.setExitAnimations(context, android.R.anim.slide_in_left, android.R.anim.slide_out_right);
         CustomTabsIntent customTabsIntent = builder.build();
-        customTabsIntent.intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        // Clear previous browser tasks, so that back/exit buttons work as intended.
+        customTabsIntent.intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         customTabsIntent.launchUrl(context, url);
     }
 
