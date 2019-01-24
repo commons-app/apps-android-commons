bba08f1f77f7abe2cfdac74129bd0c4670c5b941
vanshikaarora <vanshikaa937@gmail.com>
2019-01-24 23:25:35 +0530

    added the function in dateutils

--- app/src/main/java/fr/free/nrw/commons/media/MediaDetailFragment.java
+++ app/src/main/java/fr/free/nrw/commons/media/MediaDetailFragment.java
@@ -13,6 +13,7 @@ import android.support.annotation.Nullable;
 import android.text.Html;
 import android.text.TextUtils;
 import android.text.format.DateFormat;
+import android.text.format.DateUtils;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -54,6 +55,7 @@ import fr.free.nrw.commons.location.LatLng;
 import fr.free.nrw.commons.mwapi.MediaWikiApi;
 import fr.free.nrw.commons.ui.widget.CompatTextView;
 import timber.log.Timber;
+import fr.free.nrw.commons.utils.DateUtils;
 
 import static android.content.Context.CLIPBOARD_SERVICE;
 import static android.view.View.GONE;
@@ -519,14 +521,7 @@ public class MediaDetailFragment extends CommonsDaggerSupportFragment {
         if (date == null || date.toString() == null || date.toString().isEmpty()) {
             return "Uploaded date not available";
         }
-        String formatter;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2) {
-            formatter = new SimpleDateFormat(DateFormat.getBestDateTimePattern(Locale.getDefault(), "dd MMM yyyy"), Locale.getDefault()).format(date);
-        }
-        else {
-            formatter = String.valueOf(new SimpleDateFormat("dd MMM yyyy", Locale.getDefault()));
-        }
-        return formatter;
+        return DateUtils.dateInLocaleFormat(date);
     }
 
     /**
