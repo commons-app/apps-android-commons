e9b690453ca8218f382f4ee8ee92af2ff9120306
vanshikaarora <vanshikaa937@gmail.com>
2019-01-30 20:49:34 +0530

    readfbmd class

---
+++ app/src/main/java/fr/free/nrw/commons/upload/ReadFBMD.java
@@ -0,0 +1,18 @@
+package fr.free.nrw.commons.upload;
+
+import com.icafe4j.image.meta.Metadata;
+import com.icafe4j.image.meta.MetadataEntry;
+import com.icafe4j.image.meta.MetadataType;
+import com.icafe4j.image.meta.iptc.IPTC;
+
+import retrofit2.http.Url;
+
+public class ReadFBMD {
+    private Url imageUrl;
+    ReadFBMD(Url url){
+        this.imageUrl=url;
+    }
+    private boolean processFBMD(Url url){
+        Metadat
+    }
+}
