b3051b2816c2d2aee1ba9f624f7c36f07140dac4
vanshikaarora <vanshikaa937@gmail.com>
2019-01-31 09:37:29 +0530

    jar files

--- app/src/main/java/fr/free/nrw/commons/upload/ReadFBMD.java
+++ app/src/main/java/fr/free/nrw/commons/upload/ReadFBMD.java
@@ -1,18 +1,44 @@
 package fr.free.nrw.commons.upload;
 
 import com.icafe4j.image.meta.Metadata;
-import com.icafe4j.image.meta.MetadataEntry;
+//import com.icafe4j.image.meta.MetadataEntry;
 import com.icafe4j.image.meta.MetadataType;
 import com.icafe4j.image.meta.iptc.IPTC;
+import com.icafe4j.string.StringUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+import java.io.IOException;
+import java.util.Map;
 
 import retrofit2.http.Url;
 
 public class ReadFBMD {
-    private Url imageUrl;
-    ReadFBMD(Url url){
-        this.imageUrl=url;
+    /*public static void main(String[] args) throws IOException {
+        Map<MetadataType, Metadata> metadataMap = Metadata.readMetadata(args[0]);
+        IPTC iptc = (IPTC)metadataMap.get(MetadataType.IPTC);
+
+        if(iptc != null) {
+            Map<String, > = iptc.getDataSets();
+
+            while(iterator.hasNext()) {
+                MetadataEntry item = iterator.next();
+                printMetadata(item, "", "     ");
+            }
+        }
     }
-    private boolean processFBMD(Url url){
-        Metadat
-    }
+    private void printMetadata(MetadataEntry entry, String indent, String increment) {
+        logger.info(indent + entry.getKey() (StringUtils.isNullOrEmpty(entry.getValue())? "" : ": " + entry.getValue()));
+        if(entry.isMetadataEntryGroup()) {
+            indent += increment;
+            Collection<MetadataEntry> entries = entry.getMetadataEntries();
+            for(MetadataEntry e : entries) {
+                printMetadata(e, indent, increment);
+            }
+        }
+    }*/
+
 }
+
