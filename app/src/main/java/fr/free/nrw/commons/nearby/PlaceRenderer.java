f4cee9fb10e42e206d989415e969a0c65ecb27db
Madhur Gupta <30932899+madhurgupta10@users.noreply.github.com>
2019-02-20 20:10:51 +0530

    Removed Unused import Log statement

--- app/src/main/java/fr/free/nrw/commons/nearby/PlaceRenderer.java
+++ app/src/main/java/fr/free/nrw/commons/nearby/PlaceRenderer.java
@@ -9,7 +9,6 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
