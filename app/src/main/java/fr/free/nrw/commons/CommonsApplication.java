5a9cce0f79a6676dd42ef7b910e6f46a5140a0c1
Madhur Gupta <30932899+madhurgupta10@users.noreply.github.com>
2019-02-27 01:49:59 +0530

    Add Device Support Information to Feedback Intent #2494 (#2499)
    * Add Device Support Information to Feedback Intent #2494
    
    * Update Device Support Information for Feedback Intent #2494
    
    * Update Device Support Information and used common method for Feedback Intent #2494
    
    * Optimise code for Device Support Information for Feedback Intent #2494

--- app/src/main/java/fr/free/nrw/commons/CommonsApplication.java
+++ app/src/main/java/fr/free/nrw/commons/CommonsApplication.java
@@ -70,9 +70,11 @@ public class CommonsApplication extends Application {
 
     public static final String FEEDBACK_EMAIL = "commons-app-android@googlegroups.com";
 
-    public static final String FEEDBACK_EMAIL_SUBJECT = "Commons Android App (%s) Feedback";
+    public static final String FEEDBACK_EMAIL_SUBJECT = "Commons Android App Feedback";
 
     public static final String NOTIFICATION_CHANNEL_ID_ALL = "CommonsNotificationAll";
+
+    public static final String FEEDBACK_EMAIL_TEMPLATE_HEADER = "-- Technical information --";
 
     /**
      * Constants End
