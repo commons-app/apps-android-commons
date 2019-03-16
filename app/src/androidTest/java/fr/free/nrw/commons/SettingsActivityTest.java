f1366bdefa4d32b7bf27da02e4b40168d3b268f0
Adam Jones <jones_adam@rocketmail.com>
2019-03-16 18:09:25 +0000

    Fix SettingsActivityTest

--- app/src/androidTest/java/fr/free/nrw/commons/SettingsActivityTest.java
+++ app/src/androidTest/java/fr/free/nrw/commons/SettingsActivityTest.java
@@ -1,18 +1,20 @@
 package fr.free.nrw.commons;
 
 import android.content.Context;
-import androidx.test.InstrumentationRegistry;
-import androidx.test.espresso.Espresso;
-import androidx.test.espresso.matcher.PreferenceMatchers;
-import androidx.test.filters.LargeTest;
-import androidx.test.rule.ActivityTestRule;
-import androidx.test.runner.AndroidJUnit4;
+
+import com.google.gson.Gson;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import androidx.test.InstrumentationRegistry;
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.matcher.PreferenceMatchers;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
 import fr.free.nrw.commons.kvstore.JsonKvStore;
 import fr.free.nrw.commons.settings.Prefs;
 import fr.free.nrw.commons.settings.SettingsActivity;
@@ -28,7 +30,7 @@ import static org.junit.Assert.assertEquals;
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class SettingsActivityTest {
-    BasicKvStore prefs;
+    JsonKvStore defaultKvStore;
 
     @Rule
     public ActivityTestRule<SettingsActivity> activityRule =
@@ -38,11 +40,11 @@ public class SettingsActivityTest {
     public void setup() {
         Context context = InstrumentationRegistry.getTargetContext();
         String storeName = context.getPackageName() + "_preferences";
-        prefs = new BasicKvStore(context, storeName);
+        defaultKvStore = new JsonKvStore(context, storeName, new Gson());
     }
 
     @Test
-    public void setRecentUploadLimitTo100() {
+    public void setRecentUploadLimitTo123() {
         // Open "Use external storage" preference
         Espresso.onData(PreferenceMatchers.withKey("uploads"))
                 .inAdapterView(withId(android.R.id.list))
@@ -50,22 +52,22 @@ public class SettingsActivityTest {
 
         // Try setting it to 100
         Espresso.onView(withId(android.R.id.edit))
-                .perform(replaceText("100"));
+                .perform(replaceText("123"));
 
         // Click "OK"
         Espresso.onView(allOf(withId(android.R.id.button1), withText("OK")))
                 .perform(click());
 
-        // Check setting set to 100 in SharedPreferences
+        // Check setting set to 123 in SharedPreferences
         assertEquals(
-                100,
-                prefs.getInt(Prefs.UPLOADS_SHOWING, 0)
+                123,
+                defaultKvStore.getInt(Prefs.UPLOADS_SHOWING, 0)
         );
 
-        // Check displaying 100 in summary text
+        // Check displaying 123 in summary text
         Espresso.onData(PreferenceMatchers.withKey("uploads"))
                 .inAdapterView(withId(android.R.id.list))
                 .onChildView(withId(android.R.id.summary))
-                .check(matches(withText("100")));
+                .check(matches(withText("123")));
     }
 }
