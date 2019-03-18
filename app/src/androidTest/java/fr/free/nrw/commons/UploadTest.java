58fb8c6ec9c72fa011855d72cbad449c601542a5
Vivek Maskara <maskaravivek@gmail.com>
2019-03-18 10:59:07 +0530

    Generate an image file before testing uploads

--- app/src/androidTest/java/fr/free/nrw/commons/UploadTest.java
+++ app/src/androidTest/java/fr/free/nrw/commons/UploadTest.java
@@ -2,19 +2,29 @@ package fr.free.nrw.commons;
 
 import android.app.Activity;
 import android.app.Instrumentation.ActivityResult;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Environment;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
 
 import androidx.test.espresso.NoMatchingViewException;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.filters.LargeTest;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 import fr.free.nrw.commons.contributions.MainActivity;
@@ -38,8 +48,52 @@ import static org.hamcrest.core.AllOf.allOf;
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class UploadTest {
+//    @Rule
+//    public GrantPermissionRule permissionRule = GrantPermissionRule.grant(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
     @Rule
     public ActivityTestRule activityRule = new IntentsTestRule<>(MainActivity.class);
+
+    @Before
+    public void setup() {
+        saveToInternalStorage();
+    }
+
+    private void saveToInternalStorage() {
+        Bitmap bitmapImage = getRandomBitmap();
+        Context applicationContext = InstrumentationRegistry.getInstrumentation().getTargetContext().getApplicationContext();
+        ContextWrapper cw = new ContextWrapper(applicationContext);
+        // path to /data/data/yourapp/app_data/imageDir
+        File mypath = new File(Environment.getExternalStorageDirectory(), "image.jpg");
+
+        Timber.d("Filepath: %s", mypath.getPath());
+
+        Timber.d("Absolute Filepath: %s", mypath.getAbsolutePath());
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(mypath);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fos != null) {
+                    fos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private Bitmap getRandomBitmap() {
+        Random random = new Random();
+        Bitmap bitmap = Bitmap.createBitmap(200, 200, Bitmap.Config.ARGB_8888);
+        bitmap.eraseColor(random.nextInt(255));
+        return bitmap;
+    }
 
     @Test
     public void uploadTest() {
