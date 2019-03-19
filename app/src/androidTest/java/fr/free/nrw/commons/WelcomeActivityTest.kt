01688741dabde2e426b614d6305c1a0fb6d03c1c
vanshikaarora <vanshikaa937@gmail.com>
2019-03-19 14:46:03 +0530

    removed unused imports

--- app/src/androidTest/java/fr/free/nrw/commons/WelcomeActivityTest.kt
+++ app/src/androidTest/java/fr/free/nrw/commons/WelcomeActivityTest.kt
@@ -1,17 +1,13 @@
 package fr.free.nrw.commons
 
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.ViewAssertion
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.contrib.ViewPagerActions
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
-import androidx.viewpager.widget.ViewPager
-import fr.free.nrw.commons.explore.ViewPagerAdapter
 import fr.free.nrw.commons.utils.ConfigUtils
 import org.hamcrest.core.IsNot.not
 import org.junit.Rule
@@ -39,9 +35,10 @@ class WelcomeActivityTest {
                     .check(matches(not(isDisplayed())))
         }
     }
+
     @Test
-    fun ifSkipButtonClicked(){
-        if(!ConfigUtils.isBetaFlavour()){
+    fun ifSkipButtonClicked() {
+        if (!ConfigUtils.isBetaFlavour()) {
             onView(withId(R.id.finishTutorialButton))
                     .perform(ViewActions.click())
             assert(activityRule.activity.isDestroyed)
@@ -49,8 +46,8 @@ class WelcomeActivityTest {
     }
 
     @Test
-    fun ifSwiped(){
-        if (!ConfigUtils.isBetaFlavour()){
+    fun ifSwiped() {
+        if (!ConfigUtils.isBetaFlavour()) {
             onView(withId(R.id.welcomePager))
                     .perform(ViewActions.swipeLeft())
             assert(true)
@@ -61,8 +58,8 @@ class WelcomeActivityTest {
     }
 
     @Test
-    fun ifEndedTutorial(){
-        if(!ConfigUtils.isBetaFlavour()){
+    fun ifEndedTutorial() {
+        if (!ConfigUtils.isBetaFlavour()) {
             onView(withId(R.id.welcomePager))
                     .perform(ViewActions.swipeLeft())
                     .perform(ViewActions.swipeLeft())
