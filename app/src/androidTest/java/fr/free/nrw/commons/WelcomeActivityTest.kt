dcb5d5b6ff8e1f4ca4a6bca923aad59dbf10a35d
vanshikaarora <vanshikaa937@gmail.com>
2019-03-19 14:40:11 +0530

    minor

--- app/src/androidTest/java/fr/free/nrw/commons/WelcomeActivityTest.kt
+++ app/src/androidTest/java/fr/free/nrw/commons/WelcomeActivityTest.kt
@@ -1,13 +1,17 @@
 package fr.free.nrw.commons
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewAssertion
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.ViewPagerActions
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
+import androidx.viewpager.widget.ViewPager
+import fr.free.nrw.commons.explore.ViewPagerAdapter
 import fr.free.nrw.commons.utils.ConfigUtils
 import org.hamcrest.core.IsNot.not
 import org.junit.Rule
