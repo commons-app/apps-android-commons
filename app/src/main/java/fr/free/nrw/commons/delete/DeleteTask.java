697d771d9db309c0f191ef9a46d5dae499460afc
Noah Francis <noahwalugembe@gmail.com>
2019-03-20 11:58:10 +0300

    Improved Android notification after deletion nomination

--- app/src/main/java/fr/free/nrw/commons/delete/DeleteTask.java
+++ app/src/main/java/fr/free/nrw/commons/delete/DeleteTask.java
@@ -161,16 +161,25 @@ public class DeleteTask extends AsyncTask<Void, Integer, Boolean> {
 
     @Override
     protected void onPostExecute(Boolean result) {
+
+        //link to string resources
+        String title_resource = context.getResources().getString(R.string.nominating_for_Deletion);
+        String success_resource = context.getResources().getString(R.string.success);
+        String successfully_nominated_resource = context.getResources().getString(R.string.successfully_nominated);
+        String for_deletion_resource = context.getResources().getString(R.string.for_deletion);
+        String failed_resource = context.getResources().getString(R.string.failed);
+        String could_not_request_deletion_resource = context.getResources().getString(R.string.could_not_request_deletion);
+
         String message;
-        String title = "Nominating for Deletion";
+        String title = title_resource;
 
         if (result){
-            title += ": Success";
-            message = "Successfully nominated " + media.getDisplayTitle() + " deletion.";
+            title += ":"+success_resource;
+            message = successfully_nominated_resource + media.getDisplayTitle() + for_deletion_resource+".";
         }
         else {
-            title += ": Failed";
-            message = "Could not request deletion.";
+            title += ":"+failed_resource;
+            message = could_not_request_deletion_resource+".";
         }
 
         notificationBuilder.setDefaults(DEFAULT_ALL)
