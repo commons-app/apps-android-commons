97cd0f9d37e1f1d9a257802d26c28f1ee4234505
silky <silkypriya64@gmail.com>
2019-03-28 20:09:18 +0545

    Change class name

--- app/src/test/kotlin/fr/free/nrw/commons/utils/ImageUtilsTest.kt
+++ app/src/test/kotlin/fr/free/nrw/commons/utils/ImageUtilsTest.kt
@@ -3,7 +3,7 @@ package fr.free.nrw.commons.utils
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
-class imagecheck {
+class AlgoCheck {
     @Test
     fun validateImageForOkImage() {
         assertEquals(ImageUtils.IMAGE_OK, ImageUtils.checkIfImageIsTooDark("src/test/resources/fr/free/nrw/commons/utils/check1.jpg"))
