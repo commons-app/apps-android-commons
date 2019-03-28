8021577f07433a5cd29c79f7f363647ac05c32c8
shubham pinjwani <shubhampinjwani710@outlook.com>
2019-03-28 10:22:50 +0530

    commit

--- app/src/main/java/fr/free/nrw/commons/nearby/NearbyMapFragment.java
+++ app/src/main/java/fr/free/nrw/commons/nearby/NearbyMapFragment.java
@@ -552,7 +552,7 @@ public class NearbyMapFragment extends DaggerFragment {
             if (NearbyController.currentLocation != null) { // If our nearby markers are calculated at least once
 
                 if (searchThisAreaButton.getVisibility() == View.GONE && NetworkUtils.isInternetConnectionEstablished(getContext())) {
-                        searchThisAreaButton.setVisibility(View.VISIBLE);
+                    searchThisAreaButton.setVisibility(View.VISIBLE);
                 }
                 double distance = mapboxMap.getCameraPosition().target
                         .distanceTo(new LatLng(NearbyController.currentLocation.getLatitude()
