cea8d413d8b5eebe48e392fd6976ee20f6917c7d
Ashish <ashishkumar468@gmail.com>
2019-04-03 22:55:35 +0530

    Bug Fix #2845 * Show username instead of revision.content in image caption

--- app/src/main/java/fr/free/nrw/commons/review/ReviewActivity.java
+++ app/src/main/java/fr/free/nrw/commons/review/ReviewActivity.java
@@ -146,7 +146,7 @@ public class ReviewActivity extends AuthenticatedActivity {
                 .subscribe(revision -> {
                     reviewController.firstRevision = revision;
                     reviewPagerAdapter.updateFileInformation(fileName);
-                    ((TextView) imageCaption).setText(fileName + " is uploaded by: " + revision.content());
+                    ((TextView) imageCaption).setText(fileName + " is uploaded by: " + revision.getUser());
                     progressBar.setVisibility(View.GONE);
                 }));
         reviewPager.setCurrentItem(0);
