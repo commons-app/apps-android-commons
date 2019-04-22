569d931a2f73b1adbcd734a81a163d025d736dd0
szymon <szymon20000@gmail.com>
2019-04-22 19:35:27 +0200

    fixed height of ReviewViewPager

--- app/src/main/java/fr/free/nrw/commons/review/ReviewPagerAdapter.java
+++ app/src/main/java/fr/free/nrw/commons/review/ReviewPagerAdapter.java
@@ -1,7 +1,9 @@
 package fr.free.nrw.commons.review;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
@@ -9,6 +11,7 @@ import androidx.fragment.app.FragmentStatePagerAdapter;
 public class ReviewPagerAdapter extends FragmentStatePagerAdapter {
     ReviewImageFragment[] reviewImageFragments;
 
+    private int mCurrentPosition = -1;
 
     public ReviewPagerAdapter(FragmentManager fm) {
         super(fm);
@@ -43,6 +46,20 @@ public class ReviewPagerAdapter extends FragmentStatePagerAdapter {
         bundle.putInt("position", position);
         reviewImageFragments[position].setArguments(bundle);
         return reviewImageFragments[position];
+    }
+
+    // https://stackoverflow.com/a/32410274
+    @Override
+    public void setPrimaryItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        super.setPrimaryItem(container, position, object);
+        if (position != mCurrentPosition) {
+            Fragment fragment = (Fragment) object;
+            ReviewViewPager pager = (ReviewViewPager) container;
+            if (fragment != null && fragment.getView() != null) {
+                mCurrentPosition = position;
+                pager.measureCurrentView(fragment.getView());
+            }
+        }
     }
 
 }
--- app/src/main/java/fr/free/nrw/commons/review/ReviewViewPager.java
+++ app/src/main/java/fr/free/nrw/commons/review/ReviewViewPager.java
@@ -3,10 +3,15 @@ package fr.free.nrw.commons.review;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.View;
+
+import java.lang.ref.WeakReference;
 
 import androidx.viewpager.widget.ViewPager;
 
 public class ReviewViewPager extends ViewPager {
+
+    private WeakReference<View> mCurrentView;
 
     public ReviewViewPager(Context context) {
         super(context);
@@ -26,5 +31,29 @@ public class ReviewViewPager extends ViewPager {
     public boolean onTouchEvent(MotionEvent event) {
         // Never allow swiping to switch between pages
         return false;
+    }
+
+    // https://stackoverflow.com/a/32410274
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if (mCurrentView == null || mCurrentView.get() == null) {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+            return;
+        }
+        int height = 0;
+
+        View currentView = mCurrentView.get();
+
+        currentView.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+        int h = currentView.getMeasuredHeight();
+        if (h > height) height = h;
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    public void measureCurrentView(View currentView) {
+        mCurrentView = new WeakReference<>(currentView);
+        requestLayout();
     }
 }
