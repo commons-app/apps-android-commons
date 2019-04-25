9d660c197db4d6abd04b0ad75d8a86d6f752407f
Vitaly V. Pinchuk <vetal.978@gmail.com>
2019-04-25 17:57:44 +0300

    [WIP] FileMetadataUtilsTest added

---
+++ app/src/test/kotlin/fr/free/nrw/commons/upload/FileMetadataUtilsTest.kt
@@ -0,0 +1,19 @@
+package fr.free.nrw.commons.upload
+
+import androidx.exifinterface.media.ExifInterface.*
+import io.reactivex.Observable
+import junit.framework.Assert.assertTrue
+import org.junit.Test
+import java.util.*
+
+class FileMetadataUtilsTest {
+
+    @Test
+    fun getTagsFromPref() {
+        val author: Observable<String>? = FileMetadataUtils.getTagsFromPref("Author")
+        val authorRef: Observable<String>? = Observable.fromArray(TAG_ARTIST, TAG_CAMARA_OWNER_NAME)
+
+        assertTrue(Arrays.deepEquals(arrayOf(author?.toList()?.blockingGet()),
+                arrayOf(authorRef?.toList()?.blockingGet())))
+    }
+}
