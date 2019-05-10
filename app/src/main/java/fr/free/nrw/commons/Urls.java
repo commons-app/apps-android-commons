df475dc56b2cef073a6940dcb639357a4fc7c7b8
Madhur Gupta <30932899+madhurgupta10@users.noreply.github.com>
2019-05-10 15:05:20 +0530

    Fixed Typo

--- app/src/main/java/fr/free/nrw/commons/Urls.java
+++ app/src/main/java/fr/free/nrw/commons/Urls.java
@@ -7,7 +7,7 @@ class Urls {
     static final String CREDITS_URL = "https://github.com/commons-app/apps-android-commons/blob/master/CREDITS";
     static final String FAQ_URL = "https://github.com/commons-app/apps-android-commons/wiki/Frequently-Asked-Questions";
     static final String PLAY_STORE_URL = "https://play.google.com/store/apps/details?id=fr.free.nrw.commons";
-    static final String TRANSLATE_WIKI_URL = "https://translatewiki.net/w/i.php?title=Special:Translate&group=commons-android-public static final Strings&filter=%21translated&action=translate&language=";
+    static final String TRANSLATE_WIKI_URL = "https://translatewiki.net/w/i.php?title=Special:Translate&group=commons-android-strings&filter=%21translated&action=translate&language=";
     static final String FACEBOOK_WEB_URL = "https://www.facebook.com/1921335171459985";
     static final String FACEBOOK_APP_URL = "fb://page/1921335171459985";
     static final String FACEBOOK_PACKAGE_NAME = "com.facebook.katana";
