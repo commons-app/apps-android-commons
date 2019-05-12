48a550f5fd48d510f453ed0a33e4bf0a71884afe
neslihanturan <tur.neslihan@gmail.com>
2019-05-12 11:09:14 +0300

    Create presenters for map and list and implement user actions accordingly

---
+++ app/src/main/java/fr/free/nrw/commons/nearby/mvp/presenter/NearbyMapPresenter.java
@@ -0,0 +1,55 @@
+package fr.free.nrw.commons.nearby.mvp.presenter;
+
+import fr.free.nrw.commons.nearby.mvp.contract.NearbyMapContract;
+
+public class NearbyMapPresenter implements NearbyMapContract.UserActions {
+    @Override
+    public void searchThisArea() {
+
+    }
+
+    @Override
+    public void recenterMap() {
+
+    }
+
+    @Override
+    public void uploadImageGallery() {
+
+    }
+
+    @Override
+    public void uploadImageCamera() {
+
+    }
+
+    @Override
+    public void bookmarkItem() {
+
+    }
+
+    @Override
+    public void getDirections() {
+
+    }
+
+    @Override
+    public void seeWikidataItem() {
+
+    }
+
+    @Override
+    public void seeWikipediaArticle() {
+
+    }
+
+    @Override
+    public void seeCommonsFilePage() {
+
+    }
+
+    @Override
+    public void rotateScreen() {
+
+    }
+}
