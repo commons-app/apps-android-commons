24aa7a25cc482d0ebe5043a75abb8f69e5322adf
neslihanturan <tur.neslihan@gmail.com>
2019-05-13 08:34:21 +0300

    Add javadocs

---
+++ app/src/main/java/fr/free/nrw/commons/nearby/mvp/fragments/NearbyMapFragment.java
@@ -0,0 +1,26 @@
+package fr.free.nrw.commons.nearby.mvp.fragments;
+
+import fr.free.nrw.commons.di.CommonsDaggerSupportFragment;
+import fr.free.nrw.commons.nearby.mvp.contract.NearbyMapContract;
+
+public class NearbyMapFragment extends CommonsDaggerSupportFragment implements NearbyMapContract.View {
+    @Override
+    public void showSearchThisAreaButton() {
+
+    }
+
+    @Override
+    public void showInformationBottomSheet() {
+
+    }
+
+    @Override
+    public void showFABs() {
+
+    }
+
+    @Override
+    public void showPlaces() {
+
+    }
+}
