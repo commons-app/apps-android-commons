741a39858be91f0124c881d6f2583b27ed090525
szymon <szymon20000@gmail.com>
2019-06-01 13:27:20 +0200

    changed log dir

--- app/src/main/java/fr/free/nrw/commons/logging/LogUtils.java
+++ app/src/main/java/fr/free/nrw/commons/logging/LogUtils.java
@@ -2,6 +2,9 @@ package fr.free.nrw.commons.logging;
 
 import android.os.Environment;
 
+import java.io.File;
+
+import fr.free.nrw.commons.BuildConfig;
 import fr.free.nrw.commons.upload.FileUtils;
 import fr.free.nrw.commons.utils.ConfigUtils;
 
@@ -12,6 +15,18 @@ public final class LogUtils {
     private LogUtils() {
     }
 
+    private static final String PATH_BASE = Environment.getExternalStoragePublicDirectory(
+            Environment.getDataDirectory().getAbsolutePath()
+    ).getAbsolutePath();
+
+    private static final String APPLICATION_ID = BuildConfig.APPLICATION_ID;
+
+    private static final String APPLICATION_DIR = PATH_BASE + File.separator + APPLICATION_ID;
+
+    private static final String LOGS_DIR_BETA = "/logs/beta";
+
+    private static final String LOGS_DIR_PROD = "/logs/prod";
+
     /**
      * Returns the directory for saving logs on the device
      *
@@ -20,9 +35,9 @@ public final class LogUtils {
     public static String getLogDirectory() {
         String dirPath;
         if (ConfigUtils.isBetaFlavour()) {
-            dirPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/logs/beta";
+            dirPath = APPLICATION_DIR + LOGS_DIR_BETA;
         } else {
-            dirPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/logs/prod";
+            dirPath = APPLICATION_DIR + LOGS_DIR_PROD;
         }
 
         FileUtils.recursivelyCreateDirs(dirPath);
@@ -37,9 +52,9 @@ public final class LogUtils {
     public static String getLogZipDirectory() {
         String dirPath;
         if (ConfigUtils.isBetaFlavour()) {
-            dirPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/logs/beta/zip";
+            dirPath = APPLICATION_DIR + LOGS_DIR_BETA + File.separator + "zip";
         } else {
-            dirPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + "/logs/prod/zip";
+            dirPath = APPLICATION_DIR + LOGS_DIR_PROD + File.separator + "zip";
         }
 
         FileUtils.recursivelyCreateDirs(dirPath);
