fa0891dc70b0233e09b575a89db52a023b37454a
vanshikaarora <vanshikaa937@gmail.com>
2019-06-04 13:36:33 +0530

    removed time from filename

--- app/src/main/java/fr/free/nrw/commons/upload/UploadModel.java
+++ app/src/main/java/fr/free/nrw/commons/upload/UploadModel.java
@@ -194,7 +194,7 @@ public class UploadModel {
         return Observable.fromIterable(items).map(item ->
         {
             Contribution contribution = new Contribution(item.mediaUri, null,
-                    item.getFileName() + getCurrentTimeStamp(),
+                    item.getFileName(),
                     UploadMediaDetail.formatList(item.uploadMediaDetails), -1,
                     null, null, sessionManager.getAuthorName(),
                     CommonsApplication.DEFAULT_EDIT_SUMMARY, item.gpsCoords.getCoords());
