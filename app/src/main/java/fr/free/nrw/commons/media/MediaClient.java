5008abc91b3b9dcbdc8fe444bc351d0cf4cf17d1
ilgazer <er.ilgaz@gmail.com>
2019-07-05 20:46:29 +0300

    Migrated OkHttpJsonApi#getMediaList partially to MediaClient#getCategoryImages

--- app/src/main/java/fr/free/nrw/commons/media/MediaClient.java
+++ app/src/main/java/fr/free/nrw/commons/media/MediaClient.java
@@ -1,10 +1,20 @@
 package fr.free.nrw.commons.media;
 
 
+import org.wikipedia.dataclient.mwapi.MwQueryResponse;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import fr.free.nrw.commons.Media;
+import io.reactivex.Observable;
 import io.reactivex.Single;
+import io.reactivex.functions.BiConsumer;
 
 /**
  * Media Client to handle custom calls to Commons MediaWiki APIs
@@ -14,9 +24,13 @@ public class MediaClient {
 
     private final MediaInterface mediaInterface;
 
+    //OkHttpJsonApiClient used JsonKvStore for this. I don't know why.
+    private Map<String, Map<String, String>> continuationStore;
+
     @Inject
     public MediaClient(MediaInterface mediaInterface) {
         this.mediaInterface = mediaInterface;
+        this.continuationStore = new HashMap<>();
     }
 
     /**
@@ -42,5 +56,30 @@ public class MediaClient {
                 .map(mwQueryResponse -> mwQueryResponse
                         .query().allImages().size() > 0)
                 .singleOrError();
+    }
+
+    /**
+     * This method takes the category as input and returns a list of  Media objects filtered using image generator query
+     * It uses the generator query API to get the images searched using a query, 10 at a time.
+     * @param category the search category. Must start with "Category:"
+     * @return
+     */
+    public Single<List<Media>> getCategoryImages(String category){
+        Observable<MwQueryResponse> response;
+        if(continuationStore.containsKey(category))
+            response=mediaInterface.getCategoryMediaList(category, 10, continuationStore.get(category));
+        else
+            response=mediaInterface.getCategoryMediaList(category, 10);
+        return response.flatMap(mwQueryResponse->{
+            if (null == mwQueryResponse
+                    || null == mwQueryResponse.query()
+                    || null == mwQueryResponse.query().pages()) {
+                return Observable.empty();
+            }
+            continuationStore.put(category, mwQueryResponse.continuation());
+            return Observable.fromIterable(mwQueryResponse.query().pages());
+        })
+                .map(Media::from)
+                .collect(ArrayList<Media>::new, List::add);
     }
 }
--- app/src/main/java/fr/free/nrw/commons/media/MediaInterface.java
+++ app/src/main/java/fr/free/nrw/commons/media/MediaInterface.java
@@ -1,10 +1,14 @@
 package fr.free.nrw.commons.media;
 
+import org.jetbrains.annotations.NotNull;
 import org.wikipedia.dataclient.mwapi.MwQueryResponse;
+
+import java.util.Map;
 
 import io.reactivex.Observable;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
 
 /**
  * Interface for interacting with Commons media related APIs
@@ -12,6 +16,7 @@ import retrofit2.http.Query;
 public interface MediaInterface {
     /**
      * Checks if a page exists or not.
+     *
      * @param title the title of the page to be checked
      * @return
      */
@@ -20,9 +25,40 @@ public interface MediaInterface {
 
     /**
      * Check if file exists
+     *
      * @param aisha1 the SHA of the media file to be checked
      * @return
      */
     @GET("w/api.php?action=query&format=json&formatversion=2&list=allimages")
     Observable<MwQueryResponse> checkFileExistsUsingSha(@Query("aisha1") String aisha1);
+
+    /**
+     * This method retrieves a list of Media objects filtered using image generator query
+     *
+     * @param title     the category name. Must start with "Category:"
+     * @param itemLimit how many images are returned
+     * @return
+     */
+    @GET("w/api.php?action=query&format=json&formatversion=2&generator=categorymembers" +
+            "&gcmtype=file&gcmsort=timestamp&gcmdir=desc&gcmlimit=10" +
+            "&prop=imageinfo&iiprop=url|extmetadata&iiurlwidth=640" +
+            "&iiextmetadatafilter=DateTime|Categories|GPSLatitude|GPSLongitude|ImageDescription|DateTimeOriginal" +
+            "|Artist|LicenseShortName|LicenseUrl")
+    Observable<MwQueryResponse> getCategoryMediaList(@Query("gcmtitle") String title, @Query("gcmlimit") int itemLimit);
+
+    /**
+     * This method retrieves a list of Media objects filtered using image generator query
+     *
+     * @param title        the category name. Must start with "Category:"
+     * @param itemLimit    how many images are returned
+     * @param continuation the continuation string from the previous query
+     * @return
+     */
+    @GET("w/api.php?action=query&format=json&formatversion=2&generator=categorymembers" +
+            "&gcmtype=file&gcmsort=timestamp&gcmdir=desc" +
+            "&prop=imageinfo&iiprop=url|extmetadata&iiurlwidth=640" +
+            "&iiextmetadatafilter=DateTime|Categories|GPSLatitude|GPSLongitude|ImageDescription|DateTimeOriginal" +
+            "|Artist|LicenseShortName|LicenseUrl")
+    Observable<MwQueryResponse> getCategoryMediaList(@Query("gcmtitle") String title, @Query("gcmlimit") int itemLimit, @QueryMap Map<String, String> continuation);
+
 }
