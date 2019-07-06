97122296ddba1e68b10f0974b76b3db88630a3f2
Ilgaz Er <er.ilgaz@gmail.com>
2019-07-06 14:25:57 +0300

    OkHttpJsonApi#getMediaList migrated to retrofit (#3054)
    * Migrated OkHttpJsonApi#getMediaList partially to MediaClient#getCategoryImages
    
    * Migrated rest of OkHttpJsonApi#getMediaList functionality to MediaClient#getCategoryImages
    
    * Removed unused code and tests
    
    * Fixed small bug
    
    * Added tests
    
    * Removed unused CategoryImageController

--- app/src/main/java/fr/free/nrw/commons/media/MediaClient.java
+++ app/src/main/java/fr/free/nrw/commons/media/MediaClient.java
@@ -1,9 +1,19 @@
 package fr.free.nrw.commons.media;
 
 
+import org.wikipedia.dataclient.mwapi.MwQueryResponse;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import fr.free.nrw.commons.Media;
+import io.reactivex.Observable;
 import io.reactivex.Single;
 
 /**
@@ -14,9 +24,13 @@ public class MediaClient {
 
     private final MediaInterface mediaInterface;
 
+    //OkHttpJsonApiClient used JsonKvStore for this. I don't know why.
+    private Map<String, Map<String, String>> continuationStore;
+
     @Inject
     public MediaClient(MediaInterface mediaInterface) {
         this.mediaInterface = mediaInterface;
+        this.continuationStore = new HashMap<>();
     }
 
     /**
@@ -43,4 +57,51 @@ public class MediaClient {
                         .query().allImages().size() > 0)
                 .singleOrError();
     }
+
+    /**
+     * This method takes the category as input and returns a list of  Media objects filtered using image generator query
+     * It uses the generator query API to get the images searched using a query, 10 at a time.
+     *
+     * @param category the search category. Must start with "Category:"
+     * @return
+     */
+    public Single<List<Media>> getMediaListFromCategory(String category) {
+        return responseToMediaList(
+                continuationStore.containsKey("category_" + category) ?
+                        mediaInterface.getMediaListFromCategory(category, 10, continuationStore.get("category_" + category)) : //if true
+                        mediaInterface.getMediaListFromCategory(category, 10, Collections.emptyMap()),
+                "category_" + category); //if false
+
+    }
+
+    /**
+     * This method takes a keyword as input and returns a list of  Media objects filtered using image generator query
+     * It uses the generator query API to get the images searched using a query, 10 at a time.
+     *
+     * @param keyword the search keyword
+     * @return
+     */
+    public Single<List<Media>> getMediaListFromSearch(String keyword) {
+        return responseToMediaList(
+                continuationStore.containsKey("search_" + keyword) ?
+                        mediaInterface.getMediaListFromSearch(keyword, 10, continuationStore.get("search_" + keyword)) : //if true
+                        mediaInterface.getMediaListFromSearch(keyword, 10, Collections.emptyMap()), //if false
+                "search_" + keyword);
+
+    }
+
+    private Single<List<Media>> responseToMediaList(Observable<MwQueryResponse> response, String key) {
+        return response.flatMap(mwQueryResponse -> {
+            if (null == mwQueryResponse
+                    || null == mwQueryResponse.query()
+                    || null == mwQueryResponse.query().pages()) {
+                return Observable.empty();
+            }
+            continuationStore.put(key, mwQueryResponse.continuation());
+            return Observable.fromIterable(mwQueryResponse.query().pages());
+        })
+                .map(Media::from)
+                .collect(ArrayList<Media>::new, List::add);
+    }
+
 }
