c216bf55bacea48fb0d857b58edbfec51f6168ab
ilgazer <er.ilgaz@gmail.com>
2019-07-24 02:16:11 +0300

    Migrated ContributionsSyncAdapter to UserClient Fixed sync related bugs

--- app/src/main/java/fr/free/nrw/commons/mwapi/UserClient.java
+++ app/src/main/java/fr/free/nrw/commons/mwapi/UserClient.java
@@ -1,6 +1,14 @@
 package fr.free.nrw.commons.mwapi;
 
+import org.wikipedia.dataclient.mwapi.MwQueryLogEvent;
+import org.wikipedia.dataclient.mwapi.MwQueryResponse;
+import org.wikipedia.dataclient.mwapi.MwQueryResult;
+
+import java.util.Collections;
+
 import javax.inject.Inject;
+
+import io.reactivex.Observable;
 
 public class UserClient {
     private final UserInterface userInterface;
@@ -8,5 +16,17 @@ public class UserClient {
     @Inject
     public UserClient(UserInterface userInterface) {
         this.userInterface = userInterface;
+    }
+
+    public Observable<MwQueryLogEvent> logEvents(String user) {
+        try {
+            return userInterface.getUserLogEvents(user, Collections.emptyMap())
+                    .map(MwQueryResponse::query)
+                    .map(MwQueryResult::logevents)
+                    .flatMap(Observable::fromIterable);
+        } catch (Throwable throwable) {
+            return Observable.empty();
+        }
+
     }
 }
