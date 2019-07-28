c5ee65275c9c8ac7f3e4ba1df7f49d751a718cb0
albendz <alicia.bendz@gmail.com>
2019-07-28 16:57:36 -0700

    remove debug code from dialog custom view parent investigation

--- app/src/main/java/fr/free/nrw/commons/utils/DialogUtil.java
+++ app/src/main/java/fr/free/nrw/commons/utils/DialogUtil.java
@@ -5,7 +5,6 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.view.View;
-import android.view.ViewGroup;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -142,10 +141,6 @@ public class DialogUtil {
             if (onNegativeBtnClick != null) {
                 onNegativeBtnClick.run();
             }
-        });
-
-        builder.setOnDismissListener((DialogInterface dialogInterface) -> {
-            dialogInterface.dismiss();
         });
 
         AlertDialog dialog = builder.create();
