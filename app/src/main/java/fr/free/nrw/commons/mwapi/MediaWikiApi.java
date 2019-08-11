bb273eb22d11513a4bd6808d33c85e9d428d3d7f
Ilgaz Er <er.ilgaz@gmail.com>
2019-08-11 16:46:11 +0300

    Migrated logEvents to retrofit (#3087)
    * Switched wikimedia-android-data-client for new features
    
    * Added UserCLient and UserInterface
    
    * Migrated ContributionsSyncAdapter to UserClient
    Fixed sync related bugs
    
    * Removed unused code
    
    * Removed unused code
    
    * Updated wikimedia-android-data-client to new version

--- app/src/main/java/fr/free/nrw/commons/mwapi/MediaWikiApi.java
+++ app/src/main/java/fr/free/nrw/commons/mwapi/MediaWikiApi.java
@@ -14,12 +14,6 @@ public interface MediaWikiApi {
     @NonNull
     Single<MediaResult> fetchMediaByFilename(String filename);
 
-    @Nullable
-    String revisionsByFilename(String filename) throws IOException;
-
-    @NonNull
-    LogEventResult logEvents(String user, String lastModified, String queryContinue, int limit) throws IOException;
-
     boolean isUserBlockedFromCommons();
 
     void logout();
