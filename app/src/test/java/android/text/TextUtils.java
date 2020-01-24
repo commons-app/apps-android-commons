787c8ca6613a406305028d7470ba77ac697fdb6e
kbhardwaj123 <kshitij256kencryption@gmail.com>
2020-01-25 02:11:24 +0530

    TextUtils: add more methods to mock for testing

--- app/src/test/java/android/text/TextUtils.java
+++ app/src/test/java/android/text/TextUtils.java
@@ -58,4 +58,25 @@ public class TextUtils {
         return type == Character.PARAGRAPH_SEPARATOR || type == Character.LINE_SEPARATOR
                 || codePoint == 10;
     }
+
+    /**
+     * Returns whether the given CharSequence contains any printable characters.
+     */
+    public static boolean isGraphic(CharSequence str) {
+        final int len = str.length();
+        for (int cp, i=0; i<len; i+=Character.charCount(cp)) {
+            cp = Character.codePointAt(str, i);
+            int gc = Character.getType(cp);
+            if (gc != Character.CONTROL
+                    && gc != Character.FORMAT
+                    && gc != Character.SURROGATE
+                    && gc != Character.UNASSIGNED
+                    && gc != Character.LINE_SEPARATOR
+                    && gc != Character.PARAGRAPH_SEPARATOR
+                    && gc != Character.SPACE_SEPARATOR) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
