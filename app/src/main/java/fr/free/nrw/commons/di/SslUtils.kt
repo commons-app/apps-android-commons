04c3f7130a86ed09f89c173c7f205c9f6a7b065e
Ashish Kumar <ashishkumar468@gmail.com>
2020-01-27 01:41:26 +0530

    removed unused assets

--- app/src/main/java/fr/free/nrw/commons/di/SslUtils.kt
+++ app/src/main/java/fr/free/nrw/commons/di/SslUtils.kt
@@ -1,58 +1,15 @@
 package fr.free.nrw.commons.di
 
-import android.content.Context
-import android.util.Log
 import java.security.KeyManagementException
-import java.security.KeyStore
 import java.security.NoSuchAlgorithmException
-import java.security.SecureRandom
-import java.security.cert.Certificate
 import java.security.cert.CertificateException
-import java.security.cert.CertificateFactory
 import java.security.cert.X509Certificate
-import javax.net.ssl.*
+import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLSocketFactory
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 object SslUtils {
-
-    fun getSslContextForCertificateFile(context: Context, fileName: String): SSLContext {
-        try {
-            val keyStore = getKeyStore(context, fileName)
-            val sslContext = SSLContext.getInstance("SSL")
-            val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
-            trustManagerFactory.init(keyStore)
-            sslContext.init(null, trustManagerFactory.trustManagers, SecureRandom())
-            return sslContext
-        } catch (e: Exception) {
-            val msg = "Error during creating SslContext for certificate from assets"
-            e.printStackTrace()
-            throw RuntimeException(msg)
-        }
-    }
-
-    private fun getKeyStore(context: Context, fileName: String): KeyStore? {
-        var keyStore: KeyStore? = null
-        try {
-            val assetManager = context.assets
-            val cf = CertificateFactory.getInstance("X.509")
-            val caInput = assetManager.open(fileName)
-            val ca: Certificate
-            try {
-                ca = cf.generateCertificate(caInput)
-                Log.d("SslUtilsAndroid", "ca=" + (ca as X509Certificate).subjectDN)
-            } finally {
-                caInput.close()
-            }
-
-            val keyStoreType = KeyStore.getDefaultType()
-            keyStore = KeyStore.getInstance(keyStoreType)
-            keyStore!!.load(null, null)
-            keyStore.setCertificateEntry("ca", ca)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-
-        return keyStore
-    }
 
     fun getTrustAllHostsSSLSocketFactory(): SSLSocketFactory? {
         try {
