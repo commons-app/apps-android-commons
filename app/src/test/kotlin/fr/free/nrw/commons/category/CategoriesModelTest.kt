86e443375755bcbe19767b8236a058a591e4cf9d
kbhardwaj123 <kshitij256kencryption@gmail.com>
2020-02-03 09:44:23 +0530

    CategoriesModelTest: make method name meaningful

--- app/src/test/kotlin/fr/free/nrw/commons/category/CategoriesModelTest.kt
+++ app/src/test/kotlin/fr/free/nrw/commons/category/CategoriesModelTest.kt
@@ -24,7 +24,7 @@ class CategoriesModelTest {
     internal lateinit var gson: Gson
 
     @Spy
-    internal lateinit var categoryItemForMethodA: CategoryItem
+    internal lateinit var categoryItemForSubstringSearch: CategoryItem
 
     @Mock
     internal var categoryDao: CategoryDao? = null
@@ -39,7 +39,7 @@ class CategoriesModelTest {
     @Throws(Exception::class)
     fun setUp() {
         gson = Gson()
-        categoryItemForMethodA = CategoryItem("",false)
+        categoryItemForSubstringSearch = CategoryItem("",false)
         MockitoAnnotations.initMocks(this)
     }
 
@@ -67,12 +67,13 @@ class CategoriesModelTest {
 
     /**
     * For testing the substring search algorithm for Categories search
-    * To be more precise it tests the Method A which has been described 
+    * To be more precise it tests the In Between substring( ex: searching `atte` 
+    * will give search suggestions: `Latte`, `Iced latte` e.t.c) which has been described 
     * on github repo wiki: 
     * https://github.com/commons-app/apps-android-commons/wiki/Category-suggestions-(readme)#user-content-3-category-search-when-typing-in-the-search-field-has-been-made-more-flexible
     */
     @Test
-    fun searchAllFoundCaseTestForMethodA() {
+    fun searchAllFoundCaseTestForSubstringSearch() {
         val mockDaoList = mutableListOf<String>("")
         val mwQueryPage = Mockito.mock(MwQueryPage::class.java)
         Mockito.`when`(mwQueryPage.title()).thenReturn("Category:Test")
@@ -83,8 +84,8 @@ class CategoriesModelTest {
         Mockito.`when`(context!!.getSharedPreferences("",0))
                 .thenReturn(null)
         val directKvStore = Mockito.spy(JsonKvStore(context,"",gson))
-        val categoriesModelForMethodA = Mockito.spy(CategoriesModel(categoryClient,categoryDao,directKvStore))
-        Mockito.doReturn(Observable.just(categoryItemForMethodA)).`when`(categoriesModelForMethodA).gpsCategories()
+        val categoriesModelForSubstringSearch = Mockito.spy(CategoriesModel(categoryClient,categoryDao,directKvStore))
+        Mockito.doReturn(Observable.just(categoryItemForSubstringSearch)).`when`(categoriesModelForSubstringSearch).gpsCategories()
         Mockito.`when`(context!!.getSharedPreferences("",0))
                 .thenReturn(null)
         Mockito.`when`(categoryInterface!!.searchCategories(ArgumentMatchers.anyString(), ArgumentMatchers.anyInt(), ArgumentMatchers.anyInt()))
@@ -95,10 +96,10 @@ class CategoriesModelTest {
                 .thenReturn(Observable.just(mockResponse))
 
         // Checking if both return "Test"
-        val actualCategoryName = categoriesModelForMethodA!!.searchAll(null, listOf<String>("tes")).blockingLast()
+        val actualCategoryName = categoriesModelForSubstringSearch!!.searchAll(null, listOf<String>("tes")).blockingLast()
         assertEquals("Test",actualCategoryName.getName())
 
-        val actualCategoryNameCaps = categoriesModelForMethodA!!.searchAll(null, listOf<String>("Tes")).blockingLast()
+        val actualCategoryNameCaps = categoriesModelForSubstringSearch!!.searchAll(null, listOf<String>("Tes")).blockingLast()
         assertEquals("Test",actualCategoryNameCaps.getName())
     }
 }
