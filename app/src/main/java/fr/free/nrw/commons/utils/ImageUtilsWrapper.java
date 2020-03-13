ef7e476976e6e3e22f7fc35a5df876f28c1faf11
Sean Mac Gillicuddy <seantheappdev@gmail.com>
2020-03-13 10:37:14 +0000

    #3493 App freezes for 15 seconds when you press Next in UploadMediaDetailsFragment - replace kotlin with java

---
+++ app/src/main/java/fr/free/nrw/commons/utils/ImageUtilsWrapper.java
@@ -0,0 +1,30 @@
+package fr.free.nrw.commons.utils;
+
+import fr.free.nrw.commons.location.LatLng;
+import io.reactivex.Single;
+import io.reactivex.schedulers.Schedulers;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+public class ImageUtilsWrapper {
+
+  @Inject
+  public ImageUtilsWrapper() {
+
+  }
+
+  public Single<Integer> checkIfImageIsTooDark(String bitmapPath) {
+    return Single.fromCallable(() -> ImageUtils.checkIfImageIsTooDark(bitmapPath))
+        .subscribeOn(Schedulers.computation());
+  }
+
+  public Single<Integer> checkImageGeolocationIsDifferent(String geolocationOfFileString,
+      LatLng latLng) {
+    return Single.fromCallable(
+        () -> ImageUtils.checkImageGeolocationIsDifferent(geolocationOfFileString, latLng))
+        .subscribeOn(Schedulers.computation())
+        .map(isDifferent -> isDifferent ? ImageUtils.IMAGE_GEOLOCATION_DIFFERENT
+            : ImageUtils.IMAGE_OK);
+  }
+}
