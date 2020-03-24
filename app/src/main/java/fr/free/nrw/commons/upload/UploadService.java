2854f6af02378247c9c4ef3db13b9bb488b12f18
Sean Mac Gillicuddy <seantheappdev@gmail.com>
2020-03-24 13:48:15 +0000

    Fix method invocations

--- app/src/main/java/fr/free/nrw/commons/upload/UploadService.java
+++ app/src/main/java/fr/free/nrw/commons/upload/UploadService.java
@@ -286,23 +286,26 @@ public class UploadService extends HandlerService<Contribution> {
                         showFailedNotification(contribution);
                     } else {
                         String canonicalFilename = "File:" + uploadResult.getFilename();
+                        final String wikiDataEntityId = contribution.getWikiDataEntityId();
                         Timber.d("Contribution upload success. Initiating Wikidata edit for entity id %s",
-                                contribution.getWikiDataEntityId());
+                            wikiDataEntityId);
                         // to perform upload of depictions we pass on depiction entityId of the selected depictions to the wikidataEditService
                         final String p18Value = contribution.getP18Value();
+                        final String wikiItemName = contribution.getWikiItemName();
                         if (contribution.getDepictionsEntityIds() != null) {
-                            for (String s : contribution.getDepictionsEntityIds()) {
-                                wikidataEditService.createClaimWithLogging(s, canonicalFilename,
-                                    p18Value);
+                            for (String depictionEntityId : contribution.getDepictionsEntityIds()) {
+                                wikidataEditService.createClaimWithLogging(depictionEntityId,
+                                    wikiItemName, canonicalFilename, p18Value);
                             }
                         }
                         Timber.d("Contribution upload success. Initiating Wikidata edit for"
                                 + " entity id %s if necessary (if P18 is null). P18 value is %s",
-                            contribution.getWikiDataEntityId(), contribution.getP18Value());
-                        wikidataEditService.createClaimWithLogging(contribution.getWikiDataEntityId(), contribution.getWikiItemName(), canonicalFilename, contribution.getP18Value());
+                            wikiDataEntityId, p18Value);
+                        wikidataEditService.createClaimWithLogging(
+                            wikiDataEntityId, wikiItemName, canonicalFilename,p18Value);
 
-                        wikidataEditService.createLabelforWikidataEntity(contribution.getWikiDataEntityId(), canonicalFilename,
-                                (Map) contribution.getCaptions());
+                        wikidataEditService.createLabelforWikidataEntity(
+                            wikiDataEntityId, canonicalFilename, contribution.getCaptions());
                         contribution.setFilename(canonicalFilename);
                         contribution.setImageUrl(uploadResult.getImageinfo().getOriginalUrl());
                         contribution.setState(Contribution.STATE_COMPLETED);
