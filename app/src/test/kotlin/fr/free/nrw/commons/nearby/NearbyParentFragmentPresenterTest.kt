8b1d62cdb1cccf74c7649071f63b83722e0ff150
Neslihan <tur.neslihan@gmail.com>
2020-04-01 15:32:18 +0300

    Add tests to check if searchthisarea button action and checkbox actions are added after initialize test

--- app/src/test/kotlin/fr/free/nrw/commons/nearby/NearbyParentFragmentPresenterTest.kt
+++ app/src/test/kotlin/fr/free/nrw/commons/nearby/NearbyParentFragmentPresenterTest.kt
@@ -1,4 +1,42 @@
 package fr.free.nrw.commons.nearby
 
+import com.nhaarman.mockitokotlin2.verify
+import fr.free.nrw.commons.bookmarks.locations.BookmarkLocationsDao
+import fr.free.nrw.commons.nearby.contract.NearbyParentFragmentContract
+import fr.free.nrw.commons.nearby.presenter.NearbyParentFragmentPresenter
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+
+
+/**
+ * The unit test class for NearbyParentFragmentPresenter
+ */
 class NearbyParentFragmentPresenterTest {
+    @Mock
+    internal lateinit var nearbyParentFragmentView: NearbyParentFragmentContract.View
+    @Mock
+    internal lateinit var bookmarkLocationsDao: BookmarkLocationsDao
+    private lateinit var nearbyPresenter: NearbyParentFragmentPresenter
+    /**
+     * initial setup
+     */
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        nearbyPresenter = NearbyParentFragmentPresenter(bookmarkLocationsDao)
+        nearbyPresenter.attachView(nearbyParentFragmentView)
+    }
+
+    /**
+     * Tests if initializeNearbyMapOperations called
+     */
+    @Test
+    fun testInitializeNearbyMapOperations() {
+        nearbyPresenter.initializeMapOperations()
+        verify(nearbyParentFragmentView).addSearchThisAreaButtonAction()
+        verify(nearbyParentFragmentView).setCheckBoxAction()
+    }
 }
