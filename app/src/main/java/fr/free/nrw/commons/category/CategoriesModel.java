e459953a018b9df0c937bc124606488995fbaeb4
kbhardwaj123 <kshitij256kencryption@gmail.com>
2020-04-02 19:31:46 +0530

    CategoriesModel: fix searchAll method to provide prefix results along with the case-insensitive ones

--- app/src/main/java/fr/free/nrw/commons/category/CategoriesModel.java
+++ app/src/main/java/fr/free/nrw/commons/category/CategoriesModel.java
@@ -125,8 +125,11 @@ public class CategoriesModel{
         //otherwise, search API for matching categories
         //term passed as lower case to make search case-insensitive(taking only lower case for everything)
         return categoryClient
-                .searchCategoriesForPrefix(term.toLowerCase(), SEARCH_CATS_LIMIT)
-                .map(name -> new CategoryItem(name, false));
+                .searchCategoriesForPrefix(term, SEARCH_CATS_LIMIT)
+                .map(name -> new CategoryItem(name, false))
+                .concatWith(
+                    getTitleCategories(term)
+                );
     }
 
 
@@ -189,7 +192,7 @@ public class CategoriesModel{
      * @return
      */
     private Observable<CategoryItem> getTitleCategories(String title) {
-        return categoryClient.searchCategories(title.toLowerCase(), SEARCH_CATS_LIMIT)
+        return categoryClient.searchCategories(title, SEARCH_CATS_LIMIT)
                 .map(name -> new CategoryItem(name, false));
     }
 
