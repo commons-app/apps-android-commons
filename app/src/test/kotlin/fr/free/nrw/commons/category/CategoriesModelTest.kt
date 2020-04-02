2b05fce5a51ce5b1bee1ea6e6072cb0de3682017
kbhardwaj123 <kshitij256kencryption@gmail.com>
2020-04-02 20:30:36 +0530

    CategoriesModelTest: removing the prefix test since the prefix search api is not case in-sensitive and they were getting passed because the page APIs ignore the first letter

--- app/src/test/kotlin/fr/free/nrw/commons/category/CategoriesModelTest.kt
+++ app/src/test/kotlin/fr/free/nrw/commons/category/CategoriesModelTest.kt
@@ -43,28 +43,6 @@ class CategoriesModelTest {
         MockitoAnnotations.initMocks(this)
     }
 
-    // Test Case for verifying that Categories search (MW api calls) are case-insensitive
-    @Test
-    fun searchAllFoundCaseTest() {
-       val mwQueryPage = Mockito.mock(MwQueryPage::class.java)
-        Mockito.`when`(mwQueryPage.title()).thenReturn("Category:Test")
-        val mwQueryResult = Mockito.mock(MwQueryResult::class.java)
-        Mockito.`when`(mwQueryResult.pages()).thenReturn(listOf(mwQueryPage))
-        val mockResponse = Mockito.mock(MwQueryResponse::class.java)
-        Mockito.`when`(mockResponse.query()).thenReturn(mwQueryResult)
-        val categoriesModel: CategoriesModel = CategoriesModel(categoryClient,null,null)
-
-        Mockito.`when`(categoryInterface!!.searchCategoriesForPrefix(ArgumentMatchers.anyString(), ArgumentMatchers.anyInt(), ArgumentMatchers.anyInt()))
-                .thenReturn(Observable.just(mockResponse))
-
-        // Checking if both return "Test"
-        val actualCategoryName = categoriesModel!!.searchAll("tes",null).blockingFirst()
-        assertEquals("Test", actualCategoryName.getName())
-        
-        val actualCategoryNameCaps = categoriesModel!!.searchAll("Tes",null).blockingFirst()
-        assertEquals("Test", actualCategoryNameCaps.getName())
-    }
-
     /**
     * For testing the substring search algorithm for Categories search
     * To be more precise it tests the In Between substring( ex: searching `atte` 
