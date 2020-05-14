e597a7c96fa359d85cfdd3ed82bca370271e290a
Seán Mac Gillicuddy <seantheappdev@gmail.com>
2020-05-14 15:32:51 +0100

    #3745 Upload depiction suggestions not following text input - on upstream error emit empty list (#3751)

--- app/src/main/java/fr/free/nrw/commons/upload/structure/depictions/DepictModel.kt
+++ app/src/main/java/fr/free/nrw/commons/upload/structure/depictions/DepictModel.kt
@@ -3,7 +3,9 @@ package fr.free.nrw.commons.upload.structure.depictions
 import fr.free.nrw.commons.explore.depictions.DepictsClient
 import fr.free.nrw.commons.nearby.Place
 import io.reactivex.Flowable
+import io.reactivex.Single
 import io.reactivex.processors.BehaviorProcessor
+import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -15,7 +17,6 @@ class DepictModel @Inject constructor(private val depictsClient: DepictsClient) 
 
     val nearbyPlaces: BehaviorProcessor<List<Place>> = BehaviorProcessor.createDefault(emptyList())
 
-
     companion object {
         private const val SEARCH_DEPICTS_LIMIT = 25
     }
@@ -24,23 +25,24 @@ class DepictModel @Inject constructor(private val depictsClient: DepictsClient) 
      * Search for depictions
      */
     fun searchAllEntities(query: String): Flowable<List<DepictedItem>> {
-        if (query.isBlank()) {
-            return nearbyPlaces.switchMap { places: List<Place> ->
-                depictsClient.getEntities(
-                    places.mapNotNull { it.wikiDataEntityId }.joinToString("|")
-                )
+        return if (query.isBlank())
+            nearbyPlaces.switchMap { places: List<Place> ->
+                depictsClient.getEntities(places.toIds())
                     .map {
-                        it.entities()!!.values.mapIndexed { index, entity ->
-                            DepictedItem(entity, places[index])
-                        }
-                    }.toFlowable()
+                        it.entities()
+                            .values
+                            .mapIndexed { index, entity -> DepictedItem(entity, places[index]) }
+                    }
+                    .onErrorResumeWithEmptyList()
+                    .toFlowable()
             }
-        }
-        return networkItems(query)
+        else
+            networkItems(query)
     }
 
     private fun networkItems(query: String): Flowable<List<DepictedItem>> {
         return depictsClient.searchForDepictions(query, SEARCH_DEPICTS_LIMIT, 0)
+            .onErrorResumeWithEmptyList()
             .toFlowable()
     }
 
@@ -48,4 +50,10 @@ class DepictModel @Inject constructor(private val depictsClient: DepictsClient) 
         nearbyPlaces.offer(emptyList())
     }
 
+}
+
+private fun List<Place>.toIds() = mapNotNull { it.wikiDataEntityId }.joinToString("|")
+
+private fun <T> Single<List<T>>.onErrorResumeWithEmptyList() = onErrorResumeNext { t: Throwable ->
+    Single.just(emptyList<T>()).also { Timber.e(t) }
 }
