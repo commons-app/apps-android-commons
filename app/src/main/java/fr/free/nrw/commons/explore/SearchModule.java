c216fdf0d4399b78df9cdad2bea57b18e02d4b1d
Seán Mac Gillicuddy <seantheappdev@gmail.com>
2020-05-28 12:10:04 +0100

    #3756 Convert SearchDepictionsFragment to use Pagination (#3758)

--- app/src/main/java/fr/free/nrw/commons/explore/SearchModule.java
+++ app/src/main/java/fr/free/nrw/commons/explore/SearchModule.java
@@ -2,6 +2,10 @@ package fr.free.nrw.commons.explore;
 
 import dagger.Binds;
 import dagger.Module;
+import dagger.Provides;
+import fr.free.nrw.commons.explore.depictions.DepictsClient;
+import fr.free.nrw.commons.explore.depictions.SearchDepictionsDataSourceFactory;
+import fr.free.nrw.commons.explore.depictions.SearchDepictionsDataSourceFactoryFactory;
 import fr.free.nrw.commons.explore.depictions.SearchDepictionsFragmentContract;
 import fr.free.nrw.commons.explore.depictions.SearchDepictionsFragmentPresenter;
 
@@ -16,4 +20,10 @@ public abstract class SearchModule {
             SearchDepictionsFragmentPresenter
                     presenter
     );
+
+    @Provides
+    static public SearchDepictionsDataSourceFactoryFactory providesSearchDepictionsFactoryFactory(
+        DepictsClient depictsClient){
+        return (query, loadingStates) -> new SearchDepictionsDataSourceFactory(depictsClient, query, loadingStates);
+    }
 }
