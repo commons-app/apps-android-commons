6130365309461347b810b078ecc4ed3f4d2a2850
dafnith <dafnitheodorou1999@gmail.com>
2020-06-12 12:37:54 +0300

    Connect RecentDepictions entity with RecentDepictionsDao
    AppDatabase connects the entities to their corresponding DAO.

--- app/src/main/java/fr/free/nrw/commons/db/AppDatabase.kt
+++ app/src/main/java/fr/free/nrw/commons/db/AppDatabase.kt
@@ -5,13 +5,20 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import fr.free.nrw.commons.contributions.Contribution
 import fr.free.nrw.commons.contributions.ContributionDao
+import fr.free.nrw.commons.depictions.RecentDepictions
+import fr.free.nrw.commons.depictions.RecentDepictionsDao
 
 /**
  * The database for accessing the respective DAOs
  *
  */
-@Database(entities = [Contribution::class], version = 1, exportSchema = false)
+@Database(entities = arrayOf(
+          Contribution::class,
+          RecentDepictions::class),
+          version = 1,
+          exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
   abstract fun contributionDao(): ContributionDao
+  abstract fun recentDepictionsDao(): RecentDepictionsDao
 }
