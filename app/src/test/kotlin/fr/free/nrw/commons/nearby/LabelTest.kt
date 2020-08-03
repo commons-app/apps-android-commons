3b7d74ceabfd9a7cbda9a9add7016d26ef25114a
Neslihan <tur.neslihan@gmail.com>
2020-08-03 16:29:42 +0300

    Add javadocs for label

--- app/src/test/kotlin/fr/free/nrw/commons/nearby/LabelTest.kt
+++ app/src/test/kotlin/fr/free/nrw/commons/nearby/LabelTest.kt
@@ -17,11 +17,17 @@ class LabelTest {
         label = Label.fromText("Q44539")
     }
 
+    /**
+     * test if a random label icon matches with intended one
+     */
     @Test
     fun testLabelIcon() {
         assert(label.icon.equals(R.drawable.round_icon_church))
     }
 
+    /**
+     * test if label is not found in label set, unknown icon is used
+     */
     @Test
     fun testNullLabelIcon() {
         var nullLabel: Label = Label.fromText("a random text not exist in label texts")
