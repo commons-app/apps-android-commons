61411884e6374c11f5c1b124cb8fc537b6e23a38
Neslihan <tur.neslihan@gmail.com>
2020-09-07 15:57:31 +0300

    Fix indentation

--- app/src/main/java/fr/free/nrw/commons/navtab/MoreBottomSheetFragment.java
+++ app/src/main/java/fr/free/nrw/commons/navtab/MoreBottomSheetFragment.java
@@ -4,20 +4,13 @@ import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.recyclerview.widget.RecyclerView;
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
@@ -35,93 +28,96 @@ import timber.log.Timber;
 
 public class MoreBottomSheetFragment extends BottomSheetDialogFragment {
 
-  @Inject
-  CommonsLogSender commonsLogSender;
+    @Inject
+    CommonsLogSender commonsLogSender;
 
-  @Nullable
-  @Override
-  public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable final Bundle savedInstanceState) {
-    super.onCreateView(inflater, container, savedInstanceState);
-    View view = inflater.inflate(R.layout.more_bottom_sheet, container, false);
-    ButterKnife.bind(this, view);
-    return view;
-  }
-  @OnClick(R.id.more_logout)
-  public void onLogoutClicked() {
-    new AlertDialog.Builder(getActivity())
-        .setMessage(R.string.logout_verification)
-        .setCancelable(false)
-        .setPositiveButton(R.string.yes, (dialog, which) -> {
-          BaseLogoutListener logoutListener = new BaseLogoutListener();
-          CommonsApplication app = (CommonsApplication) getContext().getApplicationContext();
-          app.clearApplicationData(getContext(), logoutListener);
-        })
-        .setNegativeButton(R.string.no, (dialog, which) -> dialog.cancel())
-        .show();
-  }
-
-  @OnClick(R.id.more_feedback)
-  public void onFeedbackClicked() {
-    String technicalInfo = commonsLogSender.getExtraInfo();
-
-    Intent feedbackIntent = new Intent(Intent.ACTION_SENDTO);
-    feedbackIntent.setType("message/rfc822");
-    feedbackIntent.setData(Uri.parse("mailto:"));
-    feedbackIntent.putExtra(Intent.EXTRA_EMAIL,
-        new String[]{CommonsApplication.FEEDBACK_EMAIL});
-    feedbackIntent.putExtra(Intent.EXTRA_SUBJECT,
-        CommonsApplication.FEEDBACK_EMAIL_SUBJECT);
-    feedbackIntent.putExtra(Intent.EXTRA_TEXT, String.format(
-        "\n\n%s\n%s", CommonsApplication.FEEDBACK_EMAIL_TEMPLATE_HEADER, technicalInfo));
-    try {
-      startActivity(feedbackIntent);
-    } catch (ActivityNotFoundException e) {
-      Toast.makeText(getActivity(), R.string.no_email_client, Toast.LENGTH_SHORT).show();
-    }
-  }
-
-  @OnClick(R.id.more_about)
-  public void onAboutClicked() {
-    Intent intent = new Intent(getActivity(), CategoryImagesActivity.class);
-    intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-    getActivity().startActivity(intent);
-  }
-
-  @OnClick(R.id.more_tutorial)
-  public void onTutorialClicked() {
-    WelcomeActivity.startYourself(getActivity());
-  }
-
-  @OnClick(R.id.more_settings)
-  public void onSettingsClicked() {
-    Intent intent = new Intent(getActivity(), SettingsActivity.class);
-    intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-    getActivity().startActivity(intent);
-  }
-
-  @OnClick(R.id.more_achievements)
-  public void onAchievementsClicked() {
-    Intent intent = new Intent(getActivity(), AchievementsActivity.class);
-    intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-    getActivity().startActivity(intent);
-  }
-
-  @OnClick(R.id.more_peer_review)
-  public void onPeerReviewClicked() {
-    ReviewActivity.startYourself(getActivity(), getString(R.string.title_activity_review));
-  }
-
-  private class BaseLogoutListener implements CommonsApplication.LogoutListener {
+    @Nullable
     @Override
-    public void onLogoutComplete() {
-      Timber.d("Logout complete callback received.");
-      Intent nearbyIntent = new Intent(
-          getContext(), LoginActivity.class);
-      nearbyIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-      nearbyIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-      startActivity(nearbyIntent);
-      getActivity().finish();
+    public View onCreateView(@NonNull final LayoutInflater inflater,
+        @Nullable final ViewGroup container, @Nullable final Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        final View view = inflater.inflate(R.layout.more_bottom_sheet, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
-  }
+
+    @OnClick(R.id.more_logout)
+    public void onLogoutClicked() {
+        new AlertDialog.Builder(getActivity())
+            .setMessage(R.string.logout_verification)
+            .setCancelable(false)
+            .setPositiveButton(R.string.yes, (dialog, which) -> {
+                BaseLogoutListener logoutListener = new BaseLogoutListener();
+                CommonsApplication app = (CommonsApplication) getContext().getApplicationContext();
+                app.clearApplicationData(getContext(), logoutListener);
+            })
+            .setNegativeButton(R.string.no, (dialog, which) -> dialog.cancel())
+            .show();
+    }
+
+    @OnClick(R.id.more_feedback)
+    public void onFeedbackClicked() {
+        final String technicalInfo = commonsLogSender.getExtraInfo();
+
+        final Intent feedbackIntent = new Intent(Intent.ACTION_SENDTO);
+        feedbackIntent.setType("message/rfc822");
+        feedbackIntent.setData(Uri.parse("mailto:"));
+        feedbackIntent.putExtra(Intent.EXTRA_EMAIL,
+            new String[]{CommonsApplication.FEEDBACK_EMAIL});
+        feedbackIntent.putExtra(Intent.EXTRA_SUBJECT,
+            CommonsApplication.FEEDBACK_EMAIL_SUBJECT);
+        feedbackIntent.putExtra(Intent.EXTRA_TEXT, String.format(
+            "\n\n%s\n%s", CommonsApplication.FEEDBACK_EMAIL_TEMPLATE_HEADER, technicalInfo));
+        try {
+            startActivity(feedbackIntent);
+        } catch (final ActivityNotFoundException e) {
+            Toast.makeText(getActivity(), R.string.no_email_client, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @OnClick(R.id.more_about)
+    public void onAboutClicked() {
+        final Intent intent = new Intent(getActivity(), CategoryImagesActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        getActivity().startActivity(intent);
+    }
+
+    @OnClick(R.id.more_tutorial)
+    public void onTutorialClicked() {
+        WelcomeActivity.startYourself(getActivity());
+    }
+
+    @OnClick(R.id.more_settings)
+    public void onSettingsClicked() {
+        final Intent intent = new Intent(getActivity(), SettingsActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        getActivity().startActivity(intent);
+    }
+
+    @OnClick(R.id.more_achievements)
+    public void onAchievementsClicked() {
+        final Intent intent = new Intent(getActivity(), AchievementsActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        getActivity().startActivity(intent);
+    }
+
+    @OnClick(R.id.more_peer_review)
+    public void onPeerReviewClicked() {
+        ReviewActivity.startYourself(getActivity(), getString(R.string.title_activity_review));
+    }
+
+    private class BaseLogoutListener implements CommonsApplication.LogoutListener {
+
+        @Override
+        public void onLogoutComplete() {
+            Timber.d("Logout complete callback received.");
+            final Intent nearbyIntent = new Intent(
+                getContext(), LoginActivity.class);
+            nearbyIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            nearbyIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(nearbyIntent);
+            getActivity().finish();
+        }
+    }
 }
 
