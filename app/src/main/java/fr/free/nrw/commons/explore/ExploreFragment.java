e36588ee3589ac856178cc746fd31d1b2ac34e6d
Neslihan <tur.neslihan@gmail.com>
2020-09-07 18:06:55 +0300

    Fix explore fragment indentation

--- app/src/main/java/fr/free/nrw/commons/explore/ExploreFragment.java
+++ app/src/main/java/fr/free/nrw/commons/explore/ExploreFragment.java
@@ -31,140 +31,140 @@ import java.util.List;
 public class ExploreFragment extends CommonsDaggerSupportFragment
     implements MediaDetailPagerFragment.MediaDetailProvider, CategoryImagesCallback {
 
-  private static final String FEATURED_IMAGES_CATEGORY = "Featured_pictures_on_Wikimedia_Commons";
-  private static final String MOBILE_UPLOADS_CATEGORY = "Uploaded_with_Mobile/Android";
+    private static final String FEATURED_IMAGES_CATEGORY = "Featured_pictures_on_Wikimedia_Commons";
+    private static final String MOBILE_UPLOADS_CATEGORY = "Uploaded_with_Mobile/Android";
 
 
-  @BindView(R.id.mediaContainer)
-  FrameLayout mediaContainer;
-  @BindView(R.id.tab_layout)
-  TabLayout tabLayout;
-  @BindView(R.id.viewPager)
-  ViewPager viewPager;
-  ViewPagerAdapter viewPagerAdapter;
-  private FragmentManager supportFragmentManager;
-  private MediaDetailPagerFragment mediaDetails;
-  private CategoriesMediaFragment mobileImagesListFragment;
-  private CategoriesMediaFragment featuredImagesListFragment;
+    @BindView(R.id.mediaContainer)
+    FrameLayout mediaContainer;
+    @BindView(R.id.tab_layout)
+    TabLayout tabLayout;
+    @BindView(R.id.viewPager)
+    ViewPager viewPager;
+    ViewPagerAdapter viewPagerAdapter;
+    private FragmentManager supportFragmentManager;
+    private MediaDetailPagerFragment mediaDetails;
+    private CategoriesMediaFragment mobileImagesListFragment;
+    private CategoriesMediaFragment featuredImagesListFragment;
 
-  /**
-   * Consumers should be simply using this method to use this activity.
-   *
-   * @param context A Context of the application package implementing this class.
-   */
-  public static void startYourself(Context context) {
-    Intent intent = new Intent(context, ExploreActivity.class);
-    intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-    context.startActivity(intent);
-  }
-
-  @NonNull
-  public static ExploreFragment newInstance() {
-    ExploreFragment fragment = new ExploreFragment();
-    fragment.setRetainInstance(true);
-    return fragment;
-  }
-
-  @Override
-  public void onCreate(@Nullable final Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-  }
-
-  @Override
-  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    View view = inflater.inflate(R.layout.fragment_explore, container, false);
-    ButterKnife.bind(this, view);
-    supportFragmentManager = getChildFragmentManager();
-    viewPagerAdapter = new ViewPagerAdapter(getChildFragmentManager());
-    viewPager.setAdapter(viewPagerAdapter);
-    tabLayout.setupWithViewPager(viewPager);
-    setTabs();
-    setHasOptionsMenu(true);
-    return view;
-  }
-
-  /**
-   * Sets the titles in the tabLayout and fragments in the viewPager
-   */
-  public void setTabs() {
-    List<Fragment> fragmentList = new ArrayList<>();
-    List<String> titleList = new ArrayList<>();
-
-    featuredImagesListFragment = new CategoriesMediaFragment();
-    Bundle featuredArguments = new Bundle();
-    featuredArguments.putString("categoryName", FEATURED_IMAGES_CATEGORY);
-    featuredImagesListFragment.setArguments(featuredArguments);
-    fragmentList.add(featuredImagesListFragment);
-    titleList.add(getString(R.string.explore_tab_title_featured).toUpperCase());
-
-    mobileImagesListFragment = new CategoriesMediaFragment();
-    Bundle mobileArguments = new Bundle();
-    mobileArguments.putString("categoryName", MOBILE_UPLOADS_CATEGORY);
-    mobileImagesListFragment.setArguments(mobileArguments);
-    fragmentList.add(mobileImagesListFragment);
-    titleList.add(getString(R.string.explore_tab_title_mobile).toUpperCase());
-
-    viewPagerAdapter.setTabData(fragmentList, titleList);
-    viewPagerAdapter.notifyDataSetChanged();
-  }
-
-  /**
-   * This method is called mediaDetailPagerFragment. It returns the Media Object at that Index
-   *
-   * @param i It is the index of which media object is to be returned which is same as
-   *          current index of viewPager.
-   * @return Media Object
-   */
-  @Override
-  public Media getMediaAtPosition(int i) {
-    if (tabLayout.getSelectedTabPosition() == 1) {
-      return mobileImagesListFragment.getMediaAtPosition(i);
-    } else if (tabLayout.getSelectedTabPosition() == 0) {
-      return featuredImagesListFragment.getMediaAtPosition(i);
-    } else {
-      return null;
+    /**
+     * Consumers should be simply using this method to use this activity.
+     *
+     * @param context A Context of the application package implementing this class.
+     */
+    public static void startYourself(Context context) {
+        Intent intent = new Intent(context, ExploreActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        context.startActivity(intent);
     }
-  }
 
-  /**
-   * This method is called on from getCount of MediaDetailPagerFragment
-   * The viewpager will contain same number of media items as that of media elements in adapter.
-   *
-   * @return Total Media count in the adapter
-   */
-  @Override
-  public int getTotalMediaCount() {
-    if (tabLayout.getSelectedTabPosition() == 1) {
-      return mobileImagesListFragment.getTotalMediaCount();
-    } else if (tabLayout.getSelectedTabPosition() == 0) {
-      return featuredImagesListFragment.getTotalMediaCount();
-    } else {
-      return 0;
+    @NonNull
+    public static ExploreFragment newInstance() {
+        ExploreFragment fragment = new ExploreFragment();
+        fragment.setRetainInstance(true);
+        return fragment;
     }
-  }
 
-  @Override
-  public Integer getContributionStateAt(int position) {
-    return null;
-  }
-
-  /**
-   * This method is called on success of API call for featured images or mobile uploads.
-   * The viewpager will notified that number of items have changed.
-   */
-  @Override
-  public void viewPagerNotifyDataSetChanged() {
-    if (mediaDetails != null) {
-      mediaDetails.notifyDataSetChanged();
+    @Override
+    public void onCreate(@Nullable final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
-  }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+        @Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_explore, container, false);
+        ButterKnife.bind(this, view);
+        supportFragmentManager = getChildFragmentManager();
+        viewPagerAdapter = new ViewPagerAdapter(getChildFragmentManager());
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+        setTabs();
+        setHasOptionsMenu(true);
+        return view;
+    }
 
-  /**
-   * This method is called on backPressed of anyFragment in the activity.
-   * If condition is called when mediaDetailFragment is opened.
-   */
+    /**
+     * Sets the titles in the tabLayout and fragments in the viewPager
+     */
+    public void setTabs() {
+        List<Fragment> fragmentList = new ArrayList<>();
+        List<String> titleList = new ArrayList<>();
+
+        featuredImagesListFragment = new CategoriesMediaFragment();
+        Bundle featuredArguments = new Bundle();
+        featuredArguments.putString("categoryName", FEATURED_IMAGES_CATEGORY);
+        featuredImagesListFragment.setArguments(featuredArguments);
+        fragmentList.add(featuredImagesListFragment);
+        titleList.add(getString(R.string.explore_tab_title_featured).toUpperCase());
+
+        mobileImagesListFragment = new CategoriesMediaFragment();
+        Bundle mobileArguments = new Bundle();
+        mobileArguments.putString("categoryName", MOBILE_UPLOADS_CATEGORY);
+        mobileImagesListFragment.setArguments(mobileArguments);
+        fragmentList.add(mobileImagesListFragment);
+        titleList.add(getString(R.string.explore_tab_title_mobile).toUpperCase());
+
+        viewPagerAdapter.setTabData(fragmentList, titleList);
+        viewPagerAdapter.notifyDataSetChanged();
+    }
+
+    /**
+     * This method is called mediaDetailPagerFragment. It returns the Media Object at that Index
+     *
+     * @param i It is the index of which media object is to be returned which is same as current
+     *          index of viewPager.
+     * @return Media Object
+     */
+    @Override
+    public Media getMediaAtPosition(int i) {
+        if (tabLayout.getSelectedTabPosition() == 1) {
+            return mobileImagesListFragment.getMediaAtPosition(i);
+        } else if (tabLayout.getSelectedTabPosition() == 0) {
+            return featuredImagesListFragment.getMediaAtPosition(i);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * This method is called on from getCount of MediaDetailPagerFragment The viewpager will contain
+     * same number of media items as that of media elements in adapter.
+     *
+     * @return Total Media count in the adapter
+     */
+    @Override
+    public int getTotalMediaCount() {
+        if (tabLayout.getSelectedTabPosition() == 1) {
+            return mobileImagesListFragment.getTotalMediaCount();
+        } else if (tabLayout.getSelectedTabPosition() == 0) {
+            return featuredImagesListFragment.getTotalMediaCount();
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public Integer getContributionStateAt(int position) {
+        return null;
+    }
+
+    /**
+     * This method is called on success of API call for featured images or mobile uploads. The
+     * viewpager will notified that number of items have changed.
+     */
+    @Override
+    public void viewPagerNotifyDataSetChanged() {
+        if (mediaDetails != null) {
+            mediaDetails.notifyDataSetChanged();
+        }
+    }
+
+    /**
+     * This method is called on backPressed of anyFragment in the activity.
+     * If condition is called when mediaDetailFragment is opened.
+     */
   /*@Override
   public void onBackPressed() {
     if (supportFragmentManager.getBackStackEntryCount() == 1) {
@@ -176,55 +176,56 @@ public class ExploreFragment extends CommonsDaggerSupportFragment
     super.onBackPressed();
   }*/
 
-  /**
-   * This method is called onClick of media inside category featured images or mobile uploads.
-   */
-  @Override
-  public void onMediaClicked( int position) {
-    mediaContainer.setVisibility(View.VISIBLE);
-    if (mediaDetails == null || !mediaDetails.isVisible()) {
-      // set isFeaturedImage true for featured images, to include author field on media detail
-      mediaDetails = new MediaDetailPagerFragment(false, true);
-      FragmentManager supportFragmentManager = getChildFragmentManager();
-      supportFragmentManager
-          .beginTransaction()
-          .hide(supportFragmentManager.getFragments().get(supportFragmentManager.getBackStackEntryCount()))
-          .add(R.id.mediaContainer, mediaDetails)
-          .addToBackStack(null)
-          .commit();
-      // Reason for using hide, add instead of replace is to maintain scroll position after
-      // coming back to the explore activity. See https://github.com/commons-app/apps-android-commons/issues/1631
-      // https://stackoverflow.com/questions/11353075/how-can-i-maintain-fragment-state-when-added-to-the-back-stack/19022550#19022550            supportFragmentManager.executePendingTransactions();
+    /**
+     * This method is called onClick of media inside category featured images or mobile uploads.
+     */
+    @Override
+    public void onMediaClicked(int position) {
+        mediaContainer.setVisibility(View.VISIBLE);
+        if (mediaDetails == null || !mediaDetails.isVisible()) {
+            // set isFeaturedImage true for featured images, to include author field on media detail
+            mediaDetails = new MediaDetailPagerFragment(false, true);
+            FragmentManager supportFragmentManager = getChildFragmentManager();
+            supportFragmentManager
+                .beginTransaction()
+                .hide(supportFragmentManager.getFragments()
+                    .get(supportFragmentManager.getBackStackEntryCount()))
+                .add(R.id.mediaContainer, mediaDetails)
+                .addToBackStack(null)
+                .commit();
+            // Reason for using hide, add instead of replace is to maintain scroll position after
+            // coming back to the explore activity. See https://github.com/commons-app/apps-android-commons/issues/1631
+            // https://stackoverflow.com/questions/11353075/how-can-i-maintain-fragment-state-when-added-to-the-back-stack/19022550#19022550            supportFragmentManager.executePendingTransactions();
+        }
+        mediaDetails.showImage(position);
+        //forceInitBackButton();
     }
-    mediaDetails.showImage(position);
-    //forceInitBackButton();
-  }
 
-  /**
-   * This method inflates the menu in the toolbar
-   */
-  @Override
-  public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-    inflater.inflate(R.menu.menu_search, menu);
-    super.onCreateOptionsMenu(menu, inflater);
-  }
-
-  /**
-   * This method handles the logic on ItemSelect in toolbar menu
-   * Currently only 1 choice is available to open search page of the app
-   */
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-
-    // Handle item selection
-    switch (item.getItemId()) {
-      case R.id.action_search:
-        NavigationBaseActivity.startActivityWithFlags(getActivity(), SearchActivity.class);
-        return true;
-      default:
-        return super.onOptionsItemSelected(item);
+    /**
+     * This method inflates the menu in the toolbar
+     */
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_search, menu);
+        super.onCreateOptionsMenu(menu, inflater);
     }
-  }
+
+    /**
+     * This method handles the logic on ItemSelect in toolbar menu Currently only 1 choice is
+     * available to open search page of the app
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                NavigationBaseActivity.startActivityWithFlags(getActivity(), SearchActivity.class);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
 
 
