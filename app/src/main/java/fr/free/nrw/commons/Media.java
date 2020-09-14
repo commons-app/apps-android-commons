ac5b5cb7b8e02b334e5319b27aefc1e7a8f62719
Ashish <ashishkumar468@gmail.com>
2020-09-14 16:00:23 +0530

    Revert "Fixes #3926"
    This reverts commit b208b3d7fd22bd3b961f2ef5fc8a8a418e119213.

--- app/src/main/java/fr/free/nrw/commons/Media.java
+++ app/src/main/java/fr/free/nrw/commons/Media.java
@@ -9,8 +9,6 @@ import androidx.annotation.Nullable;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 import org.apache.commons.lang3.StringUtils;
 import org.wikipedia.dataclient.mwapi.MwQueryPage;
 import org.wikipedia.gallery.ExtMetadata;
@@ -29,7 +27,6 @@ import java.util.Map;
 import fr.free.nrw.commons.location.LatLng;
 import fr.free.nrw.commons.utils.CommonsDateUtil;
 import fr.free.nrw.commons.utils.MediaDataExtractorUtil;
-import timber.log.Timber;
 
 @Entity
 public class Media implements Parcelable {
@@ -236,11 +233,6 @@ public class Media implements Parcelable {
      * @return file name as a string
      */
     public String getFilename() {
-        try {
-            return URLEncoder.encode(filename,"utf-8");
-        } catch (UnsupportedEncodingException e) {
-            Timber.e(e);
-        }
         return filename;
     }
 
