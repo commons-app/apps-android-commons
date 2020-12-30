4e9e1f79a1a4d9833869aba5815c92b72096e582
Aditya-Srivastav <54016427+4D17Y4@users.noreply.github.com>
2020-12-30 18:06:26 +0530

    Removed unneeded log import.

--- app/src/main/java/fr/free/nrw/commons/contributions/ContributionsListFragment.java
+++ app/src/main/java/fr/free/nrw/commons/contributions/ContributionsListFragment.java
@@ -9,7 +9,6 @@ import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
