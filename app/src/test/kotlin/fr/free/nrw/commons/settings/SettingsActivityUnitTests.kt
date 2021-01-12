db66c7e55652cbc3a7e816756f079b0a45d3951c
Madhur Gupta <madhurgupta10@gmail.com>
2021-01-12 22:26:10 +0530

    Added Settings Activity Tests

---
+++ app/src/test/kotlin/fr/free/nrw/commons/settings/SettingsActivityUnitTests.kt
@@ -0,0 +1,57 @@
+package fr.free.nrw.commons.settings
+
+import android.content.Context
+import android.view.MenuItem
+import fr.free.nrw.commons.TestCommonsApplication
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.MockitoAnnotations
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
+import org.robolectric.fakes.RoboMenuItem
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [21], application = TestCommonsApplication::class)
+class SettingsActivityUnitTests {
+
+    private lateinit var activity: SettingsActivity
+
+    private lateinit var context: Context
+
+    private lateinit var menuItem: MenuItem
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+
+        context = RuntimeEnvironment.application.applicationContext
+
+        activity = Robolectric.buildActivity(SettingsActivity::class.java).create().get()
+
+        menuItem = RoboMenuItem(null)
+
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun checkActivityNotNull() {
+        Assert.assertNotNull(activity)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testOnSupportNavigateUp() {
+        activity.onSupportNavigateUp()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testOnOptionsItemSelected() {
+        activity.onOptionsItemSelected(menuItem)
+    }
+
+}
