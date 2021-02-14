3ae6b662c6c9731e9eb15c01152c64cda5e962ef
Aditya Srivastava <iamaditya2009@gmail.com>
2021-02-14 20:47:02 +0530

    javadoc added

--- app/src/main/java/fr/free/nrw/commons/explore/ParentViewPager.java
+++ app/src/main/java/fr/free/nrw/commons/explore/ParentViewPager.java
@@ -5,10 +5,15 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import androidx.viewpager.widget.ViewPager;
 
+
+//  A custom viewPager whose scrolling can be enabled and disabled.
 public class ParentViewPager extends ViewPager {
 
+//  Boolean variable that stores the current state of pager scroll i.e(enabled or disabled)
   private boolean canScroll = true;
 
+
+//  Default constructors
   public ParentViewPager(Context context) {
     super(context);
   }
@@ -17,21 +22,35 @@ public class ParentViewPager extends ViewPager {
     super(context, attrs);
   }
 
+
+//  Setter method for canScroll.
   public void setCanScroll(boolean canScroll) {
     this.canScroll = canScroll;
   }
 
+
+//  Getter method for canScroll.
+  public boolean isCanScroll() {
+    return canScroll;
+  }
+
+
+
+//  Method that prevents scrolling if canScroll is set to false.
   @Override
   public boolean onTouchEvent(MotionEvent ev) {
     return canScroll && super.onTouchEvent(ev);
   }
 
+
+//  A facilitator method that allows parent to intercept touch events before its children.
+//  thus making it possible to prevent swiping parent on child end.
   @Override
   public boolean onInterceptTouchEvent(MotionEvent ev) {
     return canScroll && super.onInterceptTouchEvent(ev);
   }
 
-  public boolean isCanScroll() {
-    return canScroll;
-  }
+
+
+
 }
