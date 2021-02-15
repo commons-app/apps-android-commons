51f9526d1e04f8943631eb71f7c2fae9291fb661
Aditya Srivastava <iamaditya2009@gmail.com>
2021-02-15 10:22:44 +0530

    javadoc modified

--- app/src/main/java/fr/free/nrw/commons/explore/ParentViewPager.java
+++ app/src/main/java/fr/free/nrw/commons/explore/ParentViewPager.java
@@ -5,15 +5,21 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import androidx.viewpager.widget.ViewPager;
 
-
-//  A custom viewPager whose scrolling can be enabled and disabled.
+/**
+ * ParentViewPager
+ * A custom viewPager whose scrolling can be enabled and disabled.
+  */
 public class ParentViewPager extends ViewPager {
 
-//  Boolean variable that stores the current state of pager scroll i.e(enabled or disabled)
+  /**
+   * Boolean variable that stores the current state of pager scroll i.e(enabled or disabled)
+    */
   private boolean canScroll = true;
 
 
-//  Default constructors
+  /**
+   * Default constructors
+    */
   public ParentViewPager(Context context) {
     super(context);
   }
@@ -23,28 +29,35 @@ public class ParentViewPager extends ViewPager {
   }
 
 
-//  Setter method for canScroll.
+  /**
+   * Setter method for canScroll.
+    */
   public void setCanScroll(boolean canScroll) {
     this.canScroll = canScroll;
   }
 
 
-//  Getter method for canScroll.
+  /**
+   * Getter method for canScroll.
+    */
   public boolean isCanScroll() {
     return canScroll;
   }
 
 
-
-//  Method that prevents scrolling if canScroll is set to false.
+  /**
+   * Method that prevents scrolling if canScroll is set to false.
+    */
   @Override
   public boolean onTouchEvent(MotionEvent ev) {
     return canScroll && super.onTouchEvent(ev);
   }
 
 
-//  A facilitator method that allows parent to intercept touch events before its children.
-//  thus making it possible to prevent swiping parent on child end.
+  /**
+   *   A facilitator method that allows parent to intercept touch events before its children.
+   *   thus making it possible to prevent swiping parent on child end.
+    */
   @Override
   public boolean onInterceptTouchEvent(MotionEvent ev) {
     return canScroll && super.onInterceptTouchEvent(ev);
