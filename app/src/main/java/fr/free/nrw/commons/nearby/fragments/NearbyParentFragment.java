ff8314f2752e165c72f78b425571f2d90c6f1346
Pratham2305 <Pratham2305@users.noreply.github.com>
2021-03-15 20:59:07 +0530

    Added comments

--- app/src/main/java/fr/free/nrw/commons/nearby/fragments/NearbyParentFragment.java
+++ app/src/main/java/fr/free/nrw/commons/nearby/fragments/NearbyParentFragment.java
@@ -1317,7 +1317,7 @@ public class NearbyParentFragment extends CommonsDaggerSupportFragment
             return (isBookmarked ?
                 R.drawable.ic_custom_map_marker_green_bookmarked :
                 R.drawable.ic_custom_map_marker_green);
-        } else if (!place.exists) { // Means place is destroyed
+        } else if (!place.exists) { // Means that the topic of the Wikidata item does not exist in the real world anymore, for instance it is a past event, or a place that was destroyed
             return (isBookmarked ?
                 R.drawable.ic_custom_map_marker_grey_bookmarked :
                 R.drawable.ic_custom_map_marker_grey);
