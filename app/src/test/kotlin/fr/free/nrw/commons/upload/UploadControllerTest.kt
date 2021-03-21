737c6a2484a467ec1b46d39ad3cd19a0c167d807
Ashish <ashishkumar468@gmail.com>
2021-03-22 02:01:04 +0530

    rolledback to compileSDKVersion 28, fixed tests

--- app/src/test/kotlin/fr/free/nrw/commons/upload/UploadControllerTest.kt
+++ app/src/test/kotlin/fr/free/nrw/commons/upload/UploadControllerTest.kt
@@ -6,6 +6,7 @@ import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
 import fr.free.nrw.commons.Media
 import fr.free.nrw.commons.contributions.Contribution
+import fr.free.nrw.commons.kvstore.JsonKvStore
 import org.junit.Before
 import org.junit.Test
 import org.mockito.InjectMocks
@@ -16,6 +17,9 @@ import org.mockito.MockitoAnnotations
 class UploadControllerTest {
     @Mock
     internal var context: Context? = null
+
+    @Mock
+    internal lateinit var store: JsonKvStore
 
     @Mock
     internal lateinit var contentResolver: ContentResolver
@@ -29,20 +33,12 @@ class UploadControllerTest {
     }
 
     @Test
-    fun prepareService() {
-    }
-
-    @Test
-    fun cleanup() {
-    }
-
-    @Test
     fun startUpload() {
         val contribution = mock(Contribution::class.java)
         val media = mock<Media>()
         whenever(contribution.media).thenReturn(media)
         whenever(media.author).thenReturn("Creator")
         whenever(context?.contentResolver).thenReturn(contentResolver)
-        uploadController!!.prepareMedia(contribution)
+        uploadController?.prepareMedia(contribution)
     }
 }
