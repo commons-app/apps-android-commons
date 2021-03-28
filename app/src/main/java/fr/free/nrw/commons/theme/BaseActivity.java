b3baaf56df4b8f5199d12d49d3c4357df13016e6
Prince kushwaha <kushwahaprince418@gmail.com>
2021-03-28 18:19:07 +0530

    * Add adjuctFontScale function  to fix issue * Add Javadoc

--- app/src/main/java/fr/free/nrw/commons/theme/BaseActivity.java
+++ app/src/main/java/fr/free/nrw/commons/theme/BaseActivity.java
@@ -1,10 +1,11 @@
 package fr.free.nrw.commons.theme;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
-
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
 import javax.inject.Inject;
 import javax.inject.Named;
-
 import fr.free.nrw.commons.R;
 import fr.free.nrw.commons.di.CommonsDaggerAppCompatActivity;
 import fr.free.nrw.commons.kvstore.JsonKvStore;
@@ -27,6 +28,8 @@ public abstract class BaseActivity extends CommonsDaggerAppCompatActivity {
         super.onCreate(savedInstanceState);
         wasPreviouslyDarkTheme = systemThemeUtils.isDeviceInNightMode();
         setTheme(wasPreviouslyDarkTheme ? R.style.DarkAppTheme : R.style.LightAppTheme);
+        float fontScale = android.provider.Settings.System.getFloat(getBaseContext().getContentResolver(),android.provider.Settings.System.FONT_SCALE, 1f);
+        adjustFontScale(getResources().getConfiguration(),fontScale);
     }
 
     @Override
@@ -44,4 +47,17 @@ public abstract class BaseActivity extends CommonsDaggerAppCompatActivity {
         super.onDestroy();
         compositeDisposable.clear();
     }
+
+    /**
+     * its function is apply fontScale on device
+     */
+    public  void adjustFontScale( Configuration configuration,float scale) {
+        configuration.fontScale = scale;
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+        wm.getDefaultDisplay().getMetrics(metrics);
+        metrics.scaledDensity = configuration.fontScale * metrics.density;
+        getBaseContext().getResources().updateConfiguration(configuration, metrics);
+    }
+
 }
