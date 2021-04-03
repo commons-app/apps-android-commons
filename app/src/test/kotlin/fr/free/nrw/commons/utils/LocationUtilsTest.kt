5da153b3a90aa5d0c9843aa9acea08a1a1498bd2
Aditya Srivastava <iamaditya2009@gmail.com>
2021-04-03 22:51:12 +0530

    added location utils test

---
+++ app/src/test/kotlin/fr/free/nrw/commons/utils/LocationUtilsTest.kt
@@ -0,0 +1,33 @@
+package fr.free.nrw.commons.utils
+
+import fr.free.nrw.commons.location.LatLng
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+
+/**
+ * Test class for location utils
+ */
+class LocationUtilsTest {
+
+    /**
+     * MapBox LatLng to commons LatLng test.
+     */
+    @Test
+    fun testMapBoxLatLngToCommonsLatLng() {
+        val commonsLatLngTest = LocationUtils.mapBoxLatLngToCommonsLatLng(com.mapbox.mapboxsdk.geometry.LatLng(0.0, 0.0))
+        assertEquals(0.0, commonsLatLngTest.latitude)
+        assertEquals(0.0, commonsLatLngTest.longitude)
+    }
+
+    /**
+     * Commons LatLng to MapBox LatLng test.
+     */
+    @Test
+    fun testCommonsLatLngToMapBoxLatLng() {
+        val geoLatLngTest = LocationUtils.commonsLatLngToMapBoxLatLng(LatLng(0.0, 0.0, 0f))
+        assertEquals(0.0, geoLatLngTest.latitude)
+        assertEquals(0.0, geoLatLngTest.longitude)
+    }
+
+}
