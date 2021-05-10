b658be1a9a91b99e2f304330353de380ef28690f
Aditya Srivastava <iamaditya2009@gmail.com>
2021-05-10 22:16:47 +0530

    added bookmarkLocationFragment unit tests

---
+++ app/src/test/kotlin/fr/free/nrw/commons/bookmarks/locations/BookmarkLocationFragmentUnitTests.kt
@@ -0,0 +1,117 @@
+package fr.free.nrw.commons.bookmarks.locations
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import androidx.recyclerview.widget.RecyclerView
+import fr.free.nrw.commons.R
+import fr.free.nrw.commons.TestAppAdapter
+import fr.free.nrw.commons.TestCommonsApplication
+import fr.free.nrw.commons.contributions.ContributionController
+import fr.free.nrw.commons.kvstore.JsonKvStore
+import fr.free.nrw.commons.nearby.fragments.CommonPlaceClickActions
+import fr.free.nrw.commons.nearby.fragments.PlaceAdapter
+import fr.free.nrw.commons.profile.ProfileActivity
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import org.powermock.reflect.Whitebox
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.LooperMode
+import org.wikipedia.AppAdapter
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [21], application = TestCommonsApplication::class)
+@LooperMode(LooperMode.Mode.PAUSED)
+class BookmarkLocationFragmentUnitTests {
+
+    private lateinit var fragment: BookmarkLocationsFragment
+    private lateinit var context: Context
+    private lateinit var view: View
+    private lateinit var statusTextView: TextView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var commonPlaceClickActions: CommonPlaceClickActions
+
+    @Mock
+    lateinit var store: JsonKvStore
+
+    @Mock
+    private lateinit var parentLayout: RelativeLayout
+
+    @Mock
+    private lateinit var savedInstanceState: Bundle
+
+    @Mock
+    private lateinit var bookmarkLocationDao: BookmarkLocationsDao
+
+    @Mock
+    private lateinit var controller: BookmarkLocationsController
+
+    @Mock
+    private lateinit var contributionController: ContributionController
+
+    @Mock
+    private lateinit var adapter: PlaceAdapter
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        context = RuntimeEnvironment.application.applicationContext
+        AppAdapter.set(TestAppAdapter())
+        val activity = Robolectric.buildActivity(ProfileActivity::class.java).create().get()
+        fragment = BookmarkLocationsFragment.newInstance()
+        val fragmentManager: FragmentManager = activity.supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(fragment, null)
+        fragmentTransaction.commit()
+
+        view = LayoutInflater.from(activity)
+            .inflate(R.layout.fragment_bookmarks_locations, null) as View
+
+        statusTextView = view.findViewById(R.id.statusMessage)
+        progressBar = view.findViewById(R.id.loadingImagesProgressBar)
+        recyclerView = view.findViewById(R.id.listView)
+        commonPlaceClickActions = CommonPlaceClickActions(store,activity,contributionController)
+
+        fragment.statusTextView = statusTextView
+        fragment.progressBar = progressBar
+        fragment.recyclerView = recyclerView
+        fragment.parentLayout = parentLayout
+        fragment.bookmarkLocationDao = bookmarkLocationDao
+        fragment.controller = controller
+        fragment.commonPlaceClickActions = commonPlaceClickActions
+        Whitebox.setInternalState(fragment, "adapter", adapter)
+
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun checkFragmentNotNull() {
+        Assert.assertNotNull(fragment)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testOnViewCreated() {
+        fragment.onViewCreated(view, savedInstanceState)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testOnResume() {
+        fragment.onResume()
+    }
+}
