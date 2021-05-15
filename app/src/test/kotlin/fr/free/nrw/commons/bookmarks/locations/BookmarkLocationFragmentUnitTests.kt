1feb337a9623d754bb5c0bf43050ff8a4d8e8820
Aditya Srivastava <iamaditya2009@gmail.com>
2021-05-15 18:40:34 +0530

    more lines covered

--- app/src/test/kotlin/fr/free/nrw/commons/bookmarks/locations/BookmarkLocationFragmentUnitTests.kt
+++ app/src/test/kotlin/fr/free/nrw/commons/bookmarks/locations/BookmarkLocationFragmentUnitTests.kt
@@ -10,11 +10,13 @@ import android.widget.TextView
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.RecyclerView
+import com.nhaarman.mockitokotlin2.whenever
 import fr.free.nrw.commons.R
 import fr.free.nrw.commons.TestAppAdapter
 import fr.free.nrw.commons.TestCommonsApplication
 import fr.free.nrw.commons.contributions.ContributionController
 import fr.free.nrw.commons.kvstore.JsonKvStore
+import fr.free.nrw.commons.nearby.Place
 import fr.free.nrw.commons.nearby.fragments.CommonPlaceClickActions
 import fr.free.nrw.commons.nearby.fragments.PlaceAdapter
 import fr.free.nrw.commons.profile.ProfileActivity
@@ -31,6 +33,8 @@ import org.robolectric.RuntimeEnvironment
 import org.robolectric.annotation.Config
 import org.robolectric.annotation.LooperMode
 import org.wikipedia.AppAdapter
+import java.lang.reflect.Method
+import java.util.ArrayList
 
 @RunWith(RobolectricTestRunner::class)
 @Config(sdk = [21], application = TestCommonsApplication::class)
@@ -44,6 +48,7 @@ class BookmarkLocationFragmentUnitTests {
     private lateinit var progressBar: ProgressBar
     private lateinit var recyclerView: RecyclerView
     private lateinit var commonPlaceClickActions: CommonPlaceClickActions
+    private lateinit var layoutInflater: LayoutInflater
 
     @Mock
     lateinit var store: JsonKvStore
@@ -66,6 +71,19 @@ class BookmarkLocationFragmentUnitTests {
     @Mock
     private lateinit var adapter: PlaceAdapter
 
+    /**
+     * Get Mock bookmark list.
+     */
+    private val mockBookmarkList: List<Place>
+        private get() {
+            val list = ArrayList<Place>()
+            list.add(Place("en","a place",null,"a description",null,"a cat",null,null,true))
+            return list
+        }
+
+    /**
+     * fragment Setup
+     */
     @Before
     fun setUp() {
         MockitoAnnotations.initMocks(this)
@@ -78,7 +96,8 @@ class BookmarkLocationFragmentUnitTests {
         fragmentTransaction.add(fragment, null)
         fragmentTransaction.commit()
 
-        view = LayoutInflater.from(activity)
+        layoutInflater = LayoutInflater.from(activity)
+        view = layoutInflater
             .inflate(R.layout.fragment_bookmarks_locations, null) as View
 
         statusTextView = view.findViewById(R.id.statusMessage)
@@ -97,18 +116,48 @@ class BookmarkLocationFragmentUnitTests {
 
     }
 
+    /**
+     * test init places when non empty
+     */
+    @Test
+    fun testInitNonEmpty(){
+        whenever(controller.loadFavoritesLocations()).thenReturn(mockBookmarkList)
+        val method: Method =
+            BookmarkLocationsFragment::class.java.getDeclaredMethod("initList")
+        method.isAccessible = true
+        method.invoke(fragment)
+    }
+
+    /**
+     * test onCreateView
+     */
+    @Test
+    @Throws(Exception::class)
+    fun testOnCreateView() {
+        fragment.onCreateView(layoutInflater,null,savedInstanceState)
+    }
+
+    /**
+     * check fragment notnull
+     */
     @Test
     @Throws(Exception::class)
     fun checkFragmentNotNull() {
         Assert.assertNotNull(fragment)
     }
 
+    /**
+     * test onViewCreated
+     */
     @Test
     @Throws(Exception::class)
     fun testOnViewCreated() {
         fragment.onViewCreated(view, savedInstanceState)
     }
 
+    /**
+     * test onResume
+     */
     @Test
     @Throws(Exception::class)
     fun testOnResume() {
