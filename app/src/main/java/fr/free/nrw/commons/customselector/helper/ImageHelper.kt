581b9122b1488bdc036cb73e3ddfce80cb17d5b8
Aditya Srivastava <iamaditya2009@gmail.com>
2021-06-11 07:26:12 +0530

    Added basic Fetch

---
+++ app/src/main/java/fr/free/nrw/commons/customselector/helper/ImageHelper.kt
@@ -0,0 +1,34 @@
+package fr.free.nrw.commons.customselector.helper
+
+import fr.free.nrw.commons.customselector.model.Folder
+import fr.free.nrw.commons.customselector.model.Image
+
+object ImageHelper {
+
+    fun folderListFromImages(images: List<Image>): List<Folder> {
+        val folderMap: MutableMap<Long, Folder> = LinkedHashMap()
+        for (image in images) {
+            val bucketId = image.bucketId
+            val bucketName = image.bucketName
+            var folder = folderMap[bucketId]
+            if (folder == null) {
+                folder = Folder(bucketId, bucketName)
+                folderMap[bucketId] = folder
+            }
+        }
+        return ArrayList(folderMap.values)
+    }
+
+    fun filterImages(images: ArrayList<Image>, bukketId: Long?): ArrayList<Image> {
+        if (bukketId == null) return images
+
+        val filteredImages = arrayListOf<Image>()
+        for (image in images) {
+            if (image.bucketId == bukketId) {
+                filteredImages.add(image)
+            }
+        }
+        return filteredImages
+    }
+
+}
