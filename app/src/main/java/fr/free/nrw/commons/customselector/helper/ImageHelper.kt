ba2c9294373939c3a66a7d547e5fa3cef9504ae1
Aditya Srivastava <iamaditya2009@gmail.com>
2021-06-12 18:58:07 +0530

    Added sha1 function

--- app/src/main/java/fr/free/nrw/commons/customselector/helper/ImageHelper.kt
+++ app/src/main/java/fr/free/nrw/commons/customselector/helper/ImageHelper.kt
@@ -2,6 +2,13 @@ package fr.free.nrw.commons.customselector.helper
 
 import fr.free.nrw.commons.customselector.model.Folder
 import fr.free.nrw.commons.customselector.model.Image
+import timber.log.Timber
+import java.io.*
+import java.math.BigInteger
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import kotlin.collections.ArrayList
+import kotlin.collections.LinkedHashMap
 
 object ImageHelper {
 
@@ -30,6 +37,42 @@ object ImageHelper {
             }
         }
         return filteredImages
+    }
+
+    fun generateSHA1(`is`: InputStream): String {
+        val digest: MessageDigest = try {
+            MessageDigest.getInstance("SHA1")
+        } catch (e: NoSuchAlgorithmException) {
+            Timber.e(e, "Exception while getting Digest")
+            return ""
+        }
+        val buffer = ByteArray(8192)
+        var read: Int
+        return try {
+            while (`is`.read(buffer).also { read = it } > 0) {
+                digest.update(buffer, 0, read)
+            }
+            val md5sum = digest.digest()
+            val bigInt = BigInteger(1, md5sum)
+            var output = bigInt.toString(16)
+            output = String.format("%40s", output).replace(' ', '0')
+            Timber.i("File SHA1: %s", output)
+            output
+        } catch (e: IOException) {
+            Timber.e(e, "IO Exception")
+            ""
+        } finally {
+            try {
+                `is`.close()
+            } catch (e: IOException) {
+                Timber.e(e, "Exception on closing input stream")
+            }
+        }
+    }
+
+    @Throws(FileNotFoundException::class)
+    fun getFileInputStream(filePath: String?): InputStream {
+        return FileInputStream(filePath)
     }
 
 }
