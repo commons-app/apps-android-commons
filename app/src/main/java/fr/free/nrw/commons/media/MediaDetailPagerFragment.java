b39366616126e8be0e9d068b9a0b67c0856960b3
Madhur Gupta <madhurgupta10@gmail.com>
2021-06-28 22:21:33 +0530

    Fix #4482

--- app/src/main/java/fr/free/nrw/commons/media/MediaDetailPagerFragment.java
+++ app/src/main/java/fr/free/nrw/commons/media/MediaDetailPagerFragment.java
@@ -13,6 +13,8 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
@@ -90,7 +92,13 @@ public class MediaDetailPagerFragment extends CommonsDaggerSupportFragment imple
         pager.addOnPageChangeListener(this);
 
         adapter = new MediaDetailAdapter(getChildFragmentManager());
-        ((BaseActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        if (getActivity() != null) {
+            final ActionBar actionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.setDisplayHomeAsUpEnabled(true);
+            }
+        }
 
         if (savedInstanceState != null) {
             final int pageNumber = savedInstanceState.getInt("current-page");
