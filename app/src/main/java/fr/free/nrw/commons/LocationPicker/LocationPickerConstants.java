e9a2c32527fd9c50397a6fab392da83145076f4f
Ayan Sarkar <71203077+Ayan-10@users.noreply.github.com>
2021-06-29 14:32:20 +0530

    Added option to show and modify location while uploading (#4475)
    * initial commit
    
    * Everything done
    
    * minor modification
    
    * minor modification
    
    * Issues fixed
    
    * minor modifications
    
    * issue fixed
    
    * Issues fixed

--- app/src/main/java/fr/free/nrw/commons/LocationPicker/LocationPickerConstants.java
+++ app/src/main/java/fr/free/nrw/commons/LocationPicker/LocationPickerConstants.java
@@ -5,8 +5,12 @@ package fr.free.nrw.commons.LocationPicker;
  */
 public final class LocationPickerConstants {
 
+    public static final String ACTIVITY_KEY
+        = "location.picker.activity";
+
     public static final String MAP_CAMERA_POSITION
         = "location.picker.cameraPosition";
+
 
     private LocationPickerConstants() {
     }
