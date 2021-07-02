9ea626e2d20d0dcfe5d321cb4def7ce740df9621
Ayan <ayansarkar1810@gmail.com>
2021-07-03 01:08:21 +0530

    Share added

--- app/src/main/java/fr/free/nrw/commons/profile/ProfileActivity.java
+++ app/src/main/java/fr/free/nrw/commons/profile/ProfileActivity.java
@@ -1,9 +1,20 @@
 package fr.free.nrw.commons.profile;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.viewpager.widget.ViewPager;
@@ -11,11 +22,15 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.google.android.material.tabs.TabLayout;
 import fr.free.nrw.commons.R;
+import fr.free.nrw.commons.Utils;
 import fr.free.nrw.commons.ViewPagerAdapter;
 import fr.free.nrw.commons.auth.SessionManager;
 import fr.free.nrw.commons.profile.achievements.AchievementsFragment;
 import fr.free.nrw.commons.profile.leaderboard.LeaderboardFragment;
 import fr.free.nrw.commons.theme.BaseActivity;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Inject;
@@ -33,8 +48,10 @@ public class ProfileActivity extends BaseActivity {
 
     @BindView(R.id.tab_layout)
     TabLayout tabLayout;
+
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+
     @Inject
     SessionManager sessionManager;
 
@@ -96,4 +113,77 @@ public class ProfileActivity extends BaseActivity {
         compositeDisposable.clear();
     }
 
+    /**
+     * To inflate menu
+     * @param menu Menu
+     * @return boolean
+     */
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        final MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_about, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * To receive the id of selected item and handle further logic for that selected item
+     * @param item MenuItem
+     * @return boolean
+     */
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        final int id = item.getItemId();
+        // take screenshot in form of bitmap and show it in Alert Dialog
+        if (id == R.id.share_app_icon) {
+            final View rootView = getWindow().getDecorView().findViewById(android.R.id.content);
+            final Bitmap screenShot = Utils.getScreenShot(rootView);
+            showAlert(screenShot);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * It displays the alertDialog with Image of screenshot
+     * @param screenshot screenshot of the present screen
+     */
+    public void showAlert(final Bitmap screenshot){
+        final AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        final LayoutInflater factory = LayoutInflater.from(this);
+        final View view = factory.inflate(R.layout.image_alert_layout, null);
+        final ImageView screenShotImage = view.findViewById(R.id.alert_image);
+        screenShotImage.setImageBitmap(screenshot);
+        final TextView shareMessage = view.findViewById(R.id.alert_text);
+        shareMessage.setText(R.string.achievements_share_message);
+        alert.setView(view);
+        alert.setPositiveButton(R.string.about_translate_proceed, (dialog, which) -> shareScreen(screenshot));
+        alert.setNegativeButton(android.R.string.cancel, (dialog, which) -> dialog.cancel());
+        alert.show();
+    }
+
+    /**
+     * To take bitmap and store it temporary storage and share it
+     * @param bitmap bitmap of screenshot
+     */
+    void shareScreen(final Bitmap bitmap) {
+        try {
+            final File file = new File(getExternalCacheDir(), "screen.png");
+            final FileOutputStream fileOutputStream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fileOutputStream);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+            file.setReadable(true, false);
+
+            final Uri fileUri = FileProvider
+                .getUriForFile(getApplicationContext(), getPackageName()+".provider", file);
+            grantUriPermission(getPackageName(), fileUri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            final Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.putExtra(Intent.EXTRA_STREAM, fileUri);
+            intent.setType("image/png");
+            startActivity(Intent.createChooser(intent, getString(R.string.share_image_via)));
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
