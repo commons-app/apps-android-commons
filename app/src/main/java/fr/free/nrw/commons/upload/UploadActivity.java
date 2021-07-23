2a9ae73750e3f8df29cba9ad7662c52a7e257831
benryder1988 <benryder1988.br@gmail.com>
2021-07-23 12:40:41 +0100

    Fixes #4455 - Warning when pressing "back" in upload wizard (#4519)
    * Overrides the back button during upload with dialog
    
    * Removing unneeded import

--- app/src/main/java/fr/free/nrw/commons/upload/UploadActivity.java
+++ app/src/main/java/fr/free/nrw/commons/upload/UploadActivity.java
@@ -45,6 +45,7 @@ import fr.free.nrw.commons.upload.license.MediaLicenseFragment;
 import fr.free.nrw.commons.upload.mediaDetails.UploadMediaDetailFragment;
 import fr.free.nrw.commons.upload.mediaDetails.UploadMediaDetailFragment.UploadMediaDetailFragmentCallback;
 import fr.free.nrw.commons.upload.worker.UploadWorker;
+import fr.free.nrw.commons.utils.DialogUtil;
 import fr.free.nrw.commons.utils.PermissionUtils;
 import fr.free.nrw.commons.utils.ViewUtil;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -502,4 +503,20 @@ public class UploadActivity extends BaseActivity implements UploadContract.View,
             uploadCategoriesFragment.setCallback(null);
         }
     }
+
+    /**
+     * Overrides the back button to make sure the user is prepared to lose their progress
+     */
+    @Override
+    public void onBackPressed() {
+        DialogUtil.showAlertDialog(this,
+            getString(R.string.back_button_warning),
+            getString(R.string.back_button_warning_desc),
+            getString(R.string.back_button_continue),
+            getString(R.string.back_button_warning),
+            null,
+            this::finish
+        );
+    }
+
 }
