2a9f5ed6f7524af1d71a5ab049990808e0b6e080
Aditya-Srivastav <54016427+4D17Y4@users.noreply.github.com>
2021-07-24 14:09:59 +0530

    ImageLoader Test Updated (#4517)

--- app/src/main/java/fr/free/nrw/commons/customselector/database/UploadedDao.kt
+++ app/src/main/java/fr/free/nrw/commons/customselector/database/UploadedDao.kt
@@ -1,9 +1,7 @@
 package fr.free.nrw.commons.customselector.database
 
 import androidx.room.*
-import kotlinx.coroutines.runBlocking
 import java.util.*
-import kotlinx.coroutines.*
 
 /**
  * UploadedStatusDao for Custom Selector.
@@ -30,57 +28,29 @@ abstract class UploadedStatusDao {
     abstract suspend fun delete(uploadedStatus: UploadedStatus)
 
     /**
-     * Get All entries from the uploaded status table.
-     */
-    @Query("SELECT * FROM uploaded_table")
-    abstract suspend fun getAll() : List<UploadedStatus>
-
-    /**
      * Query uploaded status with image sha1.
      */
     @Query("SELECT * FROM uploaded_table WHERE imageSHA1 = (:imageSHA1) ")
-    abstract suspend fun getFromImageSHA1(imageSHA1 : String) : UploadedStatus
+    abstract suspend fun getFromImageSHA1(imageSHA1 : String) : UploadedStatus?
 
     /**
      * Query uploaded status with modified image sha1.
      */
     @Query("SELECT * FROM uploaded_table WHERE modifiedImageSHA1 = (:modifiedImageSHA1) ")
-    abstract suspend fun getFromModifiedImageSHA1(modifiedImageSHA1 : String) : UploadedStatus
+    abstract suspend fun getFromModifiedImageSHA1(modifiedImageSHA1 : String) : UploadedStatus?
 
     /**
      * Asynchronous insert into uploaded status table.
      */
     suspend fun insertUploaded(uploadedStatus: UploadedStatus) {
-        uploadedStatus.lastUpdated = Calendar.getInstance().time as Date?
+        uploadedStatus.lastUpdated = Calendar.getInstance().time
         insert(uploadedStatus)
-    }
-
-    /**
-     * Asynchronous delete from uploaded status table.
-     */
-    suspend fun deleteUploaded(uploadedStatus: UploadedStatus) {
-        delete(uploadedStatus)
-    }
-
-    /**
-     * Asynchronous update entry in uploaded status table.
-     */
-    suspend fun updateUploaded(uploadedStatus: UploadedStatus) {
-        update(uploadedStatus)
     }
 
     /**
      * Asynchronous image sha1 query.
      */
-    suspend fun getUploadedFromImageSHA1(imageSHA1: String):UploadedStatus {
+    suspend fun getUploadedFromImageSHA1(imageSHA1: String):UploadedStatus? {
         return getFromImageSHA1(imageSHA1)
     }
-
-    /**
-     * Asynchronous modified image sha1 query.
-     */
-    suspend fun getUploadedFromModifiedImageSHA1(modifiedImageSHA1: String):UploadedStatus {
-        return getFromModifiedImageSHA1(modifiedImageSHA1)
-    }
-
 }
