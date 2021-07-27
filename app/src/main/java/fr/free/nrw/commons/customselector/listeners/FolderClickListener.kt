771a7314995f18e3924042930b224dd1dee550b5
Aditya-Srivastav <54016427+4D17Y4@users.noreply.github.com>
2021-07-27 05:50:49 +0530

    Saving selector state (#4526)

--- app/src/main/java/fr/free/nrw/commons/customselector/listeners/FolderClickListener.kt
+++ app/src/main/java/fr/free/nrw/commons/customselector/listeners/FolderClickListener.kt
@@ -1,7 +1,5 @@
 package fr.free.nrw.commons.customselector.listeners
 
-import fr.free.nrw.commons.customselector.model.Folder
-
 interface FolderClickListener {
-    fun onFolderClick(folder : Folder)
+    fun onFolderClick(folderId: Long, folderName: String)
 }
