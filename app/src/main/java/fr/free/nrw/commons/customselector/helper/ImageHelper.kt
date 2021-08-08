70acd7b28166aeb5201680966537926238da5286
Aditya-Srivastav <54016427+4D17Y4@users.noreply.github.com>
2021-08-08 18:38:11 +0530

    [GSoC] Saved Image Fragment Scroll State (#4528)
    * Saved Image Fragment Scroll State
    
    * Fix delete image
    
    * Fixed Delete bug
    
    * Changed custom selector icon

--- app/src/main/java/fr/free/nrw/commons/customselector/helper/ImageHelper.kt
+++ app/src/main/java/fr/free/nrw/commons/customselector/helper/ImageHelper.kt
@@ -1,19 +1,7 @@
 package fr.free.nrw.commons.customselector.helper
 
-import android.content.Context
-import com.mapbox.android.core.FileUtils
 import fr.free.nrw.commons.customselector.model.Folder
 import fr.free.nrw.commons.customselector.model.Image
-import fr.free.nrw.commons.filepicker.Constants
-import timber.log.Timber
-import java.io.*
-import java.math.BigInteger
-import java.security.MessageDigest
-import java.security.NoSuchAlgorithmException
-import kotlin.collections.ArrayList
-import kotlin.collections.HashMap
-import kotlin.collections.LinkedHashMap
-
 
 /**
  * Image Helper object, includes all the static functions required by custom selector.
@@ -24,7 +12,7 @@ object ImageHelper {
     /**
      * Returns the list of folders from given image list.
      */
-    fun folderListFromImages(images: List<Image>): List<Folder> {
+    fun folderListFromImages(images: List<Image>): ArrayList<Folder> {
         val folderMap: MutableMap<Long, Folder> = LinkedHashMap()
         for (image in images) {
             val bucketId = image.bucketId
@@ -59,6 +47,17 @@ object ImageHelper {
      */
     fun getIndex(list: ArrayList<Image>, image: Image): Int {
         return list.indexOf(image)
+    }
+
+    /**
+     * getIndex: Returns the index of image in given list.
+     */
+    fun getIndexFromId(list: ArrayList<Image>, imageId: Long): Int {
+        for(i in list){
+            if(i.id == imageId)
+                return list.indexOf(i)
+        }
+        return 0;
     }
 
     /**
