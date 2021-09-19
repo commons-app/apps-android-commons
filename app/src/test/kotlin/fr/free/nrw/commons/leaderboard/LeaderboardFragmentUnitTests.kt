080f25ed74dc6d72fa2b78cc2d5e695f8ec14a33
Madhur Gupta <madhurgupta10@gmail.com>
2021-09-19 17:12:39 +0530

    Add LeaderboardFragment Unit Tests

---
+++ app/src/test/kotlin/fr/free/nrw/commons/leaderboard/LeaderboardFragmentUnitTests.kt
@@ -0,0 +1,207 @@
+package fr.free.nrw.commons.leaderboard
+
+import android.accounts.Account
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.Spinner
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import androidx.recyclerview.widget.RecyclerView
+import com.facebook.drawee.backends.pipeline.Fresco
+import com.facebook.soloader.SoLoader
+import fr.free.nrw.commons.R
+import fr.free.nrw.commons.TestAppAdapter
+import fr.free.nrw.commons.TestCommonsApplication
+import fr.free.nrw.commons.auth.SessionManager
+import fr.free.nrw.commons.profile.ProfileActivity
+import fr.free.nrw.commons.profile.leaderboard.LeaderboardFragment
+import fr.free.nrw.commons.profile.leaderboard.LeaderboardListAdapter
+import fr.free.nrw.commons.profile.leaderboard.LeaderboardListViewModel
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
+import org.powermock.reflect.Whitebox
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.LooperMode
+import org.wikipedia.AppAdapter
+import java.lang.reflect.Method
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [21], application = TestCommonsApplication::class)
+@LooperMode(LooperMode.Mode.PAUSED)
+class LeaderboardFragmentUnitTests {
+
+    private lateinit var fragment: LeaderboardFragment
+    private lateinit var fragmentManager: FragmentManager
+    private lateinit var context: Context
+    private lateinit var view: View
+    private lateinit var layoutInflater: LayoutInflater
+
+    @Mock
+    private lateinit var progressBar: ProgressBar
+
+    @Mock
+    private lateinit var spinner: Spinner
+
+    @Mock
+    private lateinit var viewModel: LeaderboardListViewModel
+
+    @Mock
+    private lateinit var recyclerView: RecyclerView
+
+    @Mock
+    private lateinit var adapter: LeaderboardListAdapter
+
+    @Mock
+    private lateinit var sessionManager: SessionManager
+
+    @Mock
+    private lateinit var account: Account
+
+    @Mock
+    private lateinit var button: Button
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        context = RuntimeEnvironment.application.applicationContext
+
+        AppAdapter.set(TestAppAdapter())
+        SoLoader.setInTestMode()
+        Fresco.initialize(RuntimeEnvironment.application.applicationContext)
+
+        val activity = Robolectric.buildActivity(ProfileActivity::class.java).create().get()
+        fragment = LeaderboardFragment()
+        fragmentManager = activity.supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(fragment, null)
+        fragmentTransaction.commitNowAllowingStateLoss()
+
+        layoutInflater = LayoutInflater.from(activity)
+        view = LayoutInflater.from(activity)
+            .inflate(R.layout.fragment_leaderboard, null) as View
+
+        Whitebox.setInternalState(fragment, "progressBar", progressBar)
+        Whitebox.setInternalState(fragment, "categorySpinner", spinner)
+        Whitebox.setInternalState(fragment, "durationSpinner", spinner)
+        Whitebox.setInternalState(fragment, "viewModel", viewModel)
+        Whitebox.setInternalState(fragment, "scrollButton", button)
+        Whitebox.setInternalState(fragment, "leaderboardListRecyclerView", recyclerView)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun checkFragmentNotNull() {
+        Assert.assertNotNull(fragment)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testOnCreateView() {
+        fragment.onCreateView(layoutInflater, null, null)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testRefreshLeaderboard() {
+        val method: Method = LeaderboardFragment::class.java.getDeclaredMethod(
+            "refreshLeaderboard"
+        )
+        method.isAccessible = true
+        method.invoke(fragment)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testScrollToUserRank() {
+        val method: Method = LeaderboardFragment::class.java.getDeclaredMethod(
+            "scrollToUserRank"
+        )
+        method.isAccessible = true
+        method.invoke(fragment)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testScrollToUserRankCaseNonZeroTrue() {
+        Whitebox.setInternalState(fragment, "userRank", 1)
+        `when`(recyclerView.adapter).thenReturn(adapter)
+        `when`(adapter.itemCount).thenReturn(3)
+        val method: Method = LeaderboardFragment::class.java.getDeclaredMethod(
+            "scrollToUserRank"
+        )
+        method.isAccessible = true
+        method.invoke(fragment)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testScrollToUserRankCaseNonZeroFalse() {
+        Whitebox.setInternalState(fragment, "userRank", 1)
+        `when`(recyclerView.adapter).thenReturn(adapter)
+        `when`(adapter.itemCount).thenReturn(1)
+        val method: Method = LeaderboardFragment::class.java.getDeclaredMethod(
+            "scrollToUserRank"
+        )
+        method.isAccessible = true
+        method.invoke(fragment)
+    }
+
+
+    @Test
+    @Throws(Exception::class)
+    fun testSetLeaderboard() {
+        Whitebox.setInternalState(fragment, "sessionManager", sessionManager)
+        `when`(sessionManager.currentAccount).thenReturn(account)
+        val method: Method = LeaderboardFragment::class.java.getDeclaredMethod(
+            "setLeaderboard",
+            String::class.java,
+            String::class.java,
+            Int::class.java,
+            Int::class.java
+        )
+        method.isAccessible = true
+        method.invoke(fragment, "", "", 0, 0)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testHideProgressBar() {
+        val method: Method = LeaderboardFragment::class.java.getDeclaredMethod(
+            "hideProgressBar"
+        )
+        method.isAccessible = true
+        method.invoke(fragment)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testShowProgressBar() {
+        val method: Method = LeaderboardFragment::class.java.getDeclaredMethod(
+            "showProgressBar"
+        )
+        method.isAccessible = true
+        method.invoke(fragment)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testOnError() {
+        val method: Method = LeaderboardFragment::class.java.getDeclaredMethod(
+            "onError"
+        )
+        method.isAccessible = true
+        method.invoke(fragment)
+    }
+
+}
