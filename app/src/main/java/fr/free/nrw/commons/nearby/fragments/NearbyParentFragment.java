a53207e11f8ee0bc1f25c3715fe9ceda3ffa428a
pratik2315 <pratikhaldankar360@gmail.com>
2021-09-21 10:52:20 +0530

    Added multi-file picker functionality for the WLM

--- app/src/main/java/fr/free/nrw/commons/nearby/fragments/NearbyParentFragment.java
+++ app/src/main/java/fr/free/nrw/commons/nearby/fragments/NearbyParentFragment.java
@@ -1623,6 +1623,7 @@ public class NearbyParentFragment extends CommonsDaggerSupportFragment
             if (fabGallery.isShown()) {
                 Timber.d("Gallery button tapped. Place: %s", selectedPlace.toString());
                 storeSharedPrefs(selectedPlace);
+                
                 controller.initiateGalleryPick(getActivity(), chipWlm.isChecked());
             }
         });
