6649da88a596a77fa21840a6e2a57a320582b773
dado1111 <92014420+dado1111@users.noreply.github.com>
2021-10-17 10:24:18 +0200

    Clicking "More" on tabLayout caused it to be displayed on toolbar (in any other language than English) (#4666)
    * Changed hardcoded "More" to getStrings(R.string.more) for Unlogged user
    - MainActivity toolbar showed "More" when clicked on More options
       in other language than English
    
    * Changed hardcoded "More" to getStrings(R.string.more) for Logged user
    - MainActivity toolbar showed "More" when clicked on More options
       in other language than English
    
    * Added test for: MainActivity.setUpPager

--- app/src/test/kotlin/fr/free/nrw/commons/contributions/MainActivityUnitTests.kt
+++ app/src/test/kotlin/fr/free/nrw/commons/contributions/MainActivityUnitTests.kt
@@ -212,6 +212,16 @@ class MainActivityUnitTests {
 
     @Test
     @Throws(Exception::class)
+    fun testSetUpPager() {
+        val method: Method = MainActivity::class.java.getDeclaredMethod(
+            "setUpPager"
+        )
+        method.isAccessible = true
+        method.invoke(activity)
+    }
+
+    @Test
+    @Throws(Exception::class)
     fun testSetUpLoggedOutPager() {
         val method: Method = MainActivity::class.java.getDeclaredMethod(
             "setUpLoggedOutPager"
