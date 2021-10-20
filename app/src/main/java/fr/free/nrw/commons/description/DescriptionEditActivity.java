6060087b738f0762ce2d8cd87bfda52cf5b31818
ayans <ayansarkar1810@gmail.com>
2021-10-20 21:47:29 +0530

    Problem with ButterKnife

--- app/src/main/java/fr/free/nrw/commons/description/DescriptionEditActivity.java
+++ app/src/main/java/fr/free/nrw/commons/description/DescriptionEditActivity.java
@@ -6,6 +6,7 @@ import static fr.free.nrw.commons.description.EditDescriptionConstants.WIKITEXT;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -101,10 +102,10 @@ public class DescriptionEditActivity extends AppCompatActivity implements
 
     }
 
-    @OnClick(R.id.mapbox_place_picker_toolbar_back_button)
-    public void onBackButtonClicked(){
-        onBackPressed();
-    }
+//    @OnClick(R.id.mapbox_place_picker_toolbar_back_button)
+//    public void onBackButtonClicked(){
+//        onBackPressed();
+//    }
 
     @OnClick(R.id.btn_add_description)
     public void onButtonAddDescriptionClicked() {
@@ -145,6 +146,9 @@ public class DescriptionEditActivity extends AppCompatActivity implements
             buffer.append(descriptionStart);
             for (int i=0; i<uploadMediaDetails.size(); i++) {
                 final UploadMediaDetail uploadDetails = uploadMediaDetails.get(i);
+                Log.d("TAG", "updateDescription: "+uploadDetails.getDescriptionText());
+                Log.d("TAG", "updateDescription1: "+uploadDetails.getCaptionText());
+                Log.d("TAG", "updateDescription:2 "+uploadDetails.getLanguageCode());
                 if (!uploadDetails.getDescriptionText().equals("")) {
                     if (i == uploadMediaDetails.size() - 1) {
                         buffer.append("{{");
