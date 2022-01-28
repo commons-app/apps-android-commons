a7d04ced595052edbc2bd26e52f721f69e73934e
Madhur Gupta <madhurgupta10@gmail.com>
2022-01-28 10:53:11 +0530

    Add assertion and verifications

--- app/src/test/kotlin/fr/free/nrw/commons/description/DescriptionEditActivityUnitTest.kt
+++ app/src/test/kotlin/fr/free/nrw/commons/description/DescriptionEditActivityUnitTest.kt
@@ -1,9 +1,12 @@
 package fr.free.nrw.commons.description
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.app.ProgressDialog
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
@@ -14,21 +17,26 @@ import fr.free.nrw.commons.description.EditDescriptionConstants.LIST_OF_DESCRIPT
 import fr.free.nrw.commons.description.EditDescriptionConstants.WIKITEXT
 import fr.free.nrw.commons.upload.UploadMediaDetail
 import fr.free.nrw.commons.upload.UploadMediaDetailAdapter
+import junit.framework.Assert.assertEquals
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
 import org.mockito.MockitoAnnotations
 import org.powermock.reflect.Whitebox
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
+import org.robolectric.Shadows
 import org.robolectric.annotation.Config
 import org.robolectric.annotation.LooperMode
+import org.robolectric.shadows.ShadowAlertDialog
+import org.robolectric.shadows.ShadowProgressDialog
 import java.lang.reflect.Method
-import java.util.ArrayList
+import java.util.*
 
 @RunWith(RobolectricTestRunner::class)
 @Config(sdk = [21], application = TestCommonsApplication::class)
@@ -39,9 +47,6 @@ class DescriptionEditActivityUnitTest {
     private lateinit var activity: Activity
     private lateinit var uploadMediaDetails: ArrayList<UploadMediaDetail>
     private lateinit var binding: ActivityDescriptionEditBinding
-
-    @Mock
-    private lateinit var bundle: Bundle
 
     @Mock
     private lateinit var uploadMediaDetailAdapter: UploadMediaDetailAdapter
@@ -62,7 +67,7 @@ class DescriptionEditActivityUnitTest {
         bundle.putString(WIKITEXT, "desc")
         intent.putExtras(bundle)
         activity =
-            Robolectric.buildActivity(DescriptionEditActivity::class.java, intent).get()
+            Robolectric.buildActivity(DescriptionEditActivity::class.java, intent).create().get()
         binding = ActivityDescriptionEditBinding.inflate(LayoutInflater.from(activity))
         activity.setContentView(R.layout.activity_description_edit)
 
@@ -81,68 +86,69 @@ class DescriptionEditActivityUnitTest {
 
     @Test
     @Throws(Exception::class)
-    fun testOnCreate() {
-        val method: Method = DescriptionEditActivity::class.java.getDeclaredMethod(
-            "onCreate", Bundle::class.java
-        )
-        method.isAccessible = true
-        method.invoke(activity, bundle)
-    }
-
-
-    @Test
-    @Throws(Exception::class)
     fun testShowLoggingProgressBar() {
+        Shadows.shadowOf(Looper.getMainLooper()).idle()
         val method: Method = DescriptionEditActivity::class.java.getDeclaredMethod(
             "showLoggingProgressBar"
         )
         method.isAccessible = true
         method.invoke(activity)
+        val dialog: ProgressDialog = ShadowProgressDialog.getLatestDialog() as ProgressDialog
+        assertEquals(dialog.isShowing, true)
     }
 
     @Test
     @Throws(Exception::class)
     fun testUpdateDescription() {
+        Shadows.shadowOf(Looper.getMainLooper()).idle()
         val method: Method = DescriptionEditActivity::class.java.getDeclaredMethod(
             "updateDescription", List::class.java
         )
         method.isAccessible = true
         method.invoke(activity, mutableListOf(UploadMediaDetail("en", "desc")))
+        assertEquals(activity.isFinishing, true)
     }
 
     @Test
     @Throws(Exception::class)
     fun testOnSubmitButtonClicked() {
+        Shadows.shadowOf(Looper.getMainLooper()).idle()
         val method: Method = DescriptionEditActivity::class.java.getDeclaredMethod(
             "onSubmitButtonClicked", View::class.java
         )
         method.isAccessible = true
         method.invoke(activity, null)
+        assertEquals(activity.isFinishing, true)
     }
 
     @Test
     @Throws(Exception::class)
     fun testOnButtonAddDescriptionClicked() {
+        Shadows.shadowOf(Looper.getMainLooper()).idle()
         val method: Method = DescriptionEditActivity::class.java.getDeclaredMethod(
             "onButtonAddDescriptionClicked", View::class.java
         )
         method.isAccessible = true
         method.invoke(activity, null)
+        verify(uploadMediaDetailAdapter).addDescription(UploadMediaDetail())
     }
 
     @Test
     @Throws(Exception::class)
     fun testOnBackButtonClicked() {
+        Shadows.shadowOf(Looper.getMainLooper()).idle()
         val method: Method = DescriptionEditActivity::class.java.getDeclaredMethod(
             "onBackButtonClicked", View::class.java
         )
         method.isAccessible = true
         method.invoke(activity, null)
+        assertEquals(activity.isFinishing, true)
     }
 
     @Test
     @Throws(Exception::class)
     fun testOnPrimaryCaptionTextChange() {
+        Shadows.shadowOf(Looper.getMainLooper()).idle()
         val method: Method = DescriptionEditActivity::class.java.getDeclaredMethod(
             "onPrimaryCaptionTextChange", Boolean::class.java
         )
@@ -153,6 +159,7 @@ class DescriptionEditActivityUnitTest {
     @Test
     @Throws(Exception::class)
     fun testShowInfoAlert() {
+        Shadows.shadowOf(Looper.getMainLooper()).idle()
         val method: Method = DescriptionEditActivity::class.java.getDeclaredMethod(
             "showInfoAlert", Int::class.java, Int::class.java
         )
@@ -162,19 +169,8 @@ class DescriptionEditActivityUnitTest {
             android.R.string.ok,
             android.R.string.ok
         )
-    }
-
-    @Test
-    @Throws(Exception::class)
-    fun testInitRecyclerView() {
-        val method: Method = DescriptionEditActivity::class.java.getDeclaredMethod(
-            "initRecyclerView", ArrayList::class.java
-        )
-        method.isAccessible = true
-        method.invoke(
-            activity,
-            uploadMediaDetails
-        )
+        val dialog: AlertDialog = ShadowAlertDialog.getLatestDialog() as AlertDialog
+        assertEquals(dialog.isShowing, true)
     }
 
 }
