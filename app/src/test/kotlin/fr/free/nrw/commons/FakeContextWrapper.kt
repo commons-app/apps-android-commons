fd0dbf4cfda3479138b0a0c485ebc1381c9d0a77
Madhur Gupta <30932899+madhurgupta10@users.noreply.github.com>
2022-02-15 15:10:13 +0530

    Add AccountUtil Unit Tests (#4814)

---
+++ app/src/test/kotlin/fr/free/nrw/commons/FakeContextWrapper.kt
@@ -0,0 +1,33 @@
+package fr.free.nrw.commons
+
+import android.accounts.Account
+import android.accounts.AccountManager
+import android.content.Context
+import android.content.ContextWrapper
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+
+class FakeContextWrapper(base: Context?) : ContextWrapper(base) {
+
+    @Mock
+    private lateinit var mMockAccountManager: AccountManager
+
+    override fun getSystemService(name: String): Any {
+        return if (ACCOUNT_SERVICE == name) {
+            mMockAccountManager
+        } else super.getSystemService(name)
+    }
+
+    companion object {
+        private val ACCOUNT = Account("test@example.com", BuildConfig.ACCOUNT_TYPE)
+        private val ACCOUNTS = arrayOf(ACCOUNT)
+    }
+
+    init {
+        MockitoAnnotations.initMocks(this)
+        Mockito.`when`(mMockAccountManager.accounts).thenReturn(ACCOUNTS)
+        Mockito.`when`(mMockAccountManager.getAccountsByType(BuildConfig.ACCOUNT_TYPE))
+            .thenReturn(ACCOUNTS)
+    }
+}
