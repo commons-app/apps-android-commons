4a5f65662b2f379b7f4dc9935cb59b9332d23623
Madhur Gupta <madhurgupta10@gmail.com>
2022-02-18 13:59:35 +0530

    Fix SignupTest Instrumentation Tests

--- app/src/androidTest/java/fr/free/nrw/commons/SignupTest.kt
+++ app/src/androidTest/java/fr/free/nrw/commons/SignupTest.kt
@@ -2,51 +2,43 @@ package fr.free.nrw.commons
 
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.rule.ActivityTestRule
-import androidx.test.runner.AndroidJUnit4
 import fr.free.nrw.commons.auth.LoginActivity
 import fr.free.nrw.commons.auth.SignupActivity
+import org.junit.After
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
+@RunWith(AndroidJUnit4ClassRunner::class)
 class SignupTest {
     @get:Rule
     var activityRule: ActivityTestRule<*> = ActivityTestRule(LoginActivity::class.java)
 
     @Before
     fun setup() {
+        Intents.init()
         UITestHelper.skipWelcome()
     }
 
-    @Test
-    @Ignore("Fix Failing Test")
-    fun testSignupButton() {
-        try {
-            Intents.init()
-        } catch (ex: IllegalStateException) {
-
-        }
-
-        UITestHelper.sleep(3000)
-        Espresso.onView(withId(R.id.sign_up_button))
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-                .perform(click())
-        intended(hasComponent(SignupActivity::class.java.name))
+    @After
+    fun cleanUp() {
         Intents.release()
     }
 
     @Test
-    @Ignore("Fix Failing Test")
+    fun testSignupButton() {
+        Espresso.onView(withId(R.id.sign_up_button)).perform(click())
+        intended(hasComponent(SignupActivity::class.java.name))
+    }
+
+    @Test
     fun orientationChange() {
         UITestHelper.changeOrientation(activityRule)
     }
