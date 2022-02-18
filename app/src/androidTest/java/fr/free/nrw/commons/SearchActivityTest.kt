d0c51912d9963d97ea597b9e668d24efaf36c450
Madhur Gupta <madhurgupta10@gmail.com>
2022-02-18 13:54:11 +0530

    Fix SearchActivity Instrumentation Tests

--- app/src/androidTest/java/fr/free/nrw/commons/SearchActivityTest.kt
+++ app/src/androidTest/java/fr/free/nrw/commons/SearchActivityTest.kt
@@ -1,13 +1,13 @@
 package fr.free.nrw.commons
 
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.rule.ActivityTestRule
-import androidx.test.runner.AndroidJUnit4
+import fr.free.nrw.commons.explore.SearchActivity
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import fr.free.nrw.commons.explore.SearchActivity
 
-@RunWith(AndroidJUnit4::class)
+@RunWith(AndroidJUnit4ClassRunner::class)
 class SearchActivityTest {
     @get:Rule
     var activityRule = ActivityTestRule(SearchActivity::class.java)
