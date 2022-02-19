608c217f20f247176f32ba26923ec16bf894e5c3
devarsh-mavani-19 <devarshmavani19@gmail.com>
2022-02-19 14:48:00 +0530

    removed system logs

--- app/src/main/java/fr/free/nrw/commons/delete/DeleteHelper.java
+++ app/src/main/java/fr/free/nrw/commons/delete/DeleteHelper.java
@@ -83,7 +83,6 @@ public class DeleteHelper {
                 "|year=" + calendar.get(Calendar.YEAR) +
                 "}}";
 
-        System.out.println("Delete: "+fileDeleteString);
 
         String subpageString = "=== [[:" + media.getFilename() + "]] ===\n" +
                 reason +
