fea6eb2552469492d203938b3cd644bd2be8eaa6
Madhur Gupta <madhurgupta10@gmail.com>
2022-02-20 21:13:11 +0530

    Change device to pixel 5

--- app/src/androidTest/java/fr/free/nrw/commons/ProfileActivityTest.kt
+++ app/src/androidTest/java/fr/free/nrw/commons/ProfileActivityTest.kt
@@ -68,22 +68,6 @@ class ProfileActivityTest {
         onView(isRoot()).perform(pressBack())
         device.setOrientationNatural()
         device.freezeRotation()
-        try {
-            onView(
-                Matchers.allOf(
-                    ViewMatchers.withContentDescription("More"),
-                    childAtPosition(
-                        childAtPosition(
-                            withId(R.id.fragment_main_nav_tab_layout),
-                            0
-                        ),
-                        4
-                    ),
-                    ViewMatchers.isDisplayed()
-                )
-            ).perform(ViewActions.click())
-        } catch (ignored: NoMatchingViewException) {
-        }
         onView(Matchers.allOf(withId(R.id.more_logout))).perform(
             ViewActions.scrollTo(),
             ViewActions.click()
