6a19ac663f266595085fd554ed2ee5edb8b4da40
devarsh-mavani-19 <devarshmavani19@gmail.com>
2022-03-12 12:24:50 +0530

    fixed tests

--- app/src/test/kotlin/fr/free/nrw/commons/feedback/FeedbackContentCreatorUnitTests.kt
+++ app/src/test/kotlin/fr/free/nrw/commons/feedback/FeedbackContentCreatorUnitTests.kt
@@ -1,23 +1,26 @@
 package fr.free.nrw.commons.feedback
 
+import android.content.Context
 import fr.free.nrw.commons.feedback.model.Feedback
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
+import org.robolectric.RuntimeEnvironment
 
 class FeedbackContentCreatorUnitTests {
     private lateinit var creator: FeedbackContentCreator
     private lateinit var feedback: Feedback
-
+    private lateinit var context: Context
+    
     @Before
     fun setup() {
-
+        context = RuntimeEnvironment.application.applicationContext
     }
 
     @Test
     fun testToString() {
         feedback = Feedback("123", "apiLevel", "title", "androidVersion", "deviceModel", "mfg", "deviceName", "wifi")
-        creator = FeedbackContentCreator(feedback)
+        creator = FeedbackContentCreator(context, feedback)
         Assert.assertNotNull(creator.toString())
     }
 
