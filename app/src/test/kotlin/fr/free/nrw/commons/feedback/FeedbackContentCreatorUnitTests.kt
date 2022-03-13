abc511b847b966fb119f66662fc0832c725db39f
devarsh-mavani-19 <devarshmavani19@gmail.com>
2022-03-13 17:03:29 +0530

    Fixed tests

--- app/src/test/kotlin/fr/free/nrw/commons/feedback/FeedbackContentCreatorUnitTests.kt
+++ app/src/test/kotlin/fr/free/nrw/commons/feedback/FeedbackContentCreatorUnitTests.kt
@@ -1,19 +1,36 @@
 package fr.free.nrw.commons.feedback
 
 import android.content.Context
+import fr.free.nrw.commons.FakeContextWrapper
+import fr.free.nrw.commons.TestAppAdapter
+import fr.free.nrw.commons.TestCommonsApplication
 import fr.free.nrw.commons.feedback.model.Feedback
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.doReturn
+import org.mockito.Mockito.mock
+import org.mockito.MockitoAnnotations
+import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
+import org.wikipedia.AppAdapter
 
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [21], application = TestCommonsApplication::class)
 class FeedbackContentCreatorUnitTests {
     private lateinit var creator: FeedbackContentCreator
     private lateinit var feedback: Feedback
+
+    @Mock
     private lateinit var context: Context
     
     @Before
     fun setup() {
+        MockitoAnnotations.initMocks(this)
+        AppAdapter.set(TestAppAdapter())
         context = RuntimeEnvironment.application.applicationContext
     }
 
