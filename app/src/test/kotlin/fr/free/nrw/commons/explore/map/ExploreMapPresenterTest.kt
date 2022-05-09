0eac233bab0e3e93c2282851f6c9eb0432eba7b3
neslihanturan <tur.neslihan@gmail.com>
2022-05-09 16:37:41 +0300

    Add some explore map presenter comments

--- app/src/test/kotlin/fr/free/nrw/commons/explore/map/ExploreMapPresenterTest.kt
+++ app/src/test/kotlin/fr/free/nrw/commons/explore/map/ExploreMapPresenterTest.kt
@@ -1,7 +1,12 @@
 package fr.free.nrw.commons.explore.map
 
+import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.verifyZeroInteractions
+import com.nhaarman.mockitokotlin2.whenever
 import fr.free.nrw.commons.bookmarks.locations.BookmarkLocationsDao
+import fr.free.nrw.commons.location.LatLng
+import fr.free.nrw.commons.location.LocationServiceManager
 
 import org.junit.Before
 import org.junit.Test
@@ -16,6 +21,8 @@ class ExploreMapPresenterTest {
     internal lateinit var exploreMapView: ExploreMapContract.View
     @Mock
     internal lateinit var bookmarkLocationsDao: BookmarkLocationsDao
+    @Mock
+    internal lateinit var latestLocation: LatLng
 
     private lateinit var exploreMapPresenter: ExploreMapPresenter
 
@@ -34,18 +41,54 @@ class ExploreMapPresenterTest {
     @Test
     fun initializeMapOperations() {
         exploreMapPresenter.initializeMapOperations()
-        testLockUnlockMap()
-        testUpdateMap()
         verify(exploreMapView).addSearchThisAreaButtonAction()
+        expectMapAndListUpdate()
+        exploreMapPresenter.updateMap(LocationServiceManager.LocationChangeType.LOCATION_SIGNIFICANTLY_CHANGED)
+        verify(exploreMapView).getLastLocation()
+        verify(exploreMapView).disableFABRecenter()
+        verify(exploreMapView).setProgressBarVisibility(true)
+        verify(exploreMapView).populatePlaces(latestLocation, latestLocation)
+    }
+
+    /**
+     * Test lockUnlockNearby method to lock nearby case
+     */
+    @Test
+    fun testLockUnlockNearbyForLocked() {
+        exploreMapPresenter.lockUnlockNearby(true)
+        verify(exploreMapView).disableFABRecenter()
+    }
+
+    /**
+     * Test lockUnlockNearby method to unlock nearby case
+     */
+    @Test
+    fun testLockUnlockNearbyForUnlocked() {
+        exploreMapPresenter.lockUnlockNearby(false)
+        verify(exploreMapView).enableFABRecenter()
     }
 
     @Test
-    fun testLockUnlockMap() {
-
+    fun testUpdateMapAndListWhenLocationLocked() {
+        exploreMapPresenter.lockUnlockNearby(true)
+        exploreMapPresenter.updateMap(LocationServiceManager.LocationChangeType.LOCATION_SIGNIFICANTLY_CHANGED)
+        verify(exploreMapView).disableFABRecenter()
+        verifyZeroInteractions(exploreMapView)
     }
 
     @Test
-    fun testUpdateMap() {
+    fun testUpdateMapAndListWhenLocationUnlocked() {
+        exploreMapPresenter.lockUnlockNearby(false)
+        whenever(exploreMapView.isNetworkConnectionEstablished()).thenReturn(false)
+        exploreMapPresenter.updateMap(LocationServiceManager.LocationChangeType.LOCATION_SIGNIFICANTLY_CHANGED)
+        verify(exploreMapView).enableFABRecenter()
+        verify(exploreMapView).isNetworkConnectionEstablished()
+        verifyZeroInteractions(exploreMapView)
+    }
 
+    fun expectMapAndListUpdate() {
+        exploreMapPresenter.lockUnlockNearby(false)
+        whenever(exploreMapView.isNetworkConnectionEstablished()).thenReturn(true)
+        whenever(exploreMapView.getLastLocation()).thenReturn(latestLocation)
     }
 }
