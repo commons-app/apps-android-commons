bce94f42e60730ba0de077c82d5e9f800fb78998
Alex <MahlonAlex@gmail.com>
2022-05-14 14:02:24 -0400

    Fixes #4934. Enforces Wikimedia character blocklisting on media titles.

--- app/src/main/java/fr/free/nrw/commons/upload/UploadMediaDetailAdapter.java
+++ app/src/main/java/fr/free/nrw/commons/upload/UploadMediaDetailAdapter.java
@@ -3,6 +3,7 @@ package fr.free.nrw.commons.upload;
 import android.app.Dialog;
 import android.content.Intent;
 import android.text.Editable;
+import android.text.InputFilter;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -165,6 +166,9 @@ public class UploadMediaDetailAdapter extends RecyclerView.Adapter<UploadMediaDe
                 captionInputLayout.setEndIconDrawable(R.drawable.mapbox_info_icon_default);
                 captionInputLayout.setEndIconOnClickListener(v ->
                     callback.showAlert(R.string.media_detail_caption, R.string.caption_info));
+                Objects.requireNonNull(captionInputLayout.getEditText()).setFilters(new InputFilter[] {
+                    new UploadMediaDetailInputFilter()
+                });
 
                 descInputLayout.setEndIconMode(TextInputLayout.END_ICON_CUSTOM);
                 descInputLayout.setEndIconDrawable(R.drawable.mapbox_info_icon_default);
