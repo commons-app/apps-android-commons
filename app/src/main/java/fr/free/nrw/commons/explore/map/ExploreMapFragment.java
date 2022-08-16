9efafc8e1ff62c5ad8bcd972d9284c6e13501016
Madhur Gupta <30932899+madhurgupta10@users.noreply.github.com>
2022-08-16 14:51:01 +0530

    Add prompt before asking for location in Nearby and Explore Maps (#5034)
    * Add prompt before asking for location in Nearby and Explore Maps
    
    * Fix string
    
    Co-authored-by: Josephine Lim <josephinelim86@gmail.com>

--- app/src/main/java/fr/free/nrw/commons/explore/map/ExploreMapFragment.java
+++ app/src/main/java/fr/free/nrw/commons/explore/map/ExploreMapFragment.java
@@ -31,6 +31,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.AppCompatTextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -110,6 +111,7 @@ public class ExploreMapFragment extends CommonsDaggerSupportFragment
     private Marker currentLocationMarker;
     private Polygon currentLocationPolygon;
     IntentFilter intentFilter = new IntentFilter(MapUtils.NETWORK_INTENT_ACTION);
+    private AlertDialog locationDialog;
 
     @Inject
     LiveDataConverter liveDataConverter;
@@ -400,9 +402,27 @@ public class ExploreMapFragment extends CommonsDaggerSupportFragment
     @Override
     public void checkPermissionsAndPerformAction() {
         Timber.d("Checking permission and perfoming action");
+        if (PermissionUtils.hasPermission(getActivity(),
+            Manifest.permission.ACCESS_FINE_LOCATION)) {
+            Timber.d("Permission granted");
+            getPermissions();
+        } else {
+            if (locationDialog == null) {
+                locationDialog = new AlertDialog.Builder(requireActivity())
+                    .setTitle(R.string.location_permission_title)
+                    .setMessage(R.string.location_permission_rationale_nearby)
+                    .setPositiveButton(R.string.ok, (dialog, which) -> {
+                        dialog.dismiss();
+                        getPermissions();
+                    }).show();
+            }
+        }
+    }
+
+    private void getPermissions() {
         PermissionUtils.checkPermissionsAndPerformAction(getActivity(),
             Manifest.permission.ACCESS_FINE_LOCATION,
-            () -> locationPermissionGranted(),
+            this::locationPermissionGranted,
             () -> isPermissionDenied = true,
             R.string.location_permission_title,
             R.string.location_permission_rationale_nearby);
--- app/src/main/java/fr/free/nrw/commons/utils/PermissionUtils.java
+++ app/src/main/java/fr/free/nrw/commons/utils/PermissionUtils.java
@@ -130,18 +130,7 @@ public class PermissionUtils {
                 @Override
                 public void onPermissionRationaleShouldBeShown(PermissionRequest permission,
                     PermissionToken token) {
-                    if (rationaleTitle == -1 && rationaleMessage == -1) {
-                        token.continuePermissionRequest();
-                        return;
-                    }
-                    DialogUtil.showAlertDialog(activity, activity.getString(rationaleTitle),
-                        activity.getString(rationaleMessage),
-                        activity.getString(android.R.string.ok),
-                        activity.getString(android.R.string.cancel),
-                        token::continuePermissionRequest,
-                        token::cancelPermissionRequest,
-                        null,
-                        false);
+                    token.continuePermissionRequest();
                 }
             })
             .check();
