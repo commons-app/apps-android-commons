aebaa687311b8ecac74a45e235c2424009ba2d6a
ragu-1997 <81232337+ragu-1997@users.noreply.github.com>
2022-10-10 00:35:39 +0530

    Update the shell file
    add some line in shell file because exit code is important and nohup process also import for background process.

--- sign.sh
+++ sign.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore /home/nico/d/d/wya/nr-commons.keystore commons/target/commons-1.0-SNAPSHOT.apk nrkeystorealias
-zipalign -f -v 4 commons/target/commons-1.0-SNAPSHOT.apk commons/target/commons-1.0-SNAPSHOT_signed.apk
+nohup jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore /home/nico/d/d/wya/nr-commons.keystore commons/target/commons-1.0-SNAPSHOT.apk nrkeystorealias &
+nohup zipalign -f -v 4 commons/target/commons-1.0-SNAPSHOT.apk commons/target/commons-1.0-SNAPSHOT_signed.apk &
+exit 0
