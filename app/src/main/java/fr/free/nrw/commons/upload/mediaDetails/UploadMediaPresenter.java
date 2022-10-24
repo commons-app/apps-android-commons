7db11d4a65396562356790a3dbac4b746a971045
DKolibar <dkolibar@gmail.com>
2022-10-24 10:12:12 +0200

    removed unused slf4j import in UploadMediaPresenter class

--- app/src/main/java/fr/free/nrw/commons/upload/mediaDetails/UploadMediaPresenter.java
+++ app/src/main/java/fr/free/nrw/commons/upload/mediaDetails/UploadMediaPresenter.java
@@ -36,8 +36,6 @@ import java.util.Map;
 import javax.inject.Inject;
 import javax.inject.Named;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import timber.log.Timber;
 
 public class UploadMediaPresenter implements UserActionListener, SimilarImageInterface {
