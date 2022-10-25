65886fd74836fd240940f0773744eedde9d268a8
Ruofan <u7307578@anu.edu.au>
2022-10-25 22:17:53 +1100

    add dark and proper images and check if too dar

--- app/src/test/kotlin/fr/free/nrw/commons/utils/ImageUtilsTest.kt
+++ app/src/test/kotlin/fr/free/nrw/commons/utils/ImageUtilsTest.kt
@@ -51,8 +51,28 @@ class ImageUtilsTest {
 
     @Test
     fun testCheckIfImageIsTooDarkCaseException() {
-        Assert.assertEquals(ImageUtils.checkIfImageIsTooDark("src/test/resources/ImageTest/dark1.jpg")
+        Assert.assertEquals(ImageUtils.checkIfImageIsTooDark("")
             , ImageUtils.IMAGE_OK)
+    }
+
+    @Test
+    fun testCheckIfProperImageIsTooDark() {
+        Assert.assertEquals(ImageUtils.checkIfImageIsTooDark("src/test/resources/ImageTest/ok1.jpg")
+            , ImageUtils.IMAGE_OK)
+        Assert.assertEquals(ImageUtils.checkIfImageIsTooDark("src/test/resources/ImageTest/ok2.jpg")
+            , ImageUtils.IMAGE_OK)
+        Assert.assertEquals(ImageUtils.checkIfImageIsTooDark("src/test/resources/ImageTest/ok3.jpg")
+            , ImageUtils.IMAGE_OK)
+        Assert.assertEquals(ImageUtils.checkIfImageIsTooDark("src/test/resources/ImageTest/ok4.jpg")
+            , ImageUtils.IMAGE_OK)
+    }
+
+    @Test
+    fun testCheckIfDarkImageIsTooDark() {
+        Assert.assertEquals(ImageUtils.checkIfImageIsTooDark("src/test/resources/ImageTest/dark1.jpg")
+            , ImageUtils.IMAGE_DARK)
+        Assert.assertEquals(ImageUtils.checkIfImageIsTooDark("src/test/resources/ImageTest/dark2.jpg")
+            , ImageUtils.IMAGE_DARK)
     }
 
     @Test
