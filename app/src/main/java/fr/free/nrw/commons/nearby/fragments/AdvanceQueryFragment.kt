cc24caa83c888d0d860ca9c5a9092b6333f7afc0
Ankush Bose <46471379+bosankus@users.noreply.github.com>
2022-10-29 15:03:50 +0530

    4664: Moved AdvanceQueryFragment to ViewBinding (#5091)

--- app/src/main/java/fr/free/nrw/commons/nearby/fragments/AdvanceQueryFragment.kt
+++ app/src/main/java/fr/free/nrw/commons/nearby/fragments/AdvanceQueryFragment.kt
@@ -6,52 +6,48 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import androidx.appcompat.widget.AppCompatButton
-import androidx.appcompat.widget.AppCompatEditText
 import androidx.fragment.app.Fragment
-import fr.free.nrw.commons.R
-import kotlinx.android.synthetic.main.fragment_advance_query.*
+import fr.free.nrw.commons.databinding.FragmentAdvanceQueryBinding
 
 class AdvanceQueryFragment : Fragment() {
 
+    private var _binding: FragmentAdvanceQueryBinding? = null
+    private val binding get() = _binding
     lateinit var originalQuery: String
     lateinit var callback: Callback
-    lateinit var etQuery: AppCompatEditText
-    lateinit var btnApply: AppCompatButton
-    lateinit var btnReset: AppCompatButton
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_advance_query, container, false)
+        _binding = FragmentAdvanceQueryBinding.inflate(inflater, container, false)
         originalQuery = arguments?.getString("query")!!
         setHasOptionsMenu(false)
-        return view
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        etQuery = view.findViewById(R.id.et_query)
-        btnApply = view.findViewById(R.id.btn_apply)
-        btnReset = view.findViewById(R.id.btn_reset)
 
-        etQuery.setText(originalQuery)
-        btnReset.setOnClickListener {
-            btnReset.post {
-                etQuery.setText(originalQuery)
-                etQuery.clearFocus()
-                hideKeyBoard()
-                callback.reset()
+        with(requireNotNull(binding)) {
+            etQuery.setText(originalQuery)
+            btnReset.setOnClickListener {
+                btnReset.post {
+                    etQuery.setText(originalQuery)
+                    etQuery.clearFocus()
+                    hideKeyBoard()
+                    callback.reset()
+                }
             }
-        }
 
-        btnApply.setOnClickListener {
-            btnApply.post {
-                etQuery.clearFocus()
-                hideKeyBoard()
-                callback.apply(etQuery.text.toString())
-                callback.close()
+            btnApply.setOnClickListener {
+                btnApply.post {
+                    etQuery.clearFocus()
+                    hideKeyBoard()
+                    callback.apply(etQuery.text.toString())
+                    callback.close()
+                }
             }
         }
     }
