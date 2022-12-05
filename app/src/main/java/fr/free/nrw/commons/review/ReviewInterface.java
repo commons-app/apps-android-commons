75f55244f4abad3f31ee88870ed8a62caf52061f
Jeremy Kenny <jeremytkenny@gmail.com>
2022-12-05 14:01:57 -0500

    review: exclude images not uploaded from the app
    * the Review feature (in the app's menu) was originally thought as a way to improve our app's overall upload quality, it is not really adapted to the wide variety of pictures uploaded via other upload tools.
    
    * update the Review API call to request only images with the `android app edit` tag.

--- app/src/main/java/fr/free/nrw/commons/review/ReviewInterface.java
+++ app/src/main/java/fr/free/nrw/commons/review/ReviewInterface.java
@@ -10,7 +10,7 @@ import retrofit2.http.Query;
  * Interface class for peer review calls
  */
 public interface ReviewInterface {
-    @GET("w/api.php?action=query&format=json&formatversion=2&list=recentchanges&rcprop=title|ids&rctype=new|log&rctoponly=1&rcnamespace=6")
+    @GET("w/api.php?action=query&format=json&formatversion=2&list=recentchanges&rcprop=title|ids&rctype=new|log&rctoponly=1&rcnamespace=6&rctag=android%20app%20edit")
     Observable<MwQueryResponse> getRecentChanges(@Query("rcstart") String rcStart);
 
     @GET("w/api.php?action=query&format=json&formatversion=2&prop=revisions&rvprop=timestamp|ids|user&rvdir=newer&rvlimit=1")
