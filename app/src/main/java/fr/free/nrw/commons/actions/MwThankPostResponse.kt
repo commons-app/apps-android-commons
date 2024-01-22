9a27d69c5c93463c40a5b3cf478fbe84ce3fc346
Nicolas Raoul <nicolas.raoul@gmail.com>
2024-01-22 12:24:22 +0900

    KDoc

--- app/src/main/java/fr/free/nrw/commons/actions/MwThankPostResponse.kt
+++ app/src/main/java/fr/free/nrw/commons/actions/MwThankPostResponse.kt
@@ -2,6 +2,12 @@ package fr.free.nrw.commons.actions
 
 import org.wikipedia.dataclient.mwapi.MwResponse
 
+/**
+ * Response of the Thanks API.
+ * Context:
+ * The Commons Android app lets you thank other contributors who have uploaded a great picture.
+ * See https://www.mediawiki.org/wiki/Extension:Thanks
+ */
 class MwThankPostResponse : MwResponse() {
     var result: Result? = null
 
