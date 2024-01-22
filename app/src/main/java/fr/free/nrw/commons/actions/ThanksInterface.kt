5960630e4bc5458fab715f253dac02db2740a34c
Nicolas Raoul <nicolas.raoul@gmail.com>
2024-01-22 12:25:16 +0900

    KDoc

--- app/src/main/java/fr/free/nrw/commons/actions/ThanksInterface.kt
+++ app/src/main/java/fr/free/nrw/commons/actions/ThanksInterface.kt
@@ -6,6 +6,12 @@ import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
+/**
+ * Thanks API.
+ * Context:
+ * The Commons Android app lets you thank another contributor who has uploaded a great picture.
+ * See https://www.mediawiki.org/wiki/Extension:Thanks
+ */
 interface ThanksInterface {
     @FormUrlEncoded
     @POST(Service.MW_API_PREFIX + "action=thank")
