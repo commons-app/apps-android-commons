d078b2d81a253146adf44a575bc385f639e5a060
Matija Nalis <mnalis-git@voyager.hr>
2024-06-24 02:17:11 +0200

    also count 2020s as "recent years"

--- app/src/main/java/fr/free/nrw/commons/category/CategoriesModel.kt
+++ app/src/main/java/fr/free/nrw/commons/category/CategoriesModel.kt
@@ -50,7 +50,7 @@ class CategoriesModel @Inject constructor(
                 || item.matches("(.*)needing(.*)".toRegex())
                 || item.matches("(.*)taken on(.*)".toRegex())
                 || item.matches(".*0s.*".toRegex())
-                && !item.matches(".*(200|201)0s.*".toRegex())
+                && !item.matches(".*20[0-2]0s.*".toRegex())
     }
 
     /**
