eb4edece2b4ee69de022f77e5556eb7c9ce8236c
Lachie Major <u7676758@anu.edu.au>
2024-10-18 20:15:23 +1100

    ImageFragment.kt: unnecessary initialisation after exiting media preview was removed from passSelectedImages

--- app/src/main/java/fr/free/nrw/commons/customselector/ui/selector/ImageFragment.kt
+++ app/src/main/java/fr/free/nrw/commons/customselector/ui/selector/ImageFragment.kt
@@ -279,6 +279,9 @@ class ImageFragment :
                 filteredImages = ImageHelper.filterImages(images, bucketId)
                 allImages = ArrayList(filteredImages)
                 imageAdapter.init(filteredImages, allImages, TreeMap(), uploadingContributions)
+                viewModel?.selectedImages?.value?.let { selectedImages ->
+                    imageAdapter.setSelectedImages(selectedImages)
+                }
                 imageAdapter.notifyDataSetChanged()
                 selectorRV?.let {
                     it.visibility = View.VISIBLE
@@ -383,14 +386,6 @@ class ImageFragment :
         selectedImages: ArrayList<Image>,
         shouldRefresh: Boolean,
     ) {
-        imageAdapter.setSelectedImages(selectedImages)
-
-        val uploadingContributions = getUploadingContributions()
-
-        if (!showAlreadyActionedImages && shouldRefresh) {
-            imageAdapter.init(filteredImages, allImages, TreeMap(), uploadingContributions)
-            imageAdapter.setSelectedImages(selectedImages)
-        }
     }
 
     /**
