634bc3ede1d67a775a7b8957e89007e658a48fe5
StrawberryShortcake <73601423+why-lab@users.noreply.github.com>
2024-11-13 14:11:38 +0100

    custom-selector: adds a button to delete the current folder in custom selector (#5925)
    * Issue #5811: "Delete folder" menu in custom image selector
    
    * Issue 5811: folder deletion for api < 29.
    
    * Issue 5811: folder deletion for api < 29.
    
    * Issue 5811: folder deletion for api 29.
    
    * Issue 5811: folder deletion
    
    * Issue 5811: fixes merge conflicts, replaces used function onActivityResult with an ActivityResultLauncher
    
    * Update Constants.java
    
    ---------
    
    Co-authored-by: Nicolas Raoul <nicolas.raoul@gmail.com>

--- app/src/main/java/fr/free/nrw/commons/settings/SettingsFragment.java
+++ app/src/main/java/fr/free/nrw/commons/settings/SettingsFragment.java
@@ -12,6 +12,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -59,6 +60,7 @@ import java.util.Locale;
 import java.util.Map;
 import javax.inject.Inject;
 import javax.inject.Named;
+import timber.log.Timber;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
@@ -81,6 +83,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     private ListPreference themeListPreference;
     private Preference descriptionLanguageListPreference;
     private Preference appUiLanguageListPreference;
+    private Preference showDeletionButtonPreference;
     private String keyLanguageListPreference;
     private TextView recentLanguagesTextView;
     private View separator;
@@ -187,6 +190,18 @@ public class SettingsFragment extends PreferenceFragmentCompat {
                 return true;
             }
         });
+
+        //
+        showDeletionButtonPreference = findPreference("displayDeletionButton");
+        if (showDeletionButtonPreference != null) {
+            showDeletionButtonPreference.setOnPreferenceChangeListener((preference, newValue) -> {
+                boolean isEnabled = (boolean) newValue;
+                // Save preference when user toggles the button
+                defaultKvStore.putBoolean("displayDeletionButton", isEnabled);
+                return true;
+            });
+        }
+
 
         Preference betaTesterPreference = findPreference("becomeBetaTester");
         betaTesterPreference.setOnPreferenceClickListener(preference -> {
