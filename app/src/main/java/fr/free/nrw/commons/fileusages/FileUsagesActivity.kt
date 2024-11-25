1fa78d816348e0c6ee1c7cfa228bf97f2151f4ed
parneet-guraya <gurayaparneet@gmail.com>
2024-11-25 16:56:06 +0530

    add top bar with back navigation
    Signed-off-by: parneet-guraya <gurayaparneet@gmail.com>

--- app/src/main/java/fr/free/nrw/commons/fileusages/FileUsagesActivity.kt
+++ app/src/main/java/fr/free/nrw/commons/fileusages/FileUsagesActivity.kt
@@ -7,8 +7,15 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.ui.Modifier
@@ -27,6 +34,7 @@ class FileUsagesActivity : BaseActivity() {
         viewModelFactory
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val media = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
@@ -42,14 +50,25 @@ class FileUsagesActivity : BaseActivity() {
                 colorScheme = if (isSystemInDarkTheme()) darkColorScheme(
                     primary = colorResource(R.color.primaryDarkColor),
                     surface = colorResource(R.color.main_background_dark),
-                     background = colorResource(R.color.main_background_dark)
+                    background = colorResource(R.color.main_background_dark)
                 ) else lightColorScheme(
                     primary = colorResource(R.color.primaryColor),
                     surface = colorResource(R.color.main_background_light),
                     background = colorResource(R.color.main_background_light)
                 )
             ) {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
+                    TopAppBar(title = { Text(text = "File usages") }, navigationIcon = {
+                        IconButton(onClick = {
+                            onBackPressedDispatcher.onBackPressed()
+                        }) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                                contentDescription = null
+                            )
+                        }
+                    })
+                }) { innerPadding ->
                     FileUsagesScreen(modifier = Modifier.padding(innerPadding), viewModel)
                 }
             }
