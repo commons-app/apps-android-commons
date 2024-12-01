6a508be9247b38342ab1c6ef29c503addff3e528
Neel Doshi <neeldoshi147@gmail.com>
2024-12-02 03:41:12 +0530

    Migrated OnFeedbackSubmitCallback to kotlin

--- app/src/main/java/fr/free/nrw/commons/feedback/OnFeedbackSubmitCallback.kt
+++ app/src/main/java/fr/free/nrw/commons/feedback/OnFeedbackSubmitCallback.kt
@@ -1,15 +1,7 @@
-package fr.free.nrw.commons.feedback;
+package fr.free.nrw.commons.feedback
 
-import fr.free.nrw.commons.feedback.model.Feedback;
+import fr.free.nrw.commons.feedback.model.Feedback
 
-/**
- * This interface is used to provide callback
- * from Feedback dialog whenever submit button is clicked
- */
-public interface OnFeedbackSubmitCallback {
-
-    /**
-     * callback function, called when user clicks on submit
-     */
-    void onFeedbackSubmit(Feedback feedback);
+interface OnFeedbackSubmitCallback {
+    fun onFeedbackSubmit(feedback: Feedback)
 }
