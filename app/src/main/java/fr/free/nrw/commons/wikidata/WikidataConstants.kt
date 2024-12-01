16d4a64224e69c58e6ab259a5d9eb8889bcb6ab8
Paul Hawke <paul.hawke@gmail.com>
2024-11-30 20:56:19 -0600

    Convert WikidataConstants to kotlin

---
+++ app/src/main/java/fr/free/nrw/commons/wikidata/WikidataConstants.kt
@@ -0,0 +1,11 @@
+package fr.free.nrw.commons.wikidata
+
+object WikidataConstants {
+    const val PLACE_OBJECT: String = "place"
+    const val BOOKMARKS_ITEMS: String = "bookmarks.items"
+    const val SELECTED_NEARBY_PLACE: String = "selected.nearby.place"
+    const val SELECTED_NEARBY_PLACE_CATEGORY: String = "selected.nearby.place.category"
+
+    const val MW_API_PREFIX: String = "w/api.php?format=json&formatversion=2&errorformat=plaintext&"
+    const val WIKIPEDIA_URL: String = "https://wikipedia.org/"
+}
