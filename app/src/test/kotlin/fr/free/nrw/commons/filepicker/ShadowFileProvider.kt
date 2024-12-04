24181a8fb8a1abd84c08d3ff702c90d80f60232c
Saifuddin <adenwala32@gmail.com>
2024-12-04 20:39:49 +0530

    Migrated filepicker module from Java to Kotlin

--- app/src/test/kotlin/fr/free/nrw/commons/filepicker/ShadowFileProvider.kt
+++ app/src/test/kotlin/fr/free/nrw/commons/filepicker/ShadowFileProvider.kt
@@ -1,32 +1,36 @@
-package fr.free.nrw.commons.filepicker;
+package fr.free.nrw.commons.filepicker
 
-import android.database.Cursor;
-import android.database.MatrixCursor;
-import android.net.Uri;
-import android.provider.OpenableColumns;
-import androidx.core.content.FileProvider;
-import org.robolectric.annotation.Implementation;
-import org.robolectric.annotation.Implements;
+import android.database.Cursor
+import android.database.MatrixCursor
+import android.net.Uri
+import android.provider.OpenableColumns
+import androidx.core.content.FileProvider
+import org.robolectric.annotation.Implementation
+import org.robolectric.annotation.Implements
 
-@Implements(FileProvider.class)
-public class ShadowFileProvider {
+@Implements(FileProvider::class)
+class ShadowFileProvider {
 
     @Implementation
-    public Cursor query(final Uri uri, final String[] projection, final String selection,
-        final String[] selectionArgs,
-        final String sortOrder) {
+    fun query(
+        uri: Uri?,
+        projection: Array<String>?,
+        selection: String?,
+        selectionArgs: Array<String>?,
+        sortOrder: String?
+    ): Cursor? {
 
         if (uri == null) {
-            return null;
+            return null
         }
 
-        final String[] columns = {OpenableColumns.DISPLAY_NAME, OpenableColumns.SIZE};
-        final Object[] values = {"dummy", 500};
-        final MatrixCursor cursor = new MatrixCursor(columns, 1);
+        val columns = arrayOf(OpenableColumns.DISPLAY_NAME, OpenableColumns.SIZE)
+        val values = arrayOf<Any>("dummy", 500)
+        val cursor = MatrixCursor(columns, 1)
 
-        if (!uri.equals(Uri.EMPTY)) {
-            cursor.addRow(values);
+        if (uri != Uri.EMPTY) {
+            cursor.addRow(values)
         }
-        return cursor;
+        return cursor
     }
 }
