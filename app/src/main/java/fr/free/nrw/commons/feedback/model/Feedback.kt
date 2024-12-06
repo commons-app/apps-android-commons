47c2f2c4814011e0bd55e26d1592f0c947e771df
Neel Doshi <neeldoshi147@gmail.com>
2024-12-06 22:45:41 +0530

    Modified the data class to var for mutuability

--- app/src/main/java/fr/free/nrw/commons/feedback/model/Feedback.kt
+++ app/src/main/java/fr/free/nrw/commons/feedback/model/Feedback.kt
@@ -5,26 +5,26 @@ package fr.free.nrw.commons.feedback.model
  */
 data class Feedback (
     // Version of app
-    val version : String? = null,
+    var version : String? = null,
 
     // API level of user's phone
-    val apiLevel: String? = null,
+    var apiLevel: String? = null,
 
     // Title/Description entered by user
-    val title: String? = null,
+    var title: String? = null,
 
     // Android version of user's device
-    val androidVersion: String? = null,
+    var androidVersion: String? = null,
 
     // Device Model of user's device
-    val deviceModel: String? = null,
+    var deviceModel: String? = null,
 
     // Device manufacturer name
-    val deviceManufacturer: String? = null,
+    var deviceManufacturer: String? = null,
 
     // Device name stored on user's device
-    val device: String? = null,
+    var device: String? = null,
 
     // network type user is having (Ex: Wifi)
-    val networkType: String? = null
+    var networkType: String? = null
 )
