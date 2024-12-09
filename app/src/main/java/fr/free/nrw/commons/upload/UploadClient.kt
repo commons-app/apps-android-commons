c3b4297cff2fd982866cb3f0267297ed7cb91b37
Paul Hawke <paul.hawke@gmail.com>
2024-12-09 11:47:12 -0600

    Convert upload dagger module to kotlin

--- app/src/main/java/fr/free/nrw/commons/upload/UploadClient.kt
+++ app/src/main/java/fr/free/nrw/commons/upload/UploadClient.kt
@@ -7,7 +7,9 @@ import fr.free.nrw.commons.auth.csrf.CsrfTokenClient
 import fr.free.nrw.commons.contributions.ChunkInfo
 import fr.free.nrw.commons.contributions.Contribution
 import fr.free.nrw.commons.contributions.ContributionDao
+import fr.free.nrw.commons.di.NetworkingModule
 import fr.free.nrw.commons.upload.worker.UploadWorker.NotificationUpdateProgressListener
+import fr.free.nrw.commons.utils.TimeProvider
 import fr.free.nrw.commons.wikidata.mwapi.MwException
 import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
@@ -26,6 +28,7 @@ import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicInteger
 import java.util.concurrent.atomic.AtomicReference
 import javax.inject.Inject
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Singleton
@@ -33,7 +36,7 @@ class UploadClient
     @Inject
     constructor(
         private val uploadInterface: UploadInterface,
-        private val csrfTokenClient: CsrfTokenClient,
+        @Named(NetworkingModule.NAMED_COMMONS_CSRF) private val csrfTokenClient: CsrfTokenClient,
         private val pageContentsCreator: PageContentsCreator,
         private val fileUtilsWrapper: FileUtilsWrapper,
         private val gson: Gson,
@@ -278,11 +281,7 @@ class UploadClient
                 Timber.e(throwable, "Exception occurred in uploading file from stash")
                 Observable.error(throwable)
             }
-
-        fun interface TimeProvider {
-            fun currentTimeMillis(): Long
-        }
-    }
+}
 
 private fun canProcess(
     contributionDao: ContributionDao,
---
+++ app/src/main/java/fr/free/nrw/commons/upload/UploadModule.kt
@@ -0,0 +1,33 @@
+package fr.free.nrw.commons.upload
+
+import dagger.Binds
+import dagger.Module
+import fr.free.nrw.commons.upload.categories.CategoriesContract
+import fr.free.nrw.commons.upload.categories.CategoriesPresenter
+import fr.free.nrw.commons.upload.depicts.DepictsContract
+import fr.free.nrw.commons.upload.depicts.DepictsPresenter
+import fr.free.nrw.commons.upload.license.MediaLicenseContract
+import fr.free.nrw.commons.upload.license.MediaLicensePresenter
+import fr.free.nrw.commons.upload.mediaDetails.UploadMediaDetailsContract
+import fr.free.nrw.commons.upload.mediaDetails.UploadMediaPresenter
+
+/**
+ * The Dagger Module for upload related presenters and (some other objects maybe in future)
+ */
+@Module
+abstract class UploadModule {
+    @Binds
+    abstract fun bindHomePresenter(presenter: UploadPresenter): UploadContract.UserActionListener
+
+    @Binds
+    abstract fun bindsCategoriesPresenter(presenter: CategoriesPresenter): CategoriesContract.UserActionListener
+
+    @Binds
+    abstract fun bindsMediaLicensePresenter(presenter: MediaLicensePresenter): MediaLicenseContract.UserActionListener
+
+    @Binds
+    abstract fun bindsUploadMediaPresenter(presenter: UploadMediaPresenter): UploadMediaDetailsContract.UserActionListener
+
+    @Binds
+    abstract fun bindsDepictsPresenter(presenter: DepictsPresenter): DepictsContract.UserActionListener
+}
