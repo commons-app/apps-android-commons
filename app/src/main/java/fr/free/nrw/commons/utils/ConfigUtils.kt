eb92c8aba9efe0dc65b35a20f3af5b85ec54ad4d
Paul Hawke <paul.hawke@gmail.com>
2024-12-10 23:05:41 -0600

    Convert PageContentsCreator to kotlin

--- app/src/main/java/fr/free/nrw/commons/utils/ConfigUtils.kt
+++ app/src/main/java/fr/free/nrw/commons/utils/ConfigUtils.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.content.pm.PackageManager
 import fr.free.nrw.commons.BuildConfig
 
+// TODO - this can be constructed in a Dagger provider method, in a module and injected.  No need
+//        to compute these values every time, and it means we can avoid having a Context in various
+//        other places in the app.
 object ConfigUtils {
     @JvmStatic
     val isBetaFlavour: Boolean = BuildConfig.FLAVOR == "beta"
