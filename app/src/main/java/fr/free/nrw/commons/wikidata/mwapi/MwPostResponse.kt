d6718a772a875d05ac624664511a26c4b9d96c19
parneet-guraya <gurayaparneet@gmail.com>
2024-12-25 01:39:55 +0530

    fix parsing
    Signed-off-by: parneet-guraya <gurayaparneet@gmail.com>

--- app/src/main/java/fr/free/nrw/commons/wikidata/mwapi/MwPostResponse.kt
+++ app/src/main/java/fr/free/nrw/commons/wikidata/mwapi/MwPostResponse.kt
@@ -1,6 +1,9 @@
 package fr.free.nrw.commons.wikidata.mwapi
 
+import com.google.gson.annotations.SerializedName
+
 open class MwPostResponse : MwResponse() {
+    @SerializedName("success")
     val successVal: Int = 0
 
     fun success(result: String?): Boolean =
