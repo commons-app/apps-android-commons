738b22d45554ef08a6772a4251df988371f0568c
Ifeoluwa Andrew Omole <iomole3@gmail.com>
2025-02-26 20:38:09 +0100

    Upload: Restrict generic type T to non-nullable types in BaseDelegateAdapter

--- app/src/main/java/fr/free/nrw/commons/upload/categories/BaseDelegateAdapter.kt
+++ app/src/main/java/fr/free/nrw/commons/upload/categories/BaseDelegateAdapter.kt
@@ -4,7 +4,7 @@ import androidx.recyclerview.widget.DiffUtil
 import com.hannesdorfmann.adapterdelegates4.AdapterDelegate
 import com.hannesdorfmann.adapterdelegates4.AsyncListDifferDelegationAdapter
 
-abstract class BaseDelegateAdapter<T>(
+abstract class BaseDelegateAdapter<T : Any>(
     vararg delegates: AdapterDelegate<List<T>>,
     areItemsTheSame: (T, T) -> Boolean,
     areContentsTheSame: (T, T) -> Boolean = { old, new -> old == new },
