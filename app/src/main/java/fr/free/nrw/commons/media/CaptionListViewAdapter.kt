c28ea14cac83684e43e02a63140ce65b3d02de73
Paul Hawke <paul.hawke@gmail.com>
2025-07-06 22:20:47 -0500

    Convert CaptionListViewAdapter to kotlin

--- app/src/main/java/fr/free/nrw/commons/media/CaptionListViewAdapter.kt
+++ app/src/main/java/fr/free/nrw/commons/media/CaptionListViewAdapter.kt
@@ -11,25 +11,16 @@ import fr.free.nrw.commons.R
  * Adapter for Caption Listview
  */
 class CaptionListViewAdapter(var captions: List<Caption>) : BaseAdapter() {
-    override fun getCount(): Int {
-        return captions.size
-    }
+    override fun getCount(): Int = captions.size
 
-    override fun getItem(i: Int): Any? {
-        return null
-    }
+    override fun getItem(i: Int): Any? = null
 
-    override fun getItemId(i: Int): Long {
-        return 0
-    }
+    override fun getItemId(i: Int): Long = 0
 
     override fun getView(i: Int, view: View, viewGroup: ViewGroup): View {
-        val captionLanguageTextView: TextView
-        val captionTextView: TextView
-        val captionLayout = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.caption_item, null)
-        captionLanguageTextView = captionLayout.findViewById(R.id.caption_language_textview)
-        captionTextView = captionLayout.findViewById(R.id.caption_text)
+        val captionLayout = LayoutInflater.from(viewGroup.context).inflate(R.layout.caption_item, null)
+        val captionLanguageTextView = captionLayout.findViewById<TextView>(R.id.caption_language_textview)
+        val captionTextView = captionLayout.findViewById<TextView>(R.id.caption_text)
         if (captions.size == 1 && captions[0].value == "No Caption") {
             captionLanguageTextView.text = captions[i].language
             captionTextView.text = captions[i].value
