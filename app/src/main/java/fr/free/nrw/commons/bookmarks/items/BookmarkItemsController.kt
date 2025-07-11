95ad51e204ec624f343b445836acc1827890b707
Paul Hawke <paul.hawke@gmail.com>
2025-07-11 16:00:13 -0500

    Convert BookmarkItemsController to kotlin

---
+++ app/src/main/java/fr/free/nrw/commons/bookmarks/items/BookmarkItemsController.kt
@@ -0,0 +1,23 @@
+package fr.free.nrw.commons.bookmarks.items
+
+import fr.free.nrw.commons.upload.structure.depictions.DepictedItem
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * Handles loading bookmarked items from Database
+ */
+@Singleton
+class BookmarkItemsController @Inject constructor() {
+    @JvmField
+    @Inject
+    var bookmarkItemsDao: BookmarkItemsDao? = null
+
+    /**
+     * Load from DB the bookmarked items
+     * @return a list of DepictedItem objects.
+     */
+    fun loadFavoritesItems(): List<DepictedItem> {
+        return bookmarkItemsDao?.allBookmarksItems ?: emptyList()
+    }
+}
