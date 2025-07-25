04ba42db202e38c4f167208971b6b523ebeda23b
Sonal Yadav <sonalyadav@Sonals-MacBook-Air.local>
2025-07-25 09:32:52 +0530

    Fix  Nearby place name missing in Nearby Place Found popup

--- app/src/main/java/fr/free/nrw/commons/nearby/model/NearbyResultItem.kt
+++ app/src/main/java/fr/free/nrw/commons/nearby/model/NearbyResultItem.kt
@@ -7,7 +7,7 @@ class NearbyResultItem(
     private val wikipediaArticle: ResultTuple?,
     private val commonsArticle: ResultTuple?,
     private val location: ResultTuple?,
-    @field:SerializedName("itemLabel")
+    @field:SerializedName("Label")
     private val label: ResultTuple?,
     @field:SerializedName("streetAddress") private val address: ResultTuple?,
     private val icon: ResultTuple?,
