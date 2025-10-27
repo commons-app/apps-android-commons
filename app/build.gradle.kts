d88f03db22fcf1a861d584e9624a58f3647735fa
Jason Whitmore <jason@whitmore.email>
2025-10-27 14:43:49 -0700

    build.gradle.kts, libs.versions.toml: add page indicator library
    Before this change, the page indicator library that was used
    was incompatible with ViewPager2.
    
    This change adds a page indicator library under the Apache 2.0
    license that works with ViewPager2. Now the project can take
    advantage of ViewPager2 features, such as support for RTL
    languages/orientations.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -231,6 +231,7 @@ dependencies {
 
     // UI
     implementation("${libs.viewpagerindicator.library.get()}@aar")
+    implementation(libs.viewpager2indicator)
     implementation(libs.photoview)
     implementation(libs.android.sdk)
     implementation(libs.android.plugin.scalebar)
