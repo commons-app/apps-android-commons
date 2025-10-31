8329fcb43398b4d66ca6b3e46fa9d25c206e7af1
Paul Hawke <paul.hawke@gmail.com>
2025-10-31 08:51:02 -0500

    Convert NearbyfilterSearchListView to kotlin

---
+++ app/src/main/java/fr/free/nrw/commons/nearby/NearbyfilterSearchListView.kt
@@ -0,0 +1,13 @@
+package fr.free.nrw.commons.nearby
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ListView
+
+class NearbyfilterSearchListView : ListView {
+    constructor(context: Context?) : super(context)
+
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+}
