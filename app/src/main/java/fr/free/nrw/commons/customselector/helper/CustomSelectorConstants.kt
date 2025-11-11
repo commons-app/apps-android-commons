624f93a8f52830fe163513e2ac89c6d6095df72c
Kota-Jagadeesh <kota.jagadesh123@gmail.com>
2025-11-11 20:25:33 +0530

    Fix: enfore a maximum selection limit of 20 images

--- app/src/main/java/fr/free/nrw/commons/customselector/helper/CustomSelectorConstants.kt
+++ app/src/main/java/fr/free/nrw/commons/customselector/helper/CustomSelectorConstants.kt
@@ -10,4 +10,8 @@ object CustomSelectorConstants {
     const val NEW_SELECTED_IMAGES = "new_selected_images"
     const val SHOULD_REFRESH = "should_refresh"
     const val FULL_SCREEN_MODE_FIRST_LUNCH = "full_screen_mode_first_launch"
+    const val MAX_IMAGE_COUNT = 20
+
+    const val Less_image_upload = 10
+
 }
