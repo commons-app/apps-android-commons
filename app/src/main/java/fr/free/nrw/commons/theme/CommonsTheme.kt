ad4feb57cdb792e2752065b339d64a538977aa56
Kota-Jagadeesh <kota.jagadesh123@gmail.com>
2026-03-28 00:13:17 +0530

    chore: implement global CommonsTheme usin compose color schemes and material 3 surface

--- app/src/main/java/fr/free/nrw/commons/theme/CommonsTheme.kt
+++ app/src/main/java/fr/free/nrw/commons/theme/CommonsTheme.kt
@@ -2,39 +2,28 @@ package fr.free.nrw.commons.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import fr.free.nrw.commons.R
 
 @Composable
 fun CommonsTheme(content: @Composable () -> Unit) {
     val isDarkTheme = isSystemInDarkTheme()
-    val background = if (isDarkTheme)
-        colorResource(R.color.main_background_dark)
-    else
-        colorResource(R.color.main_background_light)
-
     val colorScheme = if (isDarkTheme) {
         darkColorScheme(
-            primary = colorResource(R.color.primaryColor),
-            background = background,
-            onBackground = Color.White,
-            surfaceVariant = Color(0xFF1E1E1E),
-            onSurface = Color.White
+            primary = PrimaryBlueNight,
+            background = BackgroundDark,
+            onBackground = TextWhite,
+            surfaceVariant = CardBackgroundDark,
+            onSurface = TextWhite
         )
     } else {
         lightColorScheme(
-            primary = colorResource(R.color.primaryColor),
-            background = background,
-            onBackground = Color.Black,
-            surfaceVariant = Color.White,
-            onSurface = Color.Black
+            primary = PrimaryBlue,
+            background = BackgroundLight,
+            onBackground = TextBlack,
+            surfaceVariant = CardBackgroundLight,
+            onSurface = TextBlack
         )
     }
 
