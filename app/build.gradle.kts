1e6af4b147b7da9f8f11c51e5f3c45dff838e124
Rover track <rishan.pgowda@gmail.com>
2026-08-14 18:53:58 +0530

    Remove local .aar and add library as dependency through maven

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -357,7 +357,7 @@ dependencies {
     implementation(libs.ajpegtran)
 
     // AutoDetect
-    implementation(files("libs/library-release.aar"))
+    implementation(libs.commons.ml)
 
     implementation(libs.coordinates2country.android) {
         exclude(group = "com.google.android", module = "android")
